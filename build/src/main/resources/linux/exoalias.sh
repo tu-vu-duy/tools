bb27851be342085429aeb28e3511ab068542e03d
tuan08 <tuan08@42d3c691-033b-0410-95b4-af486b8189e5>
2007-05-01 09:20:49 +0000

    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@1444 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/resources/linux/exoalias.sh
+++ build/src/main/resources/linux/exoalias.sh
@@ -5,14 +5,6 @@ alias cdback='cd  $OLDPWD'
 
 alias mvnrepoclean="rm -rf $M2_REPO/org/exoplatform/* "
 
-alias mvnmodulecleaninstall="mvn clean install"
-alias mvnmoduledeploy="mvn clean install exo:deploy "
-alias mvnmoduledeployall="mvn clean install exo:deploy "
-
-
-alias vibuild="vi $USER_HOME/build.properties"
-alias visetting="vi $USER_HOME/.m2/settings.xml"
-
 alias cdtomcat="cd $EXO_WORKING_DIR/exo-tomcat"
 alias cdtomcatbin="cd $EXO_WORKING_DIR/exo-tomcat/bin"
 alias tomcatClean="cd $EXO_WORKING_DIR/exo-tomcat/bin &&
@@ -20,7 +12,7 @@ alias tomcatClean="cd $EXO_WORKING_DIR/exo-tomcat/bin &&
                    rm -rf ../work/* &&
                    rm -rf ../logs/* "
    
-alias tomcatRun="cd $EXO_WORKING_DIR/exo-tomcat/bin && chmod +x *.sh && ./exo-run.sh "
+alias tomcatRun="cd $EXO_WORKING_DIR/exo-tomcat/bin && chmod +x *.sh && ./eXo.sh run"
 
 alias tomcatCleanRun="cd $EXO_WORKING_DIR/exo-tomcat/bin &&
 		                  rm -rf ../temp/* &&
@@ -28,10 +20,6 @@ alias tomcatCleanRun="cd $EXO_WORKING_DIR/exo-tomcat/bin &&
                       rm -rf ../logs/*  &&
                       chmod +x *.sh && ./eXo.sh run "
 
-
-alias tomcatPortletDeploy="mvn compile  jar:jar && cp target/*.jar  src/webapp/WEB-INF/lib/"
-alias tomcatContextDeploy="cp src/resources/tomcat/*.xml  $EXO_WORKING_DIR/exo-tomcat/conf/Catalina/localhost/"
-
-alias tomcat="exobsh.sh tomcat"
+alias exoproject="exobsh.sh exoproject"
 alias exobuild="exobsh.sh exobuild"
 alias exosvn="exobsh.sh exosvn"
