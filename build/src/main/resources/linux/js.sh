402c5806eb927bd498b290c95083cf2af1902e1e
tuan08 <tuan08@42d3c691-033b-0410-95b4-af486b8189e5>
2007-05-24 02:12:50 +0000

    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@2265 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/resources/linux/js.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SCRIPT_DIR=$BSH_EXO_BASE_DIRECTORY/eXoProjects/tools/trunk/build
+
+CURRENT_DIR=`pwd`
+
+JAVA_CMD=$BSH_JAVA_HOME/bin/java
+
+echo $@
+
+$JAVA_CMD -Xshare:auto -Xms128m -Xmx512m -classpath $SCRIPT_DIR/src/main/resources/java/js.jar \
+          -Dexo.java.home=$BSH_JAVA_HOME \
+          -Dexo.current.dir=$CURRENT_DIR \
+          -Dexo.base.dir=$BSH_EXO_BASE_DIRECTORY  \
+          -Dexo.m2.repos="$BSH_M2_REPOS"  \
+          org.mozilla.javascript.tools.shell.Main $SCRIPT_DIR/src/main/javascript/eXo/eXo.js $@
+
+
