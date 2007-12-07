1c732ea9ca983f6e06669f81b491108257b1334e
trongtran <trongtran@42d3c691-033b-0410-95b4-af486b8189e5>
2007-12-07 08:30:54 +0000

    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@8702 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/spff/trunk.js
+++ build/src/main/javascript/eXo/products/spff/trunk.js
@@ -7,32 +7,33 @@ function getProduct(version) {
   product.name = "spff" ;
   product.portalwar = "portal.war" ;
   product.codeRepo = "spff/internet/trunk" ;
-
+    
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/trunk") ;
   var ws = Module.GetModule("ws/trunk");
   var core = Module.GetModule("core/trunk") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/trunk") ;
   var eXoJcr = Module.GetModule("jcr/trunk") ;
-  var portal = Module.GetModule("portal/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
-  var ecm = Module.GetModule("ecm/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
-  var spff = Module.GetModule("spff/internet/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ecm : ecm});
+  var portal = Module.GetModule("portal/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
+  var cs = Module.GetModule("cs/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var spff = Module.GetModule("spff/internet/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
     
   product.addDependencies(portal.portlet.exoadmin) ;
   product.addDependencies(portal.portlet.web) ;
   product.addDependencies(portal.eXoWidget.web) ;
-  product.addDependencies(ecm.portlet.ecm) ;
-  product.addDependencies(ecm.portlet.workflow) ;
-  product.addDependencies(spff.web.spffportal) ;
-  product.addDependencies(spff.web.spffResources) ;
-  product.addDependencies(spff.portlet.web) ;
-    
+            
+  product.addDependencies(cs.eXoApplication.mail) ;
+  product.addDependencies(cs.eXoApplication.forum) ;
+  product.addDependencies(cs.eXoApplication.calendar) ;
+  product.addDependencies(cs.eXoApplication.contact) ;
+  product.addDependencies(cs.eXoApplication.content) ;
+  product.addDependencies(cs.web.csportal) ;
+  
   product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
   product.addServerPatch("jboss",  portal.server.jboss.patch) ;
   product.addServerPatch("jonas",  portal.server.jonas.patch) ;
-
   product.module = spff ;
-  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, eXoJcr, portal, ecm];
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, eXoJcr, portal];
     
   return product ;
 }
