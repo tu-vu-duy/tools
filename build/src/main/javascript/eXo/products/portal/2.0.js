d271591c997c24ff10ca0c2c76c0bba6ec7483a4
trongtran <trongtran@42d3c691-033b-0410-95b4-af486b8189e5>
2007-12-21 08:34:44 +0000

    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@9222 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/portal/2.0.js
@@ -0,0 +1,32 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "eXoPortal" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "portal/2.0" ;
+
+  var tool = Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/branches/2.0") ;
+  var ws = Module.GetModule("ws/trunk");
+  var core = Module.GetModule("core/branches/2.0") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/trunk") ;    
+  var eXoJcr = Module.GetModule("jcr/trunk") ;
+  var portal = Module.GetModule("portal/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
+
+  product.addDependencies(portal.web.portal) ;
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.eXoWidget.web) ;
+
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+
+  product.module = portal ;
+  product.dependencyModule = [ tool, kernel, core, eXoPortletContainer, ws, eXoJcr ];
+
+  return product ;
+}
