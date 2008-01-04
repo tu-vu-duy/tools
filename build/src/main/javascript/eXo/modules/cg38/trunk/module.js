dbd63fec98ad6a4e6ea149bc15960f8a6ae56944
phu_nguyen_ba <phu_nguyen_ba@42d3c691-033b-0410-95b4-af486b8189e5>
2008-01-04 10:49:24 +0000

    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@9538 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/cg38/trunk/module.js
@@ -0,0 +1,35 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+  var ecm = params.ecm;
+
+  var module = new Module();
+
+  module.version = "trunk" ;
+  module.relativeMavenRepo =  "org/exoplatform/cg38" ;
+  module.relativeSRCRepo =  "cg38/trunk" ;
+  module.name =  "cg38" ;  
+        
+  module.portlet = {}
+  module.portlet.web = new Project("org.exoplatform.cg38", "cg38.portlet.web", "exo-portlet", module.version);
+  module.portlet.web.deployName = "cg38PortletWeb" ;
+        
+  module.web = {}
+  module.web.cg38portal = 
+    new Project("org.exoplatform.cg38", "exo.cg38.web.portal", "exo-portal", module.version).
+    addDependency(portal.web.eXoResources) .
+//    addDependency(portal.web.eXoMacSkin) .
+//    addDependency(portal.web.eXoVistaSkin) .
+    addDependency(portal.webui.portal) .
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web) ;
+
+  return module;
+}
---
+++ build/src/main/javascript/eXo/products/cg38/trunk.js
@@ -0,0 +1,38 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "cg38" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "cg38/trunk" ;
+  product.useWorkflow = true;
+    
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/trunk") ;
+  var ws = Module.GetModule("ws/trunk");
+  var core = Module.GetModule("core/trunk") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/trunk") ;
+  var eXoJcr = Module.GetModule("jcr/trunk") ;
+  var portal = Module.GetModule("portal/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var ecm = Module.GetModule("ecm/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var cg38 = Module.GetModule("cg38/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ecm : ecm});
+    
+  product.addDependencies(cg38.web.cg38portal) ;
+  product.addDependencies(cg38.portlet.web) ;
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.eXoWidget.web) ;
+  product.addDependencies(ecm.portlet.ecm) ;
+  product.addDependencies(ecm.portlet.workflow) ;  
+  
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+
+  product.module = cg38 ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, ecm];
+    
+  return product ;
+}
