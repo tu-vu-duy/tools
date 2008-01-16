9c0858759a0a583045ecfe3818b41977ca846274
phu_nguyen_ba <phu_nguyen_ba@42d3c691-033b-0410-95b4-af486b8189e5>
2008-01-16 03:14:26 +0000

    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@9862 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cg38/trunk.js
+++ build/src/main/javascript/eXo/products/cg38/trunk.js
@@ -7,7 +7,7 @@ function getProduct(version) {
   product.name = "cg38" ;
   product.portalwar = "portal.war" ;
   product.codeRepo = "cg38/trunk" ;
-  //product.useWorkflow = true;
+  product.useWorkflow = true;
     
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/trunk") ;
@@ -15,7 +15,7 @@ function getProduct(version) {
   var core = Module.GetModule("core/trunk") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/trunk") ;
   var eXoJcr = Module.GetModule("jcr/trunk") ;
-  var portal = Module.GetModule("portal/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var portal = Module.GetModule("portal/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var ecm = Module.GetModule("ecm/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
   var cg38 = Module.GetModule("cg38/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ecm : ecm});
     
