3ca6911d1e1da295c6ed818f08ca13847e2fc95c
minh_dang <minh_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2008-01-18 02:34:31 +0000

    Fix for update params in exobuild
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@9968 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/command/exosvn.js
+++ build/src/main/javascript/eXo/command/exosvn.js
@@ -1,7 +1,7 @@
 eXo.require("eXo.core.TaskDescriptor") ;
-eXo.require("eXo.core.Util");
 
 function exosvn() {
+
 
 };
 
@@ -20,13 +20,13 @@ exosvn.prototype.Param = function(args) {
 }
 
 exosvn.prototype.MatchFile = function (param) {
-  this.proc = java.lang.Runtime.getRuntime().exec("svn st " + param.workingDir) ;
-  var din = new java.io.DataInputStream(this.proc.getInputStream() );
-  this.files = new java.util.ArrayList() ;
-  //var line = din.readLine();
-  while( (line=din.readLine()) != null ) {
-    if(line.matches(param.include) && !line.matches(param.exclude)) {
-      this.files.add(line) ;
+  var proc = java.lang.Runtime.getRuntime().exec("svn st " + param.workingDir) ;
+  var din = new java.io.DataInputStream(proc.getInputStream() );
+  var files = new java.util.ArrayList() ;
+  var line = din.readLine()
+  while( line  != null ) {
+    if(line.matches(param.include)) {
+      files.add(line) ;
       print(line);
     }
   }
@@ -47,67 +47,7 @@ exosvn.prototype.UpdateTask = function(module) {
   return task;
 }
 
-exosvn.prototype.AddTask = function() {
-    var descriptor =  new TaskDescriptor("svn add", eXo.env.currentDir) ;
-
-    descriptor.execute = function() {
-      var param = exosvn.Param({}) ;
-      if(param.workingDir == null)  param.workingDir = descriptor.workingDir ;
-      if(param.include == null) param.include = "^\\?.*" ;
-	  if(param.exclude == null) param.exclude = ".*target$";
-	  java.lang.System.setProperty("user.dir",  param.workingDir) ;
-	  
-      var matches =  exosvn.MatchFile(param) ;
-      var proc =  matches.proc ;
-      var exitValue = proc.exitValue();
-      if(exitValue == 0 && matches.files.size() > 0) {
-        input = eXo.System.readInput("Do you want to add the above files[yes]") ;
-        if("yes".equals(input)) {
-          var b = new java.lang.StringBuilder() ;
-          b.append("svn add ");
-          for(i = 0; i < matches.files.size();i++) { 
-            b.append(matches.files.get(i).substring(6)); 
-          }
-          proc = java.lang.Runtime.getRuntime().exec(b.toString()) ;
-        }
-      } else {
-        print("Cannot perform the svn command, svn exit with the code " + exitValue); 
-      }
-      errStream = new java.io.DataInputStream( proc.getInputStream() );
-      while( (line = errStream.readLine()) != null ) print(line) ;
-    }
-    return descriptor ;
-  }
-
-var args = arguments;
-
-function printExosvnInstructions() {
-	print(
-      "\n\n" +
-      "Usage of exosvn command: \n\n" +
-      "  exosvn add | up | rm \n\n" +
-      "Options: \n" +
-      "  * add             adds the new files from the module on svn\n" +
-      "  * rm          	   removes files in the module\n"
-    );
-}
-
-if(args.length == 0) {
-	printExosvnInstructions() ;
-	java.lang.System.exit(1);
-}
-
-exosvn = new exosvn();
-
-args = args[0];
-
-if ("add".equals(args)) {
-	exosvn.AddTask().execute() ;
-} else if ("rm".equals(args)) {
-	//exosvn.RemoveTask().execute() ;
-}
-
-//eXo.command.exosvn = exosvn.prototype.constructor;
+eXo.command.exosvn = exosvn.prototype.constructor;
 //exosvn = new exosvn();
 //var task = exosvn.UpdateTask(eXo.env.eXoProjectsDir + "/portal/trunk");
 //task.execute();
