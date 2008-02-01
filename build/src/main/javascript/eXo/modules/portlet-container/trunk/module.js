cfda7d1ffccf3c3f1f55e6c7248ecb41652d9337
tran <tran@42d3c691-033b-0410-95b4-af486b8189e5>
2008-02-01 10:43:33 +0000

    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@10648 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/portlet-container/trunk/module.js
+++ build/src/main/javascript/eXo/modules/portlet-container/trunk/module.js
@@ -14,6 +14,7 @@ function getModule(params) {
     new Project("org.exoplatform.portletcontainer", "exo.pc.component.core", "jar", module.version).
     addDependency(new Project("org.exoplatform.portletcontainer", "exo.pc.component.plugins.pc", "jar", module.version)).
     addDependency(new Project("javax.ccpp", "ccpp", "jar", "1.0")).
+    addDependency(new Project("javax.xml.bind", "jaxb-api", "jar", "2.0")).
     addDependency(new Project("javax.portlet", "portlet-api", "jar", "2.0"));
   
   return module;
