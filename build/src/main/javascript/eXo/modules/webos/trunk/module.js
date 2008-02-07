830c3523934e1caa7b6627f79cddafd636127a07
benjamin_mestrallet <benjamin_mestrallet@42d3c691-033b-0410-95b4-af486b8189e5>
2008-02-07 20:41:24 +0000

    commit skeleton of webos only
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@10786 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/webos/trunk/module.js
@@ -0,0 +1,30 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+
+  var module = new Module();
+
+  module.version = "trunk" ;
+  module.relativeMavenRepo =  "org/exoplatform/webos" ;
+  module.relativeSRCRepo =  "webos/trunk" ;
+  module.name =  "webos" ;
+                   
+  module.web = {}
+  module.web.webosportal = 
+    new Project("org.exoplatform.webos", "webos.web.portal", "exo-portal", module.version).
+    addDependency(portal.web.eXoResources) .
+    addDependency(portal.web.eXoMacSkin) .
+    addDependency(portal.web.eXoVistaSkin) .
+    addDependency(portal.webui.portal) .
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web) ;
+
+  return module;
+}
