0a77d29bc6a71017bd7d6f5388d9d2c75fbc57b5
tran <tran@42d3c691-033b-0410-95b4-af486b8189e5>
2008-02-12 03:29:25 +0000

    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@10852 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/resources/linux/js.sh
+++ build/src/main/resources/linux/js.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-SCRIPT_DIR=$EXO_BASE_DIRECTORY/eXoProjects/tools/trunk/build
+SCRIPT_DIR=$BSH_EXO_BASE_DIRECTORY/eXoProjects/tools/trunk/build
 CURRENT_DIR=`pwd`
 JAVA_CMD=$JAVA_HOME/bin/java
 echo $@
 $JAVA_CMD -Xshare:auto -Xms128m -Xmx512m -classpath $SCRIPT_DIR/src/main/resources/java/js.jar \
           -Dexo.java.home=$JAVA_HOME \
           -Dexo.current.dir=$CURRENT_DIR \
-          -Dexo.base.dir=$EXO_BASE_DIRECTORY  \
+          -Dexo.base.dir=$BSH_EXO_BASE_DIRECTORY  \
           -Dexo.m2.repos="$M2_REPOS"  \
           org.mozilla.javascript.tools.shell.Main $SCRIPT_DIR/src/main/javascript/eXo/eXo.js $@
