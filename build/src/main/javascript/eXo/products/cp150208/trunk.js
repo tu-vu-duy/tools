d89f1d177061019ea8dd12b2594326cdde65e812
tung_nguyen_dang <tung_nguyen_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2008-02-15 04:31:45 +0000

    add new poc cp150208
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@10970 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/cp150208/trunk.js
@@ -0,0 +1,35 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "cp150208" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "cp150208/trunk" ;
+  product.useWorkflow = false;
+    
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0") ;
+  var ws = Module.GetModule("ws/tags/1.1");
+  var core = Module.GetModule("core/tags/2.0") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/trunk") ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.8") ;
+	var portal = Module.GetModule("portal/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var cp150208 = Module.GetModule("cp150208/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+    
+	product.addDependencies(cp150208.web.cp150208Portal) ;
+  
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.eXoWidget.web) ;
+  
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+
+	product.module = cp150208 ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal];
+    
+  return product ;
+}
