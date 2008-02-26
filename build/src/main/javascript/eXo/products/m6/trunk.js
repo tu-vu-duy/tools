8b1dc040f8acef02722ec9fbdab33b32a85b1b15
patrice_lamarque <patrice_lamarque@42d3c691-033b-0410-95b4-af486b8189e5>
2008-02-26 18:08:37 +0000

    Upgraded customer projects to stable versions
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@11357 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/m6/trunk.js
+++ build/src/main/javascript/eXo/products/m6/trunk.js
@@ -6,7 +6,7 @@ function getProduct(version) {
 
   product.name = "m6" ;
   product.portalwar = "portal.war" ;
-  product.codeRepo = "m6/trunk" ;
+  product.codeRepo = "M6/trunk" ;
   product.useWorkflow = true;
   product.workflowVersion = "2.0" ;
   product.serverPluginVersion = "2.0" ;
@@ -14,9 +14,9 @@ function getProduct(version) {
   var tool = Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0") ;
   var ws = Module.GetModule("ws/tags/1.1");
-  var core = Module.GetModule("core/tags/2.0") ;
+  var core = Module.GetModule("core/tags/2.0.1") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/trunk") ;    
-  var eXoJcr = Module.GetModule("jcr/tags/1.7.1") ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.8") ;
   var portal = Module.GetModule("portal/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var ecm = Module.GetModule("ecm/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
   var m6 = Module.GetModule("m6/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ecm : ecm});
