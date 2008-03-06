3cbd42d67d3e394071d550f2a8060e988b20608d
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-03-06 04:02:29 +0000

    pc 2.0rc2 and ks modules
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@11636 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/portlet-container/tags/2.0rc2/module.js
@@ -0,0 +1,22 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+  var module = new Module();
+  
+  module.version = "2.0rc2" ;
+  module.relativeMavenRepo = "org/exoplatform/portletcontainer" ;
+  module.relativeSRCRepo = "portlet-container/tags/2.0rc2" ;
+  module.name = "pc" ;
+
+  module.services = {} ;
+  module.services.jsr168 = 
+    new Project("org.exoplatform.portletcontainer", "exo.pc.component.core", "jar", module.version).
+    addDependency(new Project("org.exoplatform.portletcontainer", "exo.pc.component.plugins.pc", "jar", module.version)).
+    addDependency(new Project("javax.ccpp", "ccpp", "jar", "1.0")).
+    addDependency(new Project("javax.xml.bind", "jaxb-api", "jar", "2.0")).
+    addDependency(new Project("com.sun.xml.bind", "jaxb-impl", "jar", "2.0.2")).
+    addDependency(new Project("javax.portlet", "portlet-api", "jar", "2.0"));
+  
+  return module;
+}
