c0ea36b22506bdf7398a15546a210a845b17315c
phu_nguyen_ba <phu_nguyen_ba@42d3c691-033b-0410-95b4-af486b8189e5>
2008-04-08 07:52:11 +0000

    create module.js in core 2.0.2
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@12878 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/webos/1.0.js
+++ build/src/main/javascript/eXo/products/webos/1.0.js
@@ -6,18 +6,18 @@ function getProduct(version) {
   
   product.name = "eXoWebOS" ;
   product.portalwar = "portal.war" ;
-  product.codeRepo = "webos/trunk" ;
+  product.codeRepo = "webos/branches/1.0" ;
   product.useWorkflow = false;
   product.serverPluginVersion = "2.0" ;
     
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0") ;
   var ws = Module.GetModule("ws/tags/1.1");
-  var core = Module.GetModule("core/tags/2.0.1") ;
+  var core = Module.GetModule("core/tags/2.0.2") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc4") ;
-  var eXoJcr = Module.GetModule("jcr/tags/1.8.1") ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.8.2") ;
   var portal = Module.GetModule("portal/branches/2.0", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
-  var webos = Module.GetModule("webos/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal });
+  var webos = Module.GetModule("webos/branches/1.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal });
   
   product.addDependencies(portal.portlet.exoadmin) ;
   product.addDependencies(portal.portlet.web) ;
