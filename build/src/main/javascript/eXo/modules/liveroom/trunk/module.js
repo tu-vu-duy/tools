e09008be1080751e1d442e8884914bf9f02b0418
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-04-11 04:34:02 +0000

    added liveroom module
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@13089 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/liveroom/trunk/module.js
@@ -0,0 +1,27 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+  var ws = params.ws;
+	
+  var module = new Module();
+  
+  module.version = "trunk" ;
+  module.relativeMavenRepo = "org/exoplatform/liveroom" ;
+  module.relativeSRCRepo = "liveroom/trunk" ;
+  module.name = "liveroom" ;
+  
+  module.eXoApplication = {}
+  module.eXoApplication.chat = {}
+  module.eXoApplication.chat.webapp = new Project("org.exoplatform.liveroom", "exo.liveroom.eXoApplication.chat.webapp", "war", "0.1").
+  	addDependency(new Project("org.exoplatform.liveroom", "exo.liveroom.eXoApplication.chat.service", "jar", "0.1"));
+  module.eXoApplication.chat.webapp.deployName = "exomessenger";
+  
+
+  return module ;
+}
