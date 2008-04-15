d9603c47797a2f80da47417e659b08596d4c7a65
uoc_nguyen <uoc_nguyen@42d3c691-033b-0410-95b4-af486b8189e5>
2008-04-15 11:59:48 +0000

    Add module webservice for CS product for branches v1.0
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@13250 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cs/1.0.js
+++ build/src/main/javascript/eXo/products/cs/1.0.js
@@ -25,6 +25,7 @@ function getProduct(version) {
   product.addDependencies(cs.eXoApplication.calendar) ;
   product.addDependencies(cs.eXoApplication.contact) ;
   product.addDependencies(cs.eXoApplication.content) ;
+  product.addDependencies(cs.eXoApplication.webservice) ;
   product.addDependencies(cs.web.csResources) ;
   product.addDependencies(cs.web.csportal) ;
   
