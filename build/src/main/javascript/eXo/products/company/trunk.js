d7215cfe44d0f10d415f9da06bffae91e75d9e67
minh_dang <minh_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2008-04-15 10:25:44 +0000

    Change for company use portal tags 2.0
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@13242 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/company/trunk.js
+++ build/src/main/javascript/eXo/products/company/trunk.js
@@ -18,7 +18,7 @@ function getProduct(version) {
   var core = Module.GetModule("core/tags/2.0.1") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc4") ;
   var eXoJcr = Module.GetModule("jcr/tags/1.8.2") ;
-  var portal = Module.GetModule("portal/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var portal = Module.GetModule("portal/tags/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var ecm = Module.GetModule("ecm/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
   var company = Module.GetModule("company/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
 
