7ed904a0cf48f45ca873aea257aa3222b2b56159
uoc_nguyen <uoc_nguyen@42d3c691-033b-0410-95b4-af486b8189e5>
2008-04-16 11:03:20 +0000

    Add organization application to liveroom
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@13332 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/liveroom/trunk/module.js
+++ build/src/main/javascript/eXo/modules/liveroom/trunk/module.js
@@ -18,8 +18,11 @@ function getModule(params) {
   
   module.eXoApplication = {}
   module.eXoApplication.chat = {}
-  module.eXoApplication.chat.webapp = new Project("org.exoplatform.liveroom", "exo.liveroom.eXoApplication.chat.webapp", "war", "0.1").
-  	addDependency(new Project("org.exoplatform.liveroom", "exo.liveroom.eXoApplication.chat.service", "jar", "0.1").
+  module.eXoApplication.chat.webapp = new Project("org.exoplatform.liveroom", "exo.liveroom.eXoApplication.chat.webapp", "war", "trunk").
+  	addDependency(new Project("org.exoplatform.liveroom", "exo.liveroom.eXoApplication.chat.service", "jar", "trunk").
+  		addDependency(new Project("org.exoplatform.liveroom", "exo.liveroom.eXoApplication.organization.service", "jar", "trunk")).
+  		addDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", "trunk")).
+  		addDependency(new Project("org.exoplatform.ecm", "exo.ecm.web.rest", "war", "trunk")).
   		addDependency(new Project("jabber.smack", "smack", "jar", "3.0.4")).
   		addDependency(new Project("jabber.smack", "smackx", "jar", "3.0.4")).
   		addDependency(new Project("org.jcrom", "jcrom", "jar", "1.1"))
--- build/src/main/javascript/eXo/products/liveroom/trunk.js
+++ build/src/main/javascript/eXo/products/liveroom/trunk.js
@@ -14,6 +14,7 @@ function getProduct(version) {
   var eXoPortletContainer = Module.GetModule("portlet-container/trunk") ;
   var eXoJcr = Module.GetModule("jcr/trunk") ;
   var portal = Module.GetModule("portal/trunk", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
+  var ecm = Module.GetModule("ecm/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
   var liveroom = Module.GetModule("liveroom/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal}) ;
 
   product.addDependencies(portal.web.portal) ;
@@ -24,7 +25,7 @@ function getProduct(version) {
   product.addDependencies(liveroom.eXoApplication.chat.webapp) ;
  // product.addDependencies(liveroom.eXoApplication.chat.service) ;
 
-  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("tomcat", ecm.server.tomcat.patch) ;
   product.addServerPatch("jboss",  portal.server.jboss.patch) ;
   product.addServerPatch("jonas",  portal.server.jonas.patch) ;
     
