54c06dd9765a267951e9ad6c09bffd725bbbba1c
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-04-25 11:05:45 +0000

    configure cp240408 descriptors
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@13766 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/cp240408/trunk/module.js
+++ build/src/main/javascript/eXo/modules/cp240408/trunk/module.js
@@ -8,6 +8,7 @@ function getModule(params) {
   var eXoPortletContainer = params.eXoPortletContainer;
   var jcr = params.eXoJcr;
   var portal = params.portal;
+  var webos = params.webos;
 
   var module = new Module();
 
@@ -17,11 +18,43 @@ function getModule(params) {
   module.name =  "cp240408" ;  
 
 	
-		module.web = {} ;
-		module.web.eXoResources = new Project("org.exoplatform.cp240408", "cp240408.web.cp240408Resources", "war", module.version);
+  module.web = {} ;
+  module.web.eXoResources = new Project("org.exoplatform.cp240408", "cp240408.web.cp240408Resources", "war", module.version);
   module.web.eXoResources.deployName = "eXoResourcesCp240408" ;
+  module.web.bpmportal = new Project("org.ow2.novabpm.admin", "bpmportal", "war", "1.0-SNAPSHOT").
+    addDependency(portal.web.eXoResources) .
+    addDependency(webos.web.webosportal) .
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web) ;
+  module.web.bpmportal.deployName = "portal" ;
   
+  module.component = {} ;
+  module.component.service = {} ;
+  module.component.service.bonita = new Project("org.ow2.novabpm.admin", "org.ow2.novabpm.admin.service.bonita", "jar", "1.0-SNAPSHOT").
+  	addDependency(new Project("org.ow2.novabpm.admin", "org.ow2.novabpm.admin.service.api", "jar", "1.0-SNAPSHOT")).
+  	addDependency(new Project("org.ow2.bonita", "bonita", "jar", "4.0.snapshot")).
+  	addDependency(new Project("org.ow2.bonita", "novaBpmIdentity", "jar", "4.0.snapshot")).
+  	addDependency(new Project("org.ow2.bonita", "novaBpmUtil", "jar", "4.0.snapshot")).
+  	addDependency(new Project("org.ow2.bonita", "pvm", "jar", "4.0.snapshot")).
+  	addDependency(new Project("rome", "rome", "jar", "0.8")) .
+    addDependency(new Project("com.totsp.feedpod", "itunes-com-podcast", "jar", "0.2")) .
+    addDependency(new Project("ical4j", "ical4j", "jar", "0.9.20")) .
+    addDependency(new Project("jdom", "jdom", "jar", "1.0")).
+    addDependency(new Project("bsh", "bsh", "jar", "2.0b1")).
+    addDependency(new Project("javax.servlet", "servlet-api", "jar", "2.4")).
+    addDependency(new Project("net.wimpi.pim", "jpim-0.1", "jar", "1.0")).
+    addDependency(new Project("org.hibernate", "hibernate", "jar", "3.2.5.ga")) ;
+  
+  module.portlet = {} ;
+  module.portlet.bonita = new Project("org.ow2.novabpm.admin", "bonita.portlet", "war", "1.0-SNAPSHOT").
+  		addDependency(new Project("org.ow2.novabpm.admin", "org.ow2.novabpm.admin.service.bonita", "jar", "1.0-SNAPSHOT")).
+  		addDependency(new Project("org.ow2.novabpm.admin", "org.ow2.novabpm.admin.commons.webui", "jar", "1.0-SNAPSHOT")) ;
+  module.portlet.orchestra = new Project("org.ow2.novabpm.admin", "orchestra.portlet", "war", "1.0-SNAPSHOT").
+  	addDependency(new Project("org.ow2.novabpm.admin", "org.ow2.novabpm.admin.commons.webui", "jar", "1.0-SNAPSHOT")) ;
 
+  module.server = {} ;
+  module.server.tomcat = {} ;
+  module.server.tomcat.patch = new Project("org.exoplatform.cp240408", "cp240408.server.tomcat.patch", "jar", module.version);
 
   return module;
 }
--- build/src/main/javascript/eXo/products/cp240408/trunk.js
+++ build/src/main/javascript/eXo/products/cp240408/trunk.js
@@ -17,23 +17,30 @@ function getProduct(version) {
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc5") ;
   var eXoJcr = Module.GetModule("jcr/tags/1.8.2") ;
   var portal = Module.GetModule("portal/branches/2.0.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
-  var cp240408 = Module.GetModule("cp240408/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var ecm = Module.GetModule("ecm/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var webos = Module.GetModule("webos/branches/1.0.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal });
+  var cp240408 = Module.GetModule("cp240408/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, webos : webos});
     
-    
-    product.addDependencies(portal.web.portal) ;
+  product.addDependencies(cp240408.component.service.bonita) ;
+  product.addDependencies(cp240408.web.bpmportal) ;
+  product.addDependencies(cp240408.web.eXoResources) ;
+  product.addDependencies(cp240408.portlet.bonita) ;
+  product.addDependencies(cp240408.portlet.orchestra) ;
+  
   product.addDependencies(portal.portlet.exoadmin) ;
   product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.eXoWidget.web) ;
+  product.addDependencies(portal.web.eXoMacSkin) ;
+  product.addDependencies(portal.web.eXoVistaSkin) ;
   
-  product.addDependencies(cp240408.web.eXoResources) ;
+  product.addDependencies(ecm.web.rest) ;
   
-
-  
-  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("tomcat", cp240408.server.tomcat.patch) ;
   product.addServerPatch("jboss",  portal.server.jboss.patch) ;
   product.addServerPatch("jonas",  portal.server.jonas.patch) ;
 
-	product.module = cp240408 ;
-  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal];
+  product.module = cp240408 ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, ecm, webos];
     
   return product ;
 }
