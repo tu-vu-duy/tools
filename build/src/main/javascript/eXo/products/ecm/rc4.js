d6ec3b20905719fc8386f378464fad61b1a42276
minh_dang <minh_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2008-04-28 18:01:03 +0000

    use tags rc4 for ecm
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@13810 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/ecm/rc4.js
@@ -0,0 +1,40 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "eXoECM" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "ecm/tags/rc4" ;
+  product.useWorkflow = true;
+  product.workflowVersion = "rc4" ;
+  product.serverPluginVersion = "2.0.1" ;
+    
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.1") ;
+  var ws = Module.GetModule("ws/tags/1.1");
+  var core = Module.GetModule("core/tags/2.0.2") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc5") ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
+  var portal = Module.GetModule("portal/branches/2.0.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var ecm = Module.GetModule("ecm/tags/rc4", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+    
+  product.addDependencies(ecm.web.ecmportal) ;
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(ecm.web.rest) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(ecm.portlet.ecm) ;
+  product.addDependencies(ecm.portlet.workflow) ;
+    
+  product.addDependencies(portal.eXoWidget.web) ;
+  
+  product.addServerPatch("tomcat", ecm.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+
+  product.module = ecm ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal];
+    
+  return product ;
+}
