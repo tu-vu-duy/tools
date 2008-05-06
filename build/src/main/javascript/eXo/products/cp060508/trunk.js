883403eb7f926a5396c41fda77270d4a4e120e90
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-05-06 04:03:06 +0000

    create cp060508 descriptor files
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@13903 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/cp060508/trunk.js
@@ -0,0 +1,41 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "cp060508" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "cp060508/trunk" ;
+  product.useWorkflow = false;
+  product.serverPluginVersion = "2.0" ;
+    
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.1") ;
+  var ws = Module.GetModule("ws/tags/1.1");
+  var core = Module.GetModule("core/tags/2.0.2") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc5") ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.8.2") ;
+  var portal = Module.GetModule("portal/branches/2.0.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var ecm = Module.GetModule("ecm/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var cp060508 = Module.GetModule("cp060508/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+    
+    
+  product.addDependencies(cp060508.web.eXoResources) ;
+  
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.eXoWidget.web) ;
+  product.addDependencies(portal.web.eXoMacSkin) ;
+  product.addDependencies(portal.web.eXoVistaSkin) ;
+
+  
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+
+  product.module = cp060508 ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, ecm];
+    
+  return product ;
+}
