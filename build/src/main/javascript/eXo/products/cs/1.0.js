b5f0b435f7914dc09d4c98f586ea7b619b89df22
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-05-19 03:25:42 +0000

    cs 1.0 uses portal 2.0
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@14429 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cs/1.0.js
+++ build/src/main/javascript/eXo/products/cs/1.0.js
@@ -14,7 +14,7 @@ function getProduct(version) {
   var core = Module.GetModule("core/tags/2.0.2") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc5") ;
   var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
-  var portal = Module.GetModule("portal/branches/2.0.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
+  var portal = Module.GetModule("portal/tags/2.0", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
   var cs = Module.GetModule("cs/tags/1.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
 
   product.addDependencies(portal.portlet.exoadmin) ;
