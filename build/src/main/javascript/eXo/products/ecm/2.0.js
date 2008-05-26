af2b64a5bf9dc0a217a1de4e8344b5fdad97619e
minh_dang <minh_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2008-05-26 06:44:14 +0000

    ECM-2154 Change config to use portal tag 2.0.1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@14707 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/ecm/2.0.js
+++ build/src/main/javascript/eXo/products/ecm/2.0.js
@@ -17,7 +17,7 @@ function getProduct(version) {
   var ws = Module.GetModule("ws/tags/1.1.2");
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc5") ;
   var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
-  var portal = Module.GetModule("portal/branches/2.0.1", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var portal = Module.GetModule("portal/tags/2.0.1", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var ecm = Module.GetModule("ecm/branches/2.0", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
     
   product.addDependencies(ecm.web.ecmportal) ;
