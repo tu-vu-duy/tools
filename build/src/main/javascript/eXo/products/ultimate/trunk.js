024c82e49e9b1965b7eb2c6b1668f4bfe26babba
anh_do_ngoc <anh_do_ngoc@42d3c691-033b-0410-95b4-af486b8189e5>
2008-05-28 07:21:34 +0000

    integrate wcm to ultimate WCM-81
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@14836 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/ultimate/trunk.js
+++ build/src/main/javascript/eXo/products/ultimate/trunk.js
@@ -22,6 +22,7 @@ function getProduct(version) {
   var cs = Module.GetModule("cs/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
   var ks = Module.GetModule("ks/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
   var ecm = Module.GetModule("ecm/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});  
+  var wcm = Module.GetModule("wcm/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal, ecm : ecm});  
   var liveroom = Module.GetModule("liveroom/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal}) ;
   var ultimate = Module.GetModule("ultimate/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws :ws, eXoJcr : eXoJcr, portal : portal, cs: cs, ecm : ecm, ks : ks});
     
@@ -42,13 +43,18 @@ function getProduct(version) {
   product.addDependencies(ks.eXoApplication.faq) ;
   
   product.addDependencies(liveroom.eXoApplication.chat.webapp) ;  
+  
+  product.addDependencies(wcm.portlet.iweb);
+  product.addDependencies(wcm.portlet.webpresentation);
+  product.addDependencies(wcm.portlet.websearches);
+  product.addDependencies(wcm.web.wcmResources) ;  
 
   product.addServerPatch("tomcat", ecm.server.tomcat.patch) ;
   product.addServerPatch("jboss",  portal.server.jboss.patch) ;
   product.addServerPatch("jonas",  portal.server.jonas.patch) ;
 
   product.module = ultimate ;
-  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, cs, ks, ecm, liveroom];
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, cs, ks, ecm, liveroom, wcm];
   
   return product;
 }
