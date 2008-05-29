d8970d50ff898f694ea1063d2d33935a2f6dbbd2
tuan_pham <tuan_pham@42d3c691-033b-0410-95b4-af486b8189e5>
2008-05-29 09:29:24 +0000

    add cometd package to deploy trunk cs
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@14890 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/cs/trunk/module.js
+++ build/src/main/javascript/eXo/modules/cs/trunk/module.js
@@ -31,6 +31,7 @@ function getModule(params) {
   module.eXoApplication.calendar = 
     new Project("org.exoplatform.cs", "exo.cs.eXoApplication.calendar.webapp", "war", module.version).
       addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.calendar.service", "jar",  module.version)).
+	  addDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", module.version)).
 	  addDependency(new Project("rome", "rome", "jar", "0.8")).
 	  addDependency(new Project("jdom", "jdom", "jar", "1.0")).
       addDependency(new Project("ical4j", "ical4j", "jar", "0.9.20")) ;
