96c74e963865f20e293c0e89bc8135c1c4942464
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-06-03 03:16:36 +0000

    TOOLS-11 fixed the ear patch by removing the '-' char from its name
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@15104 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/portal/branches/2.0.2/module.js
+++ build/src/main/javascript/eXo/modules/portal/branches/2.0.2/module.js
@@ -116,7 +116,7 @@ function getModule(params) {
 
   module.server.jbossear = {}
   module.server.jbossear.patch = 
-    new Project("org.exoplatform.portal", "exo.portal.server.jboss.patch-ear", "jar", module.version);
+    new Project("org.exoplatform.portal", "exo.portal.server.jboss.patchear", "jar", module.version);
     
   module.server.websphere = {}
   module.server.websphere.patch = 
