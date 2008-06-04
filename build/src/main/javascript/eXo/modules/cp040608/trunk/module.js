185cea285aee5c3a034fcb96dc3549de8c21cca3
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-06-04 03:10:55 +0000

    SC-1 add product's modules descriptor file
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@15202 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/cp040608/trunk/module.js
@@ -0,0 +1,46 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+
+  var module = new Module();
+
+  module.version = "trunk" ;
+  module.relativeMavenRepo =  "org/exoplatform/cp040608" ;
+  module.relativeSRCRepo =  "cp040608/trunk" ;
+  module.name = "cp040608" ;  
+
+	module.portlet = {}
+  module.portlet.web = new Project("org.exoplatform.cp040608", "cp040608.portlet.web", "exo-portlet", module.version);
+  module.portlet.web.deployName = "cp040608PortletWeb" ;
+	
+	module.web = {} ;
+	module.web.eXoResources = new Project("org.exoplatform.cp040608", "cp040608.web.cp040608Resources", "war", module.version);
+  module.web.eXoResources.deployName = "eXoResourcesCp040608" ;
+  
+  module.component = {} ;
+  module.component.web=
+    new Project("org.exoplatform.cp040608", "cp040608.component.web", "jar", module.version).
+    addDependency(portal.component.web) ;
+      
+  module.server = {}
+  module.server.tomcat = {}
+  module.server.tomcat.patch = 
+    new Project("org.exoplatform.cp040608", "cp040608.server.tomcat.patch", "jar", module.version);
+  
+  module.web.portal = 
+    new Project("org.exoplatform.cp040608", "cp040608.web.portal", "exo-portal", module.version).
+    addDependency(portal.web.eXoResources) .
+    addDependency(portal.webui.portal) .
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web) ;
+	
+	
+  return module;
+}
