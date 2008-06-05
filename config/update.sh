8a9d596e30f76f058f30b628acac3c871682655b
sunman <sunman@42d3c691-033b-0410-95b4-af486b8189e5>
2008-06-05 08:30:43 +0000

    unix script to update sources, should be placed within eXoProjects (or your root directory of eXo souces)
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@15320 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ config/update.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#place of this file in the directory of eXo sources
+
+EXO_PROJECTS_SRC_DIR=`pwd | awk '{i=split($0, Name, "/") ; print(Name[i])}'`
+echo EXO_PROJECTS_SRC_DIR = $EXO_PROJECTS_SRC_DIR
+
+cd ..
+echo "Updating $EXO_PROJECTS_SRC_DIR ..."
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects $EXO_PROJECTS_SRC_DIR
+echo " eXo: $EXO_PROJECTS_SRC_DIR DONE"
+cd $EXO_PROJECTS_SRC_DIR
+
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/kernel/tags kernel/tags
+echo " eXo: kernel/tags DONE"
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/kernel/branches kernel/branches
+echo " eXo: kernel/branches DONE"
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/core/tags core/tags
+echo " eXo: core/tags DONE"
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/core/branches core/branches
+echo " eXo: core/branches DONE"
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/ws/tags ws/tags
+echo " eXo: ws/tags DONE"
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/ws/branches ws/branches
+echo " eXo: ws/branches DONE"
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/portlet-container/tags portlet-container/tags
+echo " eXo: portlet-container/tags DONE"
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/portlet-container/branches portlet-container/branches
+echo " eXo: portlet-container/branches DONE"
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/jcr/tags jcr/tags
+echo " eXo: jcr/tags DONE"
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/jcr/branches jcr/branches
+echo " eXo: jcr/branches DONE"
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/portal/tags portal/tags
+echo " eXo: portal/tags DONE"
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/portal/branches portal/branches
+echo " eXo: portal/branches DONE"
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/ecm/tags ecm/tags
+echo " eXo: ecm/tags DONE"
+svn co http://svn.exoplatform.org/svnroot/exoplatform/projects/ecm/branches ecm/branches
+echo " eXo: ecm/branches DONE"
+
+echo "THE END"
+read
