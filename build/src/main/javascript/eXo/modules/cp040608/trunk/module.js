67e1133e73b85a59a6add67e1527a9b888bdfcda
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-06-13 10:40:04 +0000

    SC-2 changed configuration descriptors with ecm
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@15675 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/cp040608/trunk/module.js
+++ build/src/main/javascript/eXo/modules/cp040608/trunk/module.js
@@ -8,20 +8,22 @@ function getModule(params) {
   var eXoPortletContainer = params.eXoPortletContainer;
   var jcr = params.eXoJcr;
   var portal = params.portal;
-
+  var ecm = params.ecm ;
+  //var cs = params.cs ;
+  var ws = params.ws;
   var module = new Module();
 
   module.version = "trunk" ;
   module.relativeMavenRepo =  "org/exoplatform/cp040608" ;
   module.relativeSRCRepo =  "cp040608/trunk" ;
-  module.name = "cp040608" ;  
-
-	module.portlet = {}
+  module.name =  "cp040608" ;
+       
+  module.portlet = {}
   module.portlet.web = new Project("org.exoplatform.cp040608", "cp040608.portlet.web", "exo-portlet", module.version);
   module.portlet.web.deployName = "cp040608PortletWeb" ;
 	
-	module.web = {} ;
-	module.web.eXoResources = new Project("org.exoplatform.cp040608", "cp040608.web.cp040608Resources", "war", module.version);
+  module.web = {} ;
+  module.web.eXoResources = new Project("org.exoplatform.cp040608", "cp040608.web.cp040608Resources", "war", module.version);
   module.web.eXoResources.deployName = "eXoResourcesCp040608" ;
   
   module.component = {} ;
@@ -33,14 +35,19 @@ function getModule(params) {
   module.server.tomcat = {}
   module.server.tomcat.patch = 
     new Project("org.exoplatform.cp040608", "cp040608.server.tomcat.patch", "jar", module.version);
-  
+
+  module.web = {}
   module.web.portal = 
-    new Project("org.exoplatform.cp040608", "cp040608.web.portal", "exo-portal", module.version).
+    new Project("org.exoplatform.cp040608", "exo.cp040608.web.portal", "exo-portal", module.version).   
     addDependency(portal.web.eXoResources) .
+    addDependency(portal.web.eXoMacSkin) .
+    addDependency(portal.web.eXoVistaSkin) .
     addDependency(portal.webui.portal) .
+  //  addDependency(cs.web.csResources) .
     addDependency(jcr.frameworks.command) .
-    addDependency(jcr.frameworks.web) ;
-	
-	
+    addDependency(jcr.frameworks.web).
+    addDependency(ecm.web.rest);
+    
+
   return module;
 }
