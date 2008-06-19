d1b7b84b396ca9ed9ddc4c8438e3ea0ac6c9d3c2
tran <tran@42d3c691-033b-0410-95b4-af486b8189e5>
2008-06-19 05:02:44 +0000

    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@15983 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/portal/branches/2.1.1/module.js
+++ build/src/main/javascript/eXo/modules/portal/branches/2.1.1/module.js
@@ -10,9 +10,9 @@ function getModule(params) {
 
   var module = new Module();
 
-  module.version =  "2.0.2" ;
+  module.version =  "2.1.1" ;
   module.relativeMavenRepo =  "org/exoplatform/portal" ;
-  module.relativeSRCRepo =  "portal/branches/2.0.2" ;
+  module.relativeSRCRepo =  "portal/branches/2.1.1" ;
   module.name =  "portal" ;
     
   module.component = {}
--- build/src/main/javascript/eXo/products/portal/2.1.1.js
+++ build/src/main/javascript/eXo/products/portal/2.1.1.js
@@ -6,16 +6,16 @@ function getProduct(version) {
   
   product.name = "eXoPortal" ;
   product.portalwar = "portal.war" ;
-  product.codeRepo = "portal/branches/2.0.2" ;
-  product.serverPluginVersion = "2.0.2" ;
-
+  product.codeRepo = "portal/branches/2.1.1" ;
+  product.serverPluginVersion = "2.1.1" ;
+  
   var tool = Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.1") ;
-  var ws = Module.GetModule("ws/tags/1.1");
+  var ws = Module.GetModule("ws/tags/1.1.2");
   var core = Module.GetModule("core/tags/2.0.2") ;
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc5") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
-  var portal = Module.GetModule("portal/branches/2.0.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
+  var portal = Module.GetModule("portal/branches/2.1.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
 
   product.addDependencies(portal.web.portal) ;
   product.addDependencies(portal.portlet.exoadmin) ;
