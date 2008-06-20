79fd3551f80015e9ba02d292b59f05eceb5b5d7a
minh_dang <minh_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2008-06-20 04:50:07 +0000

    Revert version
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@16070 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/resources/linux/exoalias.sh
+++ build/src/main/resources/linux/exoalias.sh
@@ -2,21 +2,26 @@ alias ls="ls --color"
 alias cdhome="cd ~"
 alias cdwinhome="cd $USER_HOME"
 alias cdback='cd  $OLDPWD'
+
 alias mvnrepoclean="rm -rf $M2_REPO/org/exoplatform/* $M2_REPO/javax/portlet/*"
 alias cdtomcat="cd $EXO_WORKING_DIR/exo-tomcat"
 alias cdtomcatbin="cd $EXO_WORKING_DIR/exo-tomcat/bin"
+
 alias tomcatClean="cd $EXO_WORKING_DIR/exo-tomcat/bin &&
                    rm -rf ../temp/* &&
                    rm -rf ../work/* &&
                    rm -rf ../logs/* "
+
 alias tomcatRun="cd $EXO_WORKING_DIR/exo-tomcat/bin && chmod +x *.sh && ./eXo.sh run"
 alias tomcatCleanRun="cd $EXO_WORKING_DIR/exo-tomcat/bin &&
 		                  rm -rf ../temp/* &&
                       rm -rf ../work/* &&
                       rm -rf ../logs/*  &&
                       chmod +x *.sh && ./eXo.sh run "
+
 alias exoproject="js.sh exoproject"
 alias exobuild="js.sh exobuild"
 #alias exosvn="exobsh.sh exosvn"
 alias exosvn="js.sh exosvn"
+
 alias rmdb="rm -rf $EXO_WORKING_DIR/exo-tomcat/temp/data"
