793d46427c903174519a20ed7250299c34cdf08b
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-06-24 14:55:21 +0000

    update product descriptor with portal and deploy
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@16278 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cp040608/trunk.js
+++ build/src/main/javascript/eXo/products/cp040608/trunk.js
@@ -9,13 +9,13 @@ function getProduct(version) {
   product.codeRepo = "cp040608/trunk" ;
   product.useWorkflow = true;
   product.workflowVersion = "2.0" ;
-  product.serverPluginVersion = "2.0.2" ;
+  product.serverPluginVersion = "2.1.1" ;
   
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.1") ;
   var ws = Module.GetModule("ws/tags/1.1.2");
   var core = Module.GetModule("core/tags/2.0.2") ;
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc5", {kernel : kernel, core : core}) ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
   var portal = Module.GetModule("portal/branches/2.1.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
   var ecm = Module.GetModule("ecm/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
@@ -55,7 +55,7 @@ function getProduct(version) {
   product.addDependencies(cp040608.web.portal) ;
   product.addDependencies(cp040608.portlet.web) ;
 
-  product.addServerPatch("tomcat", ecm.server.tomcat.patch) ;
+  product.addServerPatch("tomcat", cp040608.server.tomcat.patch) ;
   product.addServerPatch("jboss",  portal.server.jboss.patch) ;
   product.addServerPatch("jonas",  portal.server.jonas.patch) ;
 
