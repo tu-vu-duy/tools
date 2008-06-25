c580771ef1f3b5276a8048884618778b7ce4aa7a
patrice_lamarque <patrice_lamarque@42d3c691-033b-0410-95b4-af486b8189e5>
2008-06-25 06:00:37 +0000

    ws as a portal dependency
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@16314 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/portal/2.0.1.js
+++ build/src/main/javascript/eXo/products/portal/2.0.1.js
@@ -13,9 +13,9 @@ function getProduct(version) {
   var kernel = Module.GetModule("kernel/tags/2.0.1") ;
   var ws = Module.GetModule("ws/tags/1.1");
   var core = Module.GetModule("core/tags/2.0.2") ;
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc5", {kernel : kernel, core : core}) ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
-  var portal = Module.GetModule("portal/tags/2.0.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
+  var portal = Module.GetModule("portal/tags/2.0.1", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
 
   product.addDependencies(portal.web.portal) ;
   product.addDependencies(portal.portlet.exoadmin) ;
--- build/src/main/javascript/eXo/products/portal/2.1.js
+++ build/src/main/javascript/eXo/products/portal/2.1.js
@@ -15,7 +15,7 @@ function getProduct(version) {
   var core = Module.GetModule("core/tags/2.0.2") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
-  var portal = Module.GetModule("portal/tags/2.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
+  var portal = Module.GetModule("portal/tags/2.1", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
 
   product.addDependencies(portal.web.portal) ;
   product.addDependencies(portal.portlet.exoadmin) ;
