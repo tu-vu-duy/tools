fb4d3860d3b60a025284491c19dc5d6d228802b1
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-06-30 03:08:25 +0000

    TOOLS-29 back to ecm tag 2.0
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@16604 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cp060508/trunk.js
+++ build/src/main/javascript/eXo/products/cp060508/trunk.js
@@ -18,7 +18,7 @@ function getProduct(version) {
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
   var portal = Module.GetModule("portal/tags/2.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
-  var ecm = Module.GetModule("ecm/branches/2.0.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var ecm = Module.GetModule("ecm/tags/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
   var cs = Module.GetModule("cs/tags/1.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
   var cp060508 = Module.GetModule("cp060508/trunk", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
     
