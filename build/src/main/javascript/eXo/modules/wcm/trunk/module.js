d74b798d2bd365c355bc1bd179ca17ae400b6c5a
dung_dang_tan <dung_dang_tan@42d3c691-033b-0410-95b4-af486b8189e5>
2008-07-09 10:31:17 +0000

    WCM-78 Improve winzard in edit mode for SCP
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@17080 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/wcm/trunk/module.js
+++ build/src/main/javascript/eXo/modules/wcm/trunk/module.js
@@ -23,7 +23,8 @@ function getModule(params) {
     new Project("org.exoplatform.wcm", "exo.wcm.portlet.web-presentation", "exo-portlet", module.version).    
     addDependency(new Project("org.exoplatform.wcm", "exo.wcm.webui.wcm", "jar",  module.version)).
     addDependency(new Project("org.exoplatform.wcm", "exo.wcm.connector.fckeditor", "jar",  module.version)).
-    addDependency(new Project("org.exoplatform.wcm", "exo.wcm.component.wcm", "jar",  module.version));
+    addDependency(new Project("org.exoplatform.wcm", "exo.wcm.component.wcm", "jar",  module.version)).
+    addDependency(new Project("org.exoplatform.ecm", "exo.ecm.webui.ecm", "jar",  module.version));
     
  module.portlet.websearches = 
     new Project("org.exoplatform.wcm", "exo.wcm.portlet.web-searches", "exo-portlet", module.version).
