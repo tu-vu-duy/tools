4e92b31932f136d82eff4adb86961567b902c401
minh_dang <minh_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2008-07-14 02:57:06 +0000

    use pc 2.0.1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@17180 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/ecm/2.1.js
+++ build/src/main/javascript/eXo/products/ecm/2.1.js
@@ -15,7 +15,7 @@ function getProduct(version) {
   var kernel = Module.GetModule("kernel/tags/2.0.2") ;
   var core = Module.GetModule("core/tags/2.1") ;
   var ws = Module.GetModule("ws/tags/1.2");
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0", {kernel : kernel, core : core}) ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.1", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.9") ;
   var portal = Module.GetModule("portal/branches/2.2", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var ecm = Module.GetModule("ecm/branches/2.1", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
