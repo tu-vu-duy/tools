956a1562557aa7d69d1f41db076f90b404a1dbe8
uoc_nguyen <uoc_nguyen@42d3c691-033b-0410-95b4-af486b8189e5>
2008-07-14 11:32:20 +0000

    Add missing cometd dependencies for liveroom's deploy tool
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@17203 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/liveroom/trunk.js
+++ build/src/main/javascript/eXo/products/liveroom/trunk.js
@@ -16,8 +16,7 @@ function getProduct(version) {
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.1", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.9") ;
   var portal = Module.GetModule("portal/branches/2.2", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
- // var ecm = Module.GetModule("ecm/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
-  var liveroom = Module.GetModule("liveroom/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal}) ;
+  var liveroom = Module.GetModule("liveroom/trunk", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal}) ;
 
   product.addDependencies(portal.portlet.exoadmin) ;
   product.addDependencies(portal.portlet.web) ;
