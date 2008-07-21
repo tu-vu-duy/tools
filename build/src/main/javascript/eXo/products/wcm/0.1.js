5c1bc42dd2df582d50ae34fdbf450c640deb826c
anh_do_ngoc <anh_do_ngoc@42d3c691-033b-0410-95b4-af486b8189e5>
2008-07-21 07:35:39 +0000

    config wcm branches-0.1 to use ecm tags-2.0
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@17318 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/wcm/0.1.js
+++ build/src/main/javascript/eXo/products/wcm/0.1.js
@@ -18,7 +18,7 @@ function getProduct(version) {
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc5", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
   var portal = Module.GetModule("portal/tags/2.0.1", {kernel : kernel, core : core, ws:ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
-  var ecm = Module.GetModule("ecm/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr ,ws : ws, portal : portal});
+  var ecm = Module.GetModule("ecm/tags/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr ,ws : ws, portal : portal});
   var wcm = Module.GetModule("wcm/branches/0.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal,ecm : ecm});
     
   product.addDependencies(portal.eXoWidget.web) ;
