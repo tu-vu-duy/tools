b81462ef54f1734b5937809691d51696d008efb7
patrice_lamarque <patrice_lamarque@42d3c691-033b-0410-95b4-af486b8189e5>
2008-07-28 13:24:25 +0000

    updated build scripts . No, source in w cs/branches/1.0  builds CS 1.0.2-SNAPSHOT
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@17618 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/cs/branches/1.0/module.js
+++ build/src/main/javascript/eXo/modules/cs/branches/1.0/module.js
@@ -11,7 +11,7 @@ function getModule(params) {
 
   var module = new Module();
 
-  module.version =  "1.0.1-SNAPSHOT" ;
+  module.version =  "1.0.2-SNAPSHOT" ;
   module.relativeMavenRepo =  "org/exoplatform/cs" ;
   module.relativeSRCRepo =  "cs/branches/1.0" ;
   module.name = "cs" ;
