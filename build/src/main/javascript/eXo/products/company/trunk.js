470d9f6cf9818088712517f07a31851843d77d04
nicolas <nicolas@42d3c691-033b-0410-95b4-af486b8189e5>
2008-07-30 09:37:42 +0000

    ECM 2.0 is not anymore a branche, so I make it point to the tag module
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@17730 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/company/trunk.js
+++ build/src/main/javascript/eXo/products/company/trunk.js
@@ -19,7 +19,7 @@ function getProduct(version) {
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc5", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.8.2") ;
   var portal = Module.GetModule("portal/tags/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
-  var ecm = Module.GetModule("ecm/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var ecm = Module.GetModule("ecm/tags/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
   var company = Module.GetModule("company/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
 
   product.addDependencies(company.component.web) ;
