e6b26b19508e82da07b20a2516b819c5e264ea72
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-08-12 02:38:47 +0000

    Nhat: Change Portal Branches 2.0 to Portal Tags 2.0
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@18307 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/spff/trunk.js
+++ build/src/main/javascript/eXo/products/spff/trunk.js
@@ -17,7 +17,7 @@ function getProduct(version) {
   var core = Module.GetModule("core/tags/2.1") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.1", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.9") ;
-  var portal = Module.GetModule("portal/branches/2.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, ws : ws });  
+  var portal = Module.GetModule("portal/tags/2.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, ws : ws });  
   var ecm = Module.GetModule("ecm/branches/2.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
   var cs = Module.GetModule("cs/branches/1.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
   var ks = Module.GetModule("ks/branches/1.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal}); 
