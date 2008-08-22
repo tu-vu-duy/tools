20bb1a49a1143da5f10602808c4c972b99ce79ec
nicolas <nicolas@42d3c691-033b-0410-95b4-af486b8189e5>
2008-08-22 19:59:45 +0000

    Migration from ECM 2.0 to ECM 2.0.2
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@18938 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/company/trunk.js
+++ build/src/main/javascript/eXo/products/company/trunk.js
@@ -9,17 +9,17 @@ function getProduct(version) {
   product.portalwar = "portal.war" ;
   product.codeRepo = "company/trunk" ;
   product.useWorkflow = true ;
-  product.workflowVersion = "2.0" ;
-  product.serverPluginVersion = "2.0" ;
+  product.workflowVersion = "2.0.2" ;
+  product.serverPluginVersion = "2.1.1" ;
 
   var tool =  Module.GetModule("tools/trunk") ;
-  var kernel = Module.GetModule("kernel/tags/2.0") ;
-  var ws = Module.GetModule("ws/tags/1.1");
+  var kernel = Module.GetModule("kernel/tags/2.0.1") ;
+  var ws = Module.GetModule("ws/tags/1.1.2");
   var core = Module.GetModule("core/tags/2.0.2") ;
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc5", {kernel : kernel, core : core}) ;
-  var eXoJcr = Module.GetModule("jcr/tags/1.8.2") ;
-  var portal = Module.GetModule("portal/tags/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
-  var ecm = Module.GetModule("ecm/tags/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
+  var portal = Module.GetModule("portal/tags/2.1.1", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var ecm = Module.GetModule("ecm/tags/2.0.2", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
   var company = Module.GetModule("company/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
 
   product.addDependencies(company.component.web) ;
