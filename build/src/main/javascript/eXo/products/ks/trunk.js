87d7b699671afadd738df7d1fc0c602cffb98c75
patrice_lamarque <patrice_lamarque@42d3c691-033b-0410-95b4-af486b8189e5>
2008-08-27 19:04:23 +0000

    upgraded buuild script to latest dependencies
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@19166 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/ks/trunk.js
+++ build/src/main/javascript/eXo/products/ks/trunk.js
@@ -8,13 +8,13 @@ function getProduct(version) {
   product.portalwar = "portal.war" ;
 
   var tool =  Module.GetModule("tools/trunk") ;
-  var kernel = Module.GetModule("kernel/tags/2.0.1") ;
-  var ws = Module.GetModule("ws/tags/1.1.2");
-  var core = Module.GetModule("core/tags/2.0.2") ;
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc5", {kernel : kernel, core : core}) ;
-  var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
-  var portal = Module.GetModule("portal/tags/2.0.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
-  var cs = Module.GetModule("cs/trunk", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var kernel = Module.GetModule("kernel/tags/2.0.2") ;
+  var ws = Module.GetModule("ws/tags/1.2");
+  var core = Module.GetModule("core/tags/2.1") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.1", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.9") ;
+  var portal = Module.GetModule("portal/tags/2.2", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
+  var cs = Module.GetModule("cs/branches/1.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
   var ks = Module.GetModule("ks/trunk", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, cs: cs});
   
 
@@ -24,8 +24,8 @@ function getProduct(version) {
             
   product.addDependencies(ks.eXoApplication.forum) ;
   //product.addDependencies(ks.eXoApplication.content) ;
-  product.addDependencies(ks.eXoApplication.blog) ;
-  product.addDependencies(ks.eXoApplication.wiki) ;
+  //product.addDependencies(ks.eXoApplication.blog) ;
+  //product.addDependencies(ks.eXoApplication.wiki) ;
   product.addDependencies(ks.eXoApplication.faq) ;
   product.addDependencies(ks.web.ksportal) ;
   
