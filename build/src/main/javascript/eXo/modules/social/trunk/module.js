cb61ac7705f2ac67537b7881b7530b92fa11cba7
tung_nguyen_dang <tung_nguyen_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2008-08-28 10:55:03 +0000

    create template portal
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@19241 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/social/trunk/module.js
@@ -0,0 +1,44 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+
+  var module = new Module();
+
+  module.version = "trunk" ;
+  module.relativeMavenRepo =  "org/exoplatform/social" ;
+  module.relativeSRCRepo =  "social/trunk" ;
+  module.name = "social" ;  
+	
+	module.component = {} ;
+	module.component.core = 
+		new Project("org.exoplatform.social", "exo.social.component.core","jar", module.version);
+	
+	module.component.community = 
+		new Project("org.exoplatform.social", "exo.social.component.community","jar", module.version);
+	
+	module.web = {} ;
+  module.web.portal = 
+    new Project("org.exoplatform.social", "exo.social.web.portal", "exo-portal", module.version).
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web);
+	
+	module.web.eXoResources = new Project("org.exoplatform.social", "exo.social.web.socialResources", "war", module.version);
+  module.web.eXoResources.deployName = "eXoResourcesSocial" ;
+	
+	module.server = {}
+  module.server.tomcat = {}
+  module.server.tomcat.patch = 
+    new Project("org.exoplatform.social", "exo.social.server.tomcat.patch", "jar", module.version);
+	
+	module.portlet = {}
+  module.portlet.web = new Project("org.exoplatform.social", "exo.social.portlet.web", "exo-portlet", module.version);
+  module.portlet.web.deployName = "socialPortletWeb" ;
+	
+	return module;
+}
---
+++ build/src/main/javascript/eXo/products/social/trunk.js
@@ -0,0 +1,49 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "social" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "social/trunk" ;
+  product.useWorkflow = false;
+  product.serverPluginVersion = "trunk" ;
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/trunk") ;
+  var ws = Module.GetModule("ws/trunk");
+  var core = Module.GetModule("core/trunk") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/trunk", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/trunk") ;
+  var portal = Module.GetModule("portal/trunk", {kernel : kernel, core : core, ws:ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var social = Module.GetModule("social/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var webos = Module.GetModule("webos/trunk", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
+  
+  product.addDependencies(social.web.portal) ;
+  product.addDependencies(social.web.eXoResources) ;
+  //product.addDependencies(social.component.core) ;
+  product.addDependencies(social.component.community) ;
+  product.addDependencies(social.portlet.web) ;
+    
+  product.addDependencies(portal.web.rest) ;
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.portlet.dashboard) ;
+  product.addDependencies(portal.eXoWidget.web) ;
+	product.addDependencies(portal.eXoGadgetServer) ;
+	product.addDependencies(portal.eXoGadgets) ;
+  product.addDependencies(portal.webui.portal);
+  
+	product.addDependencies(portal.web.eXoResources);
+	product.addDependencies(webos.web.webosResources);
+	product.addDependencies(portal.web.eXoMacSkin);
+	product.addDependencies(portal.web.eXoVistaSkin);
+  
+  product.addServerPatch("tomcat", social.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+	product.module = social ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal];
+    
+  return product ;
+}
