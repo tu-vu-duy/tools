e29682961dc02d8f1cd6becd2b3a63c69cb0c9a8
nicolas <nicolas@42d3c691-033b-0410-95b4-af486b8189e5>
2008-09-02 14:22:06 +0000

    Migration from ECM 2.0.2 to ECM 2.1 Integration of the ecm.portlet.ecm of ECM 2.1 into the company project, the resulting module is company.portlet.ecm Integration of the exo.ecm.component.cms of ECM 2.1 into the company project, the resulting module is company.component.cms
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@19365 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/company/trunk/module.js
+++ build/src/main/javascript/eXo/modules/company/trunk/module.js
@@ -23,7 +23,7 @@ function getModule(params) {
 
   module.portlet.ecm = 
     new Project("org.exoplatform.company", "company.portlet.ecm", "exo-portlet", module.version).
-    addDependency(new Project("org.exoplatform.ecm", "exo.ecm.component.cms", "jar",  ecm.version)) .      
+    addDependency(new Project("org.exoplatform.company", "company.component.cms", "jar",  module.version)) .      
     addDependency(new Project("org.exoplatform.ecm", "exo.ecm.component.publication", "jar", ecm.version)).
     addDependency(new Project("rome", "rome", "jar", "0.8")) .
     addDependency(new Project("com.totsp.feedpod", "itunes-com-podcast", "jar", "0.2")) .
@@ -31,6 +31,7 @@ function getModule(params) {
     addDependency(new Project("jdom", "jdom", "jar", "1.0")).
     addDependency(new Project("org.apache.ws.commons", "ws-commons-util", "jar", "1.0.1")).
     addDependency(new Project("com.sun.xml.stream", "sjsxp", "jar", "1.0")) ;
+//    addDependency(new Project("javax.xml.stream", "stax-api", "jar", "1.0")) ;    
   
   module.component={}
   module.component.web=
--- build/src/main/javascript/eXo/products/company/trunk.js
+++ build/src/main/javascript/eXo/products/company/trunk.js
@@ -2,35 +2,37 @@ eXo.require("eXo.projects.Module") ;
 eXo.require("eXo.projects.Product") ;
 
 function getProduct(version) {
-
   var product = new Product();
   
   product.name = "company" ;
   product.portalwar = "portal.war" ;
   product.codeRepo = "company/trunk" ;
   product.useWorkflow = true ;
-  product.workflowVersion = "2.0.2" ;
-  product.serverPluginVersion = "2.1.1" ;
+  product.workflowVersion = "2.1" ;
+  product.serverPluginVersion = "2.2" ;
 
   var tool =  Module.GetModule("tools/trunk") ;
-  var kernel = Module.GetModule("kernel/tags/2.0.1") ;
-  var ws = Module.GetModule("ws/tags/1.1.2");
-  var core = Module.GetModule("core/tags/2.0.2") ;
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0", {kernel : kernel, core : core}) ;
-  var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
-  var portal = Module.GetModule("portal/tags/2.1.1", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
-  var ecm = Module.GetModule("ecm/tags/2.0.2", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var kernel = Module.GetModule("kernel/tags/2.0.2") ;
+  var core = Module.GetModule("core/tags/2.1") ;
+  var ws = Module.GetModule("ws/tags/1.2.1");
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.1", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.9.1") ;
+  var portal = Module.GetModule("portal/tags/2.2", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var ecm = Module.GetModule("ecm/tags/2.1", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
   var company = Module.GetModule("company/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ecm : ecm});
 
-  product.addDependencies(company.component.web) ;
-  product.addDependencies(company.portlet.web) ;
-  product.addDependencies(company.web.portal) ;
-  product.addDependencies(company.web.companyResources) ;    
   product.addDependencies(portal.portlet.exoadmin) ;
   product.addDependencies(portal.portlet.web) ;
-  product.addDependencies(portal.eXoWidget.web) ;
+  product.addDependencies(portal.eXoWidget.web) ;  
+
+  product.addDependencies(ecm.web.rest) ;  
   product.addDependencies(company.portlet.ecm) ;
-  product.addDependencies(ecm.portlet.workflow) ;    
+  product.addDependencies(ecm.portlet.workflow) ;
+  
+  product.addDependencies(company.component.web) ;
+  product.addDependencies(company.web.portal) ;
+  product.addDependencies(company.web.companyResources) ;
+  product.addDependencies(company.portlet.web) ;
 
   product.addServerPatch("tomcat", company.server.tomcat.patch) ;
   product.addServerPatch("jboss",  portal.server.jboss.patch) ;
