01f16e4a6da5118eee1290c6235c6350248b4d2c
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-09-05 03:01:49 +0000

    add cp050908 modules descriptor
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@19517 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/cp050908/trunk/module.js
@@ -0,0 +1,53 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+  var ecm = params.ecm ;
+//  var cs = params.cs ;
+//  var ws = params.ws;
+  var module = new Module();
+
+  module.version = "trunk" ;
+  module.relativeMavenRepo =  "org/exoplatform/cp050908" ;
+  module.relativeSRCRepo =  "cp050908/trunk" ;
+  module.name =  "cp050908" ;
+       
+  module.portlet = {}
+  module.portlet.web = new Project("org.exoplatform.cp050908", "cp050908.portlet.web", "exo-portlet", module.version);
+  module.portlet.web.deployName = "cp050908PortletWeb" ;
+	
+  module.web = {} ;
+  module.web.eXoResources = new Project("org.exoplatform.cp050908", "cp050908.web.cp050908Resources", "war", module.version);
+  module.web.eXoResources.deployName = "eXoResourcesCp050908" ;
+  
+  module.component = {} ;
+  module.component.web=
+    new Project("org.exoplatform.cp050908", "cp050908.component.web", "jar", module.version).
+    addDependency(portal.component.web) ;
+      
+  module.server = {}
+  module.server.tomcat = {}
+  module.server.tomcat.patch = 
+    new Project("org.exoplatform.cp050908", "cp050908.server.tomcat.patch", "jar", module.version);
+
+  module.web = {}
+  module.web.portal = 
+    new Project("org.exoplatform.cp050908", "exo.cp050908.web.portal", "exo-portal", module.version).   
+    addDependency(portal.web.eXoResources) .
+    addDependency(portal.web.eXoMacSkin) .
+    addDependency(portal.web.eXoVistaSkin) .
+    addDependency(portal.webui.portal) .
+//    addDependency(cs.web.csResources) .
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web).
+    addDependency(ecm.web.rest);
+    
+
+  return module;
+}
