2ee42ae609c586129fb8423a313c9e6c43251bfa
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-09-09 03:33:35 +0000

    update components portal 2.2 and ecm 2.1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@19678 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cp050908/trunk.js
+++ build/src/main/javascript/eXo/products/cp050908/trunk.js
@@ -8,21 +8,17 @@ function getProduct(version) {
   product.portalwar = "portal.war" ;
   product.codeRepo = "cp050908/trunk" ;
   product.useWorkflow = true;
-  product.workflowVersion = "2.0.2" ;
-  product.serverPluginVersion = "2.1.1" ;
+  product.workflowVersion = "2.1" ;
+  product.serverPluginVersion = "2.2" ;
   
   var tool =  Module.GetModule("tools/trunk") ;
-  var kernel = Module.GetModule("kernel/tags/2.0.1") ;
-  var ws = Module.GetModule("ws/tags/1.1.2");
-  var core = Module.GetModule("core/tags/2.0.2") ;
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0", {kernel : kernel, core : core}) ;
-  var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
-  var portal = Module.GetModule("portal/tags/2.1.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
-  var ecm = Module.GetModule("ecm/tags/2.0.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
-//  var wcm = Module.GetModule("wcm/branches/0.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal, ecm : ecm});  
-//  var cs = Module.GetModule("cs/branches/1.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
-//  var ks = Module.GetModule("ks/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal}); 
-//  var liveroom = Module.GetModule("liveroom/branches/1.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal}) ;
+  var kernel = Module.GetModule("kernel/tags/2.0.2") ;
+  var ws = Module.GetModule("ws/tags/1.2.1");
+  var core = Module.GetModule("core/tags/2.1") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.1", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.9.1") ;
+  var portal = Module.GetModule("portal/tags/2.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, ws : ws });  
+  var ecm = Module.GetModule("ecm/tags/2.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
   var cp050908 = Module.GetModule("cp050908/trunk", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ecm : ecm});
     
   product.addDependencies(portal.portlet.exoadmin) ;
@@ -33,33 +29,15 @@ function getProduct(version) {
   product.addDependencies(ecm.web.rest) ;
   product.addDependencies(ecm.portlet.ecm) ;
   product.addDependencies(ecm.portlet.workflow) ;
-            
-//  product.addDependencies(cs.eXoApplication.mail) ;
-//  product.addDependencies(cs.eXoApplication.calendar) ;
-//  product.addDependencies(cs.eXoApplication.contact) ;
-//  product.addDependencies(cs.eXoApplication.content) ;
-//  product.addDependencies(cs.web.webservice) ;
-//  product.addDependencies(cs.web.csResources) ;
-//
-//  product.addDependencies(ks.eXoApplication.forum) ;
-//  product.addDependencies(ks.eXoApplication.faq) ;
-//  product.addDependencies(ks.eXoApplication.wiki) ;
-//  product.addDependencies(ks.eXoApplication.blog) ;  
-//
-//  product.addDependencies(liveroom.eXoApplication.chat.webapp) ;
-//
-//  product.addDependencies(wcm.portlet.webpresentation);
-//  product.addDependencies(wcm.portlet.websearches);
-//  product.addDependencies(wcm.web.wcmResources) ;
 
   product.addDependencies(cp050908.web.portal) ;
 
-  product.addServerPatch("tomcat", ecm.server.tomcat.patch) ;
+  product.addServerPatch("tomcat", cp050908.server.tomcat.patch) ;
   product.addServerPatch("jboss",  portal.server.jboss.patch) ;
   product.addServerPatch("jonas",  portal.server.jonas.patch) ;
 
   product.module = cp050908 ;
-  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, ecm/*, cs, ks, liveroom, wcm*/];
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, ecm];
   
   return product;
 }
