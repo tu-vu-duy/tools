5a1f9c5adaf3f4043d937f2d83e69f72c3dc17a2
patrice_lamarque <patrice_lamarque@42d3c691-033b-0410-95b4-af486b8189e5>
2008-09-15 13:07:35 +0000

    fixed prope.war build
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@19930 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/cp050908/trunk/module.js
+++ build/src/main/javascript/eXo/modules/cp050908/trunk/module.js
@@ -9,8 +9,6 @@ function getModule(params) {
   var jcr = params.eXoJcr;
   var portal = params.portal;
   var ecm = params.ecm ;
-//  var cs = params.cs ;
-//  var ws = params.ws;
   var module = new Module();
 
   module.version = "trunk" ;
@@ -23,57 +21,10 @@ function getModule(params) {
   module.portlet.web.deployName = "cp050908PortletWeb" ;
   
   module.portlet.prope = new Project("org.exoplatform.cp050908", "cp050908.portlet.prope", "exo-portlet", module.version).
-  addDependency(new Project("org.springframework", "spring-web", "jar", "2.5.3")).
-  addDependency(new Project("org.jboss.portletbridge", "portletbridge-api", "jar", "1.0.0.B4")).
-  addDependency(new Project("org.jboss.portletbridge", "portletbridge-impl", "jar", "1.0.0.B4")).
-  addDependency(new Project("org.richfaces.framework", "richfaces-api", "jar", "3.2.2.GA")).
-  addDependency(new Project("org.richfaces.ui", "richfaces-ui", "jar", "3.2.2.GA")).
-  addDependency(new Project("org.richfaces.framework", "richfaces-impl", "jar", "3.2.2.GA")). 
-  addDependency(new Project("com.sun.facelets", "jsf-facelets", "jar", "1.1.14"));
-  module.portlet.prope.deployName = "prope" ;
-  /*  
-  <socle.version>2.1.0</socle.version>
-  <richfaces.version></richfaces.version>
-  <portletbridge.version>1.0.0.B4</portletbridge.version>
+  addDependency(new Project("javax.faces", "jsf-api", "jar", "1.2_04-p02")).
+  addDependency(new Project("javax.faces", "jsf-impl", "jar", "1.2_04-p02")).
+  addDependency(new Project("javax.servlet", "jstl", "jar", "1.2"));
   
-
-
-  <!-- Provided -->
-  <dependency>
-    <groupId>javax.portlet</groupId>
-    <artifactId>portlet-api</artifactId>
-    <version>2.0</version>
-    <scope>provided</scope>
-  </dependency>
-
-  <dependency>
-    <groupId>javax.el</groupId>
-    <artifactId>el-api</artifactId>
-    <version>1.2</version>
-    <scope>provided</scope>
-  </dependency>
-
-  <dependency>
-    <groupId>javax.servlet</groupId>
-    <artifactId>jstl</artifactId>
-    <version>1.2</version>
-    <scope>provided</scope>
-  </dependency>
-
-  <dependency>
-    <groupId>javax.faces</groupId>
-    <artifactId>jsf-api</artifactId>
-    <version>1.2_04-p02</version>
-    <scope>provided</scope>
-  </dependency>
-
-  <dependency>
-    <groupId>javax.faces</groupId>
-    <artifactId>jsf-impl</artifactId>
-    <version>1.2_04-p02</version>
-    <scope>provided</scope>
-  </dependency>
-	*/
   module.web = {} ;
   module.web.eXoResources = new Project("org.exoplatform.cp050908", "cp050908.web.cp050908Resources", "war", module.version);
   module.web.eXoResources.deployName = "eXoResourcesCp050908" ;
@@ -94,7 +45,7 @@ function getModule(params) {
     addDependency(portal.web.eXoMacSkin) .
     addDependency(portal.web.eXoVistaSkin) .
     addDependency(portal.webui.portal) .
-//    addDependency(cs.web.csResources) .
+
     addDependency(jcr.frameworks.command) .
     addDependency(jcr.frameworks.web).
     addDependency(ecm.web.rest);
