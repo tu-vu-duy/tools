901aed17ea23c4ff01df5c22e6cc2271094f624a
tran <tran@42d3c691-033b-0410-95b4-af486b8189e5>
2008-10-02 08:11:39 +0000

    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@20714 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/portal/2.2.1.js
+++ build/src/main/javascript/eXo/products/portal/2.2.1.js
@@ -11,10 +11,10 @@ function getProduct(version) {
   
   var tool = Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.2") ;
-  var core = Module.GetModule("core/tags/2.1") ;
+  var core = Module.GetModule("core/tags/2.1.1") ;
   var ws = Module.GetModule("ws/tags/1.2.1");
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.1", {kernel : kernel, core : core}) ;
-  var eXoJcr = Module.GetModule("jcr/tags/1.9.1") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.2", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.9.3") ;
   var portal = Module.GetModule("portal/branches/2.2.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
 
   product.addDependencies(portal.web.portal) ;
