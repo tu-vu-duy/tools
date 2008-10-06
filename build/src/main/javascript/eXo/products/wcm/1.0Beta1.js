28412406a3fd74d700d64429c6b16cecb5f06caf
hoa_pham <hoa_pham@42d3c691-033b-0410-95b4-af486b8189e5>
2008-10-06 09:35:35 +0000

    WCM-246 add 1.0Beta1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@20915 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/wcm/1.0Beta1.js
@@ -0,0 +1,52 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "eXoWCM" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "wcm/branches/1.0Beta1/" ;
+  product.useWorkflow = true;
+  product.workflowVersion = "trunk" ;
+  product.serverPluginVersion = "trunk" ;
+    
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/trunk") ;
+  var ws = Module.GetModule("ws/trunk");
+  var core = Module.GetModule("core/trunk") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/trunk", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/trunk") ;
+  var portal = Module.GetModule("portal/trunk", {kernel : kernel, core : core, ws:ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var ecm = Module.GetModule("ecm/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr ,ws : ws, portal : portal});
+  var wcm = Module.GetModule("wcm/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal,ecm : ecm});
+    
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.portlet.dashboard) ;
+  product.addDependencies(portal.eXoWidget.web) ;
+	product.addDependencies(portal.eXoGadgetServer) ;
+	product.addDependencies(portal.eXoGadgets) ;
+	product.addDependencies(portal.web.rest);
+	
+	product.addDependencies(ecm.portlet.ecm);
+  product.addDependencies(ecm.portlet.workflow);   
+  		 
+  product.addDependencies(wcm.portlet.iweb);
+  product.addDependencies(wcm.portlet.webpresentation);
+  product.addDependencies(wcm.portlet.websearches); 
+  product.addDependencies(wcm.web.eXoWCMResources) ;
+  product.addDependencies(ecm.web.eXoECMResources) ;
+  product.addDependencies(wcm.web.wcmportal) ;        
+  
+  product.addServerPatch("tomcat", ecm.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jbossear",  portal.server.jbossear.patch) ;  
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+  product.addServerPatch("ear",  portal.server.websphere.patch) ;
+
+  product.module = wcm ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, ecm];
+    
+  return product ;
+}
