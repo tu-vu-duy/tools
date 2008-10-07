9d143e1dd102c5877821b35e527dad7a2a574e51
jeremi_joslin <jeremi_joslin@42d3c691-033b-0410-95b4-af486b8189e5>
2008-10-07 07:11:16 +0000

    remove not needed dependencies and upgrade some dependencies
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@20989 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/social/trunk/module.js
+++ build/src/main/javascript/eXo/modules/social/trunk/module.js
@@ -21,6 +21,9 @@ function getModule(params) {
 	
 	module.component.space = 
 		new Project("org.exoplatform.social", "exo.social.component.space","jar", module.version);
+
+	module.component.opensocial = 
+		new Project("org.exoplatform.social", "exo.social.component.opensocial","jar", module.version);
 	
 	module.web = {} ;
   module.web.portal = 
@@ -39,6 +42,40 @@ function getModule(params) {
 	module.portlet = {}
   module.portlet.web = new Project("org.exoplatform.social", "exo.social.portlet.web", "exo-portlet", module.version);
   module.portlet.web.deployName = "socialPortletWeb" ;
+
+  module.portlet.profile = new Project("org.exoplatform.social", "exo.social.portlet.profile", "exo-portlet", module.version);
+  module.portlet.profile.deployName = "profile" ;
+
+
+  module.web.opensocial =new Project("org.exoplatform.social", "exo.social.web.opensocial", "war", module.version).
+		addDependency(new Project("commons-betwixt", "commons-betwixt", "jar", "0.8")).
+		addDependency(new Project("net.sf.json-lib", "json-lib", "jar", "2.2")).
+		addDependency(new Project("org.apache.geronimo.specs", "geronimo-stax-api_1.0_spec", "jar", "1.0.1"));
+
+  module.web.opensocial.deployName = "social" ;
+/*  module.gadgets = {}
+  module.gadgets.server = new Project("org.exoplatform.social", "exo.social.gadgets-server", "war", module.version).
+		addDependency(new Project("commons-betwixt", "commons-betwixt", "jar", "0.8")).
+		addDependency(new Project("commons-digester", "commons-digester", "jar", "1.7")).
+		addDependency(new Project("commons-io", "commons-io", "jar", "1.4")).
+		addDependency(new Project("com.google.code.google-collections", "google-collect", "jar", "snapshot-20080321")).
+		addDependency(new Project("com.google.code.guice", "guice", "jar", "1.0")).
+		addDependency(new Project("net.oauth", "core", "jar", "20080328")).
+		addDependency(new Project("net.sf.json-lib", "json-lib", "jar", "2.2")).
+		addDependency(new Project("nu.validator.htmlparser", "htmlparser", "jar", "1.0.5")).
+		addDependency(new Project("jaxen", "jaxen", "jar", "1.1.1")).
+		addDependency(new Project("joda-time", "joda-time", "jar", "1.5.2")).
+		addDependency(new Project("org.json", "json", "jar", "20070829")).
+		addDependency(new Project("org.apache.geronimo.specs", "geronimo-stax-api_1.0_spec", "jar", "1.0.1")).
+		addDependency(new Project("org.apache.shindig", "shindig-common", "jar", "1-SNAPSHOT")).
+		addDependency(new Project("org.apache.shindig", "shindig-social-api", "jar", "1-SNAPSHOT")).
+		addDependency(new Project("org.exoplatform.portal", "exo.portal.gadgets-core", "jar", "trunk")).
+		addDependency(new Project("org.apache.shindig", "shindig-features", "jar", "1-SNAPSHOT")).
+		addDependency(new Project("org.codehaus.woodstox", "wstx-asl", "jar", "3.2.1")).
+		addDependency(new Project("org.hamcrest", "hamcrest-all", "jar", "1.1")).
+		addDependency(new Project("com.ibm.icu", "icu4j", "jar", "3.8")).
+		addDependency(new Project("caja", "caja", "jar", "r820"));
+  module.gadgets.server.deployName = "eXoGadgetServer" ;*/
 	
 	return module;
 }
--- build/src/main/javascript/eXo/products/social/trunk.js
+++ build/src/main/javascript/eXo/products/social/trunk.js
@@ -8,7 +8,7 @@ function getProduct(version) {
   product.portalwar = "portal.war" ;
   product.codeRepo = "social/trunk" ;
   product.useWorkflow = false;
-  product.serverPluginVersion = "trunk" ;
+  product.serverPluginVersion = "2.5-SNAPSHOT" ;
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/trunk") ;
   var ws = Module.GetModule("ws/trunk");
@@ -24,25 +24,51 @@ function getProduct(version) {
   product.addDependencies(social.component.people) ;
   product.addDependencies(social.component.space) ;
   product.addDependencies(social.portlet.web) ;
+  product.addDependencies(social.portlet.profile);
+  product.addDependencies(social.web.opensocial);
+  product.addDependencies(social.component.opensocial);
     
   product.addDependencies(portal.web.rest) ;
   product.addDependencies(portal.portlet.exoadmin) ;
   product.addDependencies(portal.portlet.web) ;
   product.addDependencies(portal.portlet.dashboard) ;
   product.addDependencies(portal.eXoWidget.web) ;
-	product.addDependencies(portal.eXoGadgetServer) ;
-	product.addDependencies(portal.eXoGadgets) ;
+  product.addDependencies(portal.eXoGadgetServer) ;
+  product.addDependencies(portal.eXoGadgets) ;
   product.addDependencies(portal.webui.portal);
   
-	product.addDependencies(portal.web.eXoResources);
-	product.addDependencies(webos.web.webosResources);
-	product.addDependencies(portal.web.eXoMacSkin);
-	product.addDependencies(portal.web.eXoVistaSkin);
+  product.addDependencies(portal.web.eXoResources);
+  product.addDependencies(webos.web.webosResources);
+  product.addDependencies(portal.web.eXoMacSkin);
+  product.addDependencies(portal.web.eXoVistaSkin);
+
+  product.removeDependency(eXoPortletContainer.web.wsrp);
+  product.removeDependency(new Project("org.exoplatform.jcr", "exo.jcr.component.ftp", "jar", eXoJcr.version));
+
+
+
+  //upgrade lib for opensocial 
+  product.removeDependency(new Project("commons-beanutils", "commons-beanutils", "jar", "1.6"));
+  product.removeDependency(new Project("commons-collections", "commons-collections", "jar", "2.1"));
+  product.removeDependency(new Project("commons-collections", "commons-collections", "jar", "3.1"));
+//  product.removeDependency(new Project("commons-digester", "commons-digester", "jar", "1.7")):
+  product.removeDependency(new Project("commons-httpclient", "commons-httpclient", "jar", "3.0"));
+//  product.removeDependency(new Project("commons-logging", "commons-logging-api", "jar", "1.0.4"));
+
+  product.addDependencies(new Project("commons-beanutils", "commons-beanutils", "jar", "1.7.0"));
+  product.addDependencies(new Project("commons-beanutils", "commons-beanutils-core", "jar", "1.7.0"));
+  product.addDependencies(new Project("commons-collections", "commons-collections", "jar", "3.2.1"));
+//  product.addDependencies(new Project("commons-digester", "commons-digester", "jar", "1.7")):
+  product.addDependencies(new Project("commons-httpclient", "commons-httpclient", "jar", "3.1"));
+//  product.addDependencies(new Project("commons-logging", "commons-logging-api", "jar", "1.1.1"));
+
+
+  // product.removeDependency(portal.eXoGadgetServer);
   
   product.addServerPatch("tomcat", social.server.tomcat.patch) ;
   product.addServerPatch("jboss",  portal.server.jboss.patch) ;
   product.addServerPatch("jonas",  portal.server.jonas.patch) ;
-	product.module = social ;
+  product.module = social ;
   product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal];
     
   return product ;
