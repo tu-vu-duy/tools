31c56d41539c53ba202548e97641ca5da6dc534a
tuan_pham <tuan_pham@42d3c691-033b-0410-95b4-af486b8189e5>
2008-10-13 01:56:34 +0000

    using portal tags 2.2.1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@21230 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cs/1.1-SNAPSHOT.js
+++ build/src/main/javascript/eXo/products/cs/1.1-SNAPSHOT.js
@@ -11,11 +11,11 @@ function getProduct(version) {
 
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.2") ;
-  var ws = Module.GetModule("ws/tags/1.2");
+  var ws = Module.GetModule("ws/tags/1.2.1");
   var core = Module.GetModule("core/tags/2.1.1") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.2", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.9.3") ;
-  var portal = Module.GetModule("portal/branches/2.2.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
+  var portal = Module.GetModule("portal/tags/2.2.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
   var cs = Module.GetModule("cs/branches/1.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
 
   product.addDependencies(portal.portlet.exoadmin) ;
