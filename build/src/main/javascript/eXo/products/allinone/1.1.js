dd99428cb4562e66a97f489aa27626224f374798
nhatbun <nhatbun@42d3c691-033b-0410-95b4-af486b8189e5>
2008-10-22 10:11:46 +0000

    Create javascript file for allinone version 1.1 in tools/products
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@21776 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/allinone/1.1.js
@@ -0,0 +1,51 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+
+  product.name = "eXoAllInOneProduct" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "allinone/branches/1.1" ;
+  product.useWorkflow = true;
+  product.workflowVersion = "2.1.2" ;
+  product.serverPluginVersion = "2.2.1" ;
+  
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.2") ;
+  var ws = Module.GetModule("ws/tags/1.2.1");
+  var core = Module.GetModule("core/tags/2.1.1") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.2", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.9.3") ;
+  var portal = Module.GetModule("portal/tags/2.2.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, ws : ws });  
+  var ecm = Module.GetModule("ecm/tags/2.1.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  
+  var cs = Module.GetModule("cs/tags/1.1Beta1", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var allinone = Module.GetModule("allinone/branches/1.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, ws :ws, eXoJcr : eXoJcr, portal : portal, cs: cs, ecm : ecm});
+    
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  //product.addDependencies(portal.eXoWidget.web) ;
+  
+  product.addDependencies(ecm.web.rest) ;
+  product.addDependencies(ecm.portlet.ecm) ;
+  product.addDependencies(ecm.portlet.workflow) ;
+            
+  product.addDependencies(cs.eXoApplication.mail) ;
+  product.addDependencies(cs.eXoApplication.calendar) ;
+  product.addDependencies(cs.eXoApplication.contact) ;
+  product.addDependencies(cs.eXoApplication.content) ;
+  product.addDependencies(cs.web.webservice) ;
+  product.addDependencies(cs.web.csResources) ;
+
+  product.addDependencies(allinone.web.allinoneportal) ;
+
+  product.addServerPatch("tomcat", ecm.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+
+  product.module = allinone ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, ecm, cs];
+  
+  return product;
+}
