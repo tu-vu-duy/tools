93157646c6547d4712bb6fb4b64c2fe344d9d31f
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-10-23 07:58:59 +0000

    use embedded portal ecm
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@21822 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/geneve/trunk.js
+++ build/src/main/javascript/eXo/products/geneve/trunk.js
@@ -8,8 +8,8 @@ function getProduct(version) {
   product.portalwar = "portal.war" ;
   product.codeRepo = "geneve/website_poc/trunk" ;
   product.useWorkflow = true;
-  product.workflowVersion = "2.0" ;
-  product.serverPluginVersion = "2.0" ;
+  product.workflowVersion = "2.0.4" ;
+  product.serverPluginVersion = "2.1.2" ;
 
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.1") ;
@@ -17,8 +17,8 @@ function getProduct(version) {
   var core = Module.GetModule("core/tags/2.0.2") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0rc5", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.8.2") ;
-  var portal = Module.GetModule("portal/tags/2.1.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
-  var ecm = Module.GetModule("ecm/tags/2.0.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var portal = Module.GetModule("geneve/portal/2.1.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var ecm = Module.GetModule("geneve/ecm/2.0.4", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
   var geneve = Module.GetModule("geneve/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ecm : ecm});
     
   product.addDependencies(portal.portlet.exoadmin) ;
