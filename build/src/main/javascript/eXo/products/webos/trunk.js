1c62f24e73870f4e52af01acf5961ae14a7dcf7c
tan_pham_dinh <tan_pham_dinh@42d3c691-033b-0410-95b4-af486b8189e5>
2008-11-10 07:55:38 +0000

    make webos trunk run correctly
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@22786 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/webos/trunk.js
+++ build/src/main/javascript/eXo/products/webos/trunk.js
@@ -11,11 +11,11 @@ function getProduct(version) {
   product.serverPluginVersion = "2.5-SNAPSHOT" ;
     
   var tool =  Module.GetModule("tools/trunk") ;
-  var kernel = Module.GetModule("kernel/tags/2.0.3") ;
-  var ws = Module.GetModule("ws/tags/1.3");
-  var core = Module.GetModule("core/tags/2.1.2") ;
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.3", {kernel : kernel, core : core}) ;
-  var eXoJcr = Module.GetModule("jcr/tags/1.10") ;
+  var kernel = Module.GetModule("kernel/trunk") ;
+  var ws = Module.GetModule("ws/trunk");
+  var core = Module.GetModule("core/trunk") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/trunk", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/trunk") ;
   var portal = Module.GetModule("portal/trunk", {kernel : kernel, core : core, ws:ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var webos = Module.GetModule("webos/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   
