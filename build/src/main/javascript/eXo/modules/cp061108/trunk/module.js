c5c25d9418904fee9f8b2a866a98e8aae770492e
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-11-11 07:45:43 +0000

    add js descriptors for new product
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@22862 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/cp061108/trunk/module.js
@@ -0,0 +1,41 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+
+  var module = new Module();
+
+  module.version = "trunk" ;
+  module.relativeMavenRepo =  "org/exoplatform/cp061108" ;
+  module.relativeSRCRepo =  "cp061108/project/trunk" ;
+  module.name =  "cp061108" ;  
+
+	module.portlet = {}
+  module.portlet.web = new Project("org.exoplatform.cp061108", "cp061108.portlet.web", "exo-portlet", module.version);
+  module.portlet.web.deployName = "cp061108PortletWeb" ;
+	
+	module.web = {} ;
+	module.web.eXoResources = new Project("org.exoplatform.cp061108", "cp061108.web.cp061108Resources", "war", module.version);
+  module.web.eXoResources.deployName = "eXoResourcesCp061108" ;
+
+  module.server = {}
+  module.server.tomcat = {}
+  module.server.tomcat.patch = 
+    new Project("org.exoplatform.cp061108", "cp061108.server.tomcat.patch", "jar", module.version);
+
+  
+  module.web.portal = 
+    new Project("org.exoplatform.cp061108", "cp061108.web.portal", "exo-portal", module.version).
+    addDependency(portal.web.eXoResources) .
+    addDependency(portal.webui.portal) .
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web) ;
+
+  return module;
+}
---
+++ build/src/main/javascript/eXo/products/cp061108/trunk.js
@@ -0,0 +1,54 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "cp061108" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "cp061108/project/trunk" ;
+  product.useWorkflow = true;
+  product.workflowVersion = "2.0.3" ;
+  product.serverPluginVersion = "2.1" ;
+    
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.1") ;
+  var ws = Module.GetModule("ws/tags/1.1");
+  var core = Module.GetModule("core/tags/2.0.2") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
+  // Before changing portal version, please notify Romain !!
+  var portal = Module.GetModule("portal/tags/2.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var ecm = Module.GetModule("ecm/tags/2.0.3", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var cs = Module.GetModule("cs/tags/1.0.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ws : ws});
+  var cp061108 = Module.GetModule("cp061108/trunk", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+    
+  product.addDependencies(cp061108.web.portal) ; 
+  product.addDependencies(cp061108.web.eXoResources) ;
+  product.addDependencies(cp061108.portlet.web) ;
+  
+  product.addDependencies(cs.eXoApplication.calendar) ;
+  product.addDependencies(cs.eXoApplication.mail) ;
+  product.addDependencies(cs.eXoApplication.content) ;
+  product.addDependencies(cs.web.csResources) ;
+  
+  product.addDependencies(ecm.web.rest) ;
+  product.addDependencies(ecm.portlet.ecm) ;
+  product.addDependencies(ecm.portlet.workflow) ;
+  
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.web.eXoResources) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.eXoWidget.web) ;
+  product.addDependencies(portal.web.eXoMacSkin) ;
+  product.addDependencies(portal.web.eXoVistaSkin) ;
+
+  product.addServerPatch("tomcat", cp061108.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+
+  product.module = cp061108 ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, ecm, cs];
+    
+  return product ;
+}
