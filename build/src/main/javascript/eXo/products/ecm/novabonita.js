4b48a9a6b3d5f74ab8d7985c4b2de1c0bccf8fc1
Patrick <Patrick@42d3c691-033b-0410-95b4-af486b8189e5>
2008-11-25 00:40:24 +0000

    new ECM branche: novabonita for bonita 4 integration.
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@23724 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/ecm/novabonita.js
@@ -0,0 +1,44 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "eXoECM" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "ecm/branches/novabonita" ;
+  product.useWorkflow = true;
+  product.workflowVersion = "trunk" ;
+  product.serverPluginVersion = "2.5rc1" ;
+    
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.4") ;
+  var core = Module.GetModule("core/tags/2.1.2") ;
+  var ws = Module.GetModule("ws/tags/1.3");
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.3", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.10") ;
+  var portal = Module.GetModule("portal/tags/2.5rc1", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var workflow = Module.GetModule("workflow/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var ecm = Module.GetModule("ecm//branches/novabonita", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+    
+  product.addDependencies(ecm.web.ecmportal) ;
+  product.addDependencies(ecm.web.rest) ;
+  product.addDependencies(ecm.web.eXoECMResources) ;
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(ecm.portlet.ecm) ;
+  product.addDependencies(workflow.portlet.workflow) ;
+    
+  //product.addDependencies(portal.eXoWidget.web) ;
+  
+  product.addServerPatch("tomcat", ecm.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jbossear",  portal.server.jbossear.patch) ;  
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+  product.addServerPatch("ear",  portal.server.websphere.patch) ;
+
+  product.module = ecm ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, workflow];
+
+  return product ;
+}
