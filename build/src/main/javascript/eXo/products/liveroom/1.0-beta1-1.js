a738874698b24e64964810b2e5a3abbc9e73b6e7
uoc_nguyen <uoc_nguyen@42d3c691-033b-0410-95b4-af486b8189e5>
2008-11-27 03:44:35 +0000

    Add the liveroom 1.0-beta1-1 with some bug fixes for SPFF
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@23932 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/liveroom/1.0-beta1-1.js
@@ -0,0 +1,43 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+eXo.require("eXo.core.TaskDescriptor") ;
+eXo.require("eXo.core.IOUtil") ;
+
+function getProduct(version) {
+
+  var product = new Product();
+  product.name = "liveroom" ;
+  product.portalwar = "portal.war" ;
+
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.4") ;
+  var ws = Module.GetModule("ws/tags/1.3.1");
+  var core = Module.GetModule("core/tags/2.1.3") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.2", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/branches/1.9.3.1") ;
+  var portal = Module.GetModule("portal/tags/2.2.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
+  var liveroom = Module.GetModule("liveroom/branches/1.0-beta1-1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal}) ;
+
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.eXoWidget.web) ;
+  product.addDependencies(portal.web.rest) ;
+  
+  product.addDependencies(liveroom.eXoApplication.chat.webapp) ;
+  //product.addDependencies(liveroom.eXoApplication.videoconf.webapp) ;
+  //product.addDependencies(liveroom.eXoApplication.whiteboard.webapp) ;
+  product.addDependencies(liveroom.web.liveroomportal) ;
+  product.addDependencies(liveroom.web.webservice) ;
+  
+
+  product.addServerPatch("tomcat",liveroom.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+    
+  product.codeRepo = "liveroom/branches/1.0-beta1-1" ;
+
+  product.module = liveroom ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal ];
+  
+  return product ;
+}
