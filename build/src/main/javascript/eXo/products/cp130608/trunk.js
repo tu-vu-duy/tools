f0ba04ddc60e48c6c4419eb231a6774b8502f876
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2008-11-27 10:44:13 +0000

    update after release of portal and ecm
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@23972 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cp130608/trunk.js
+++ build/src/main/javascript/eXo/products/cp130608/trunk.js
@@ -16,13 +16,10 @@ function getProduct(version) {
   var ws = Module.GetModule("ws/tags/1.1.2");
   var core = Module.GetModule("core/tags/2.0.2") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0", {kernel : kernel, core : core}) ;
-  var eXoJcr = Module.GetModule("jcr/tags/1.8.3") ;
-  var portal = Module.GetModule("portal/branches/2.1.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
-  var ecm = Module.GetModule("ecm/branches/2.0.4", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var eXoJcr = Module.GetModule("jcr/tags/1.8.3.1") ;
+  var portal = Module.GetModule("portal/tags/2.1.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
+  var ecm = Module.GetModule("ecm/tags/2.0.4", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
   var wcm = Module.GetModule("wcm/branches/0.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal, ecm : ecm});  
-//  var cs = Module.GetModule("cs/branches/1.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
-//  var ks = Module.GetModule("ks/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal}); 
-//  var liveroom = Module.GetModule("liveroom/branches/1.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal}) ;
   var cp130608 = Module.GetModule("cp130608/trunk", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ecm : ecm});
     
   product.addDependencies(portal.portlet.exoadmin) ;
@@ -34,23 +31,8 @@ function getProduct(version) {
   product.addDependencies(ecm.portlet.ecm) ;
   product.addDependencies(ecm.portlet.workflow) ;
             
-//  product.addDependencies(cs.eXoApplication.mail) ;
-//  product.addDependencies(cs.eXoApplication.calendar) ;
-//  product.addDependencies(cs.eXoApplication.contact) ;
-//  product.addDependencies(cs.eXoApplication.content) ;
-//  product.addDependencies(cs.web.webservice) ;
-//  product.addDependencies(cs.web.csResources) ;
-//
-//  product.addDependencies(ks.eXoApplication.forum) ;
-//  product.addDependencies(ks.eXoApplication.faq) ;
-//  product.addDependencies(ks.eXoApplication.wiki) ;
-//  product.addDependencies(ks.eXoApplication.blog) ;  
-//
-//  product.addDependencies(liveroom.eXoApplication.chat.webapp) ;
-//
   product.addDependencies(wcm.portlet.webpresentation);
   product.addDependencies(wcm.portlet.websearches);
-//  product.addDependencies(wcm.web.wcmResources) ;
 
   product.addDependencies(cp130608.web.portal) ;
   product.addDependencies(cp130608.portlet.web) ;
