0a0fa6f570cb06b572e609384913b8affddc8210
hung_nguyen_quang <hung_nguyen_quang@42d3c691-033b-0410-95b4-af486b8189e5>
2008-11-28 02:13:40 +0000

    make build for rc4-1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@24003 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/ks/tags/rc4-1/module.js
@@ -0,0 +1,53 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+  var ws = params.ws;
+  var module = new Module();
+
+  module.version =  "1.0-RC4-1" ;
+  module.relativeMavenRepo =  "org/exoplatform/ks" ;
+  module.relativeSRCRepo =  "ks/tags/rc4-1" ;
+  module.name = "ks" ;
+    
+  module.eXoApplication = {};
+    
+
+  module.eXoApplication.faq = 
+    new Project("org.exoplatform.ks", "exo.ks.eXoApplication.faq.webapp", "war", module.version).
+      addDependency(new Project("org.exoplatform.ks", "exo.ks.eXoApplication.faq.service", "jar",  module.version));
+  module.eXoApplication.faq.deployName = "faq";
+
+
+  module.eXoApplication.forum = 
+    new Project("org.exoplatform.ks", "exo.ks.eXoApplication.forum.webapp", "war", module.version).
+      addDependency(new Project("org.exoplatform.ks", "exo.ks.eXoApplication.forum.service", "jar",  module.version).
+      addDependency(new Project("org.exoplatform.ks", "exo.ks.component.providers", "jar", module.version)));
+  module.eXoApplication.forum.deployName = "forum";
+  
+/*
+  module.eXoApplication.content = 
+    new Project("org.exoplatform.ks", "exo.ks.eXoApplication.content.webapp", "war", module.version).
+      addDependency(new Project("org.exoplatform.ks", "exo.ks.eXoApplication.content.service", "jar",  module.version));
+  module.eXoApplication.content.deployName = "content";
+  */	  
+  module.web = {}
+  module.web.ksResources = 
+    new Project("org.exoplatform.ks", "exo.ks.web.ksResources", "war", module.version) ;
+  module.web.ksportal = 
+    new Project("org.exoplatform.ks", "exo.ks.web.portal", "exo-portal", module.version).
+      addDependency(portal.web.eXoResources) .
+      addDependency(portal.web.eXoMacSkin) .
+      addDependency(portal.web.eXoVistaSkin) .
+	  addDependency(portal.webui.portal) .
+      addDependency(jcr.frameworks.command) .
+      addDependency(jcr.frameworks.web) ;
+      
+  return module;
+}
---
+++ build/src/main/javascript/eXo/products/ks/1.0-RC4-1.js
@@ -0,0 +1,43 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+
+  var product = new Product();
+  product.name = "eXoKS" ;
+  product.portalwar = "portal.war" ;
+  
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.2") ;
+  var ws = Module.GetModule("ws/tags/1.2.1");
+  var core = Module.GetModule("core/tags/2.1.1") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.2", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.9.3") ;
+  var portal = Module.GetModule("portal/tags/2.2.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
+  var ks = Module.GetModule("ks/tags/rc4-1", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.eXoWidget.web) ;
+            
+  product.addDependencies(ks.eXoApplication.forum) ;
+  product.addDependencies(ks.eXoApplication.faq) ;
+  //product.addDependencies(ks.eXoApplication.blog) ;
+  //product.addDependencies(ks.eXoApplication.wiki) ;
+
+  product.addDependencies(ks.web.ksportal) ;
+  
+  product.addDependencies(ks.web.ksResources) ;
+  //product.addDependencies(cs.eXoApplication.contact) ;
+  
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+    
+  product.codeRepo = "ks/tags/rc4-1" ;
+
+  product.module = ks ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal];
+  
+  return product ;
+}
