6097aaa2e4d1e954a52964b64c8f96dd905546e2
minh_dang <minh_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2008-12-01 12:17:52 +0000

    Switch to use portal 2.5rc2
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@24130 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/ecm/trunk.js
+++ build/src/main/javascript/eXo/products/ecm/trunk.js
@@ -9,15 +9,15 @@ function getProduct(version) {
   product.codeRepo = "ecm/trunk" ;
   product.useWorkflow = true;
   product.workflowVersion = "2.2-SNAPSHOT" ;
-  product.serverPluginVersion = "2.5rc1" ;
+  product.serverPluginVersion = "2.5rc2" ;
     
   var tool =  Module.GetModule("tools/trunk") ;
-  var kernel = Module.GetModule("kernel/tags/2.0.4") ;
-  var core = Module.GetModule("core/tags/2.1.2") ;
-  var ws = Module.GetModule("ws/tags/1.3");
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.3", {kernel : kernel, core : core}) ;
-  var eXoJcr = Module.GetModule("jcr/tags/1.10") ;
-  var portal = Module.GetModule("portal/tags/2.5rc1", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var kernel = Module.GetModule("kernel/tags/2.0.5") ;
+  var core = Module.GetModule("core/tags/2.1.3") ;
+  var ws = Module.GetModule("ws/tags/1.3.1");
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.4", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.1", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal/tags/2.5rc2", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var ecm = Module.GetModule("ecm/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
     
   product.addDependencies(portal.web.rest) ;
@@ -25,6 +25,9 @@ function getProduct(version) {
   product.addDependencies(ecm.web.eXoECMResources) ;
   product.addDependencies(portal.portlet.exoadmin) ;
   product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.portlet.dashboard) ;
+	product.addDependencies(portal.eXoGadgetServer) ;
+	product.addDependencies(portal.eXoGadgets) ;  
   product.addDependencies(ecm.portlet.ecm) ;
   product.addDependencies(ecm.portlet.workflow) ;
     
