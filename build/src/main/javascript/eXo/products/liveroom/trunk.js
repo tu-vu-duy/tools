4bbb32a3fb25743ea27ec00ca9fd8a0fd7c560d6
uoc_nguyen <uoc_nguyen@42d3c691-033b-0410-95b4-af486b8189e5>
2008-12-03 09:06:26 +0000

    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@24273 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/liveroom/trunk.js
+++ build/src/main/javascript/eXo/products/liveroom/trunk.js
@@ -12,7 +12,7 @@ function getProduct(version) {
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.4") ;
   var ws = Module.GetModule("ws/tags/1.3.1");
-  var core = Module.GetModule("core/tags/2.1") ;
+  var core = Module.GetModule("core/tags/2.1.3") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.2", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.9.3.1", {ws : ws}) ;
   var portal = Module.GetModule("portal/tags/2.2.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
