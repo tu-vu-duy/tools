aaea66a2a2016748e84cbcf946db96358d9f3d5f
minh_dang <minh_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2008-12-08 08:47:20 +0000

    ECM-2938 make the build tool for 2.2DEV - test with ws 2.0
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@24567 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/ecm/2.2DEV.js
@@ -0,0 +1,44 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "eXoECM" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "ecm/branches-dev/ws-2.0-test" ;
+  product.useWorkflow = true;
+  product.workflowVersion = "2.2DEV" ;
+  product.serverPluginVersion = "2.5rc2" ;
+    
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/trunk") ;
+  var core = Module.GetModule("core/trunk") ;
+  var ws = Module.GetModule("ws/branches/2.0");
+  var eXoPortletContainer = Module.GetModule("portlet-container/trunk", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/branches/1.11", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal/tags/2.5rc2", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var ecm = Module.GetModule("ecm/branches-dev/ws-2.0-test", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+    
+  product.addDependencies(portal.web.rest) ;
+  product.addDependencies(ecm.web.ecmportal) ;
+  product.addDependencies(ecm.web.eXoECMResources) ;
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.portlet.dashboard) ;
+	product.addDependencies(portal.eXoGadgetServer) ;
+	product.addDependencies(portal.eXoGadgets) ;  
+  product.addDependencies(ecm.portlet.ecm) ;
+  product.addDependencies(ecm.portlet.workflow) ;
+    
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jbossear",  portal.server.jbossear.patch) ;  
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+  product.addServerPatch("ear",  portal.server.websphere.patch) ;
+
+  product.module = ecm ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal];
+
+  return product ;
+}
