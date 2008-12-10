badb2067de1503b33fd4fbf33c0046b71d90e2d1
tung_nguyen_dang <tung_nguyen_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2008-12-10 08:55:24 +0000

    create tags 1.0-alpha1 for social
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@24720 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/social/tags/1.0-alpha1/module.js
@@ -0,0 +1,62 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+
+  var module = new Module();
+
+  module.version = "1.0-alpha1" ;
+  module.relativeMavenRepo =  "org/exoplatform/social" ;
+  module.relativeSRCRepo =  "social/tags/1.0-alpha1" ;
+  module.name = "social" ;  
+	
+	module.component = {} ;
+	module.component.people = 
+		new Project("org.exoplatform.social", "exo.social.component.people","jar", module.version);
+	
+	module.component.space = 
+		new Project("org.exoplatform.social", "exo.social.component.space","jar", module.version);
+
+	module.component.opensocial = 
+		new Project("org.exoplatform.social", "exo.social.component.opensocial","jar", module.version);
+	
+	module.web = {} ;
+  module.web.portal = 
+    new Project("org.exoplatform.social", "exo.social.web.portal", "exo-portal", module.version).
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web);
+	
+	module.web.eXoResources = new Project("org.exoplatform.social", "exo.social.web.socialResources", "war", module.version);
+  module.web.eXoResources.deployName = "eXoResourcesSocial" ;
+	
+	module.server = {}
+  module.server.tomcat = {}
+  module.server.tomcat.patch = 
+    new Project("org.exoplatform.social", "exo.social.server.tomcat.patch", "jar", module.version);
+	
+	module.portlet = {}
+  module.portlet.space = new Project("org.exoplatform.social", "exo.social.portlet.space", "exo-portlet", module.version);
+  module.portlet.space.deployName = "space" ;
+
+  module.portlet.profile = new Project("org.exoplatform.social", "exo.social.portlet.profile", "exo-portlet", module.version);
+  module.portlet.profile.deployName = "profile" ;
+
+
+  module.web.opensocial =new Project("org.exoplatform.social", "exo.social.web.opensocial", "war", module.version).
+		addDependency(new Project("commons-betwixt", "commons-betwixt", "jar", "0.8")).
+		addDependency(new Project("net.sf.json-lib", "json-lib", "jar", "2.2")).
+		addDependency(new Project("org.apache.shindig", "shindig-social-api", "jar", "0.8.1-1-r719456")).
+		addDependency(new Project("com.thoughtworks.xstream", "xstream", "jar", "1.2")).
+		addDependency(new Project("jdom", "jdom", "jar", "1.0")).
+		addDependency(new Project("org.apache.geronimo.specs", "geronimo-stax-api_1.0_spec", "jar", "1.0.1"));
+
+
+  module.web.opensocial.deployName = "social" ;
+	
+	return module;
+}
