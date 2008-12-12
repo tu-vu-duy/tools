27dc4664a772cec033191bd8fbcd3aa6720cb993
patrice_lamarque <patrice_lamarque@42d3c691-033b-0410-95b4-af486b8189e5>
2008-12-12 15:11:34 +0000

    cs 1.1 relies on comunity version of portal 2.2
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@24996 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cs/1.1-Beta3.js
+++ build/src/main/javascript/eXo/products/cs/1.1-Beta3.js
@@ -15,7 +15,7 @@ function getProduct(version) {
   var core = Module.GetModule("core/tags/2.1.1") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.2", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.9.3") ;
-  var portal = Module.GetModule("portal/tags/2.2.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
+  var portal = Module.GetModule("portal/tags/2.2", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
   var cs = Module.GetModule("cs/tags/1.1Beta3", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
 
   product.addDependencies(portal.portlet.exoadmin) ;
--- build/src/main/javascript/eXo/products/cs/1.1-Beta4.js
+++ build/src/main/javascript/eXo/products/cs/1.1-Beta4.js
@@ -7,7 +7,7 @@ function getProduct(version) {
   product.name = "eXoCS" ;
   product.portalwar = "portal.war" ;
   product.codeRepo = "cs/tags/1.1Beta4" ;
-  product.serverPluginVersion = "2.2.1" ;
+  product.serverPluginVersion = "2.2" ;
 
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.2") ;
@@ -15,7 +15,7 @@ function getProduct(version) {
   var core = Module.GetModule("core/tags/2.1.1") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.2", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.9.3") ;
-  var portal = Module.GetModule("portal/tags/2.2.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
+  var portal = Module.GetModule("portal/tags/2.2", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
   var cs = Module.GetModule("cs/tags/1.1Beta4", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
 
   product.addDependencies(portal.portlet.exoadmin) ;
