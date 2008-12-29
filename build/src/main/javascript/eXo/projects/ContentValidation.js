bd74d49ba33ba94130fc69764ed39cd67dc135b7
chien_nguyen_van <chien_nguyen_van@42d3c691-033b-0410-95b4-af486b8189e5>
2008-12-29 07:41:31 +0000

    ECM-3000 Split ecm to 3 folders
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@25945 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/projects/ContentValidation.js
+++ build/src/main/javascript/eXo/projects/ContentValidation.js
@@ -9,7 +9,8 @@ function ContentValidation(contentValidationName, contentValidationVersion) {
 
 ContentValidation.prototype.configContentValidation = function(product) {  	
 	eXo.System.info("INFO", "I AM IN CONTENT VALIDATION : ");
-	product.addDependencies(new Project("org.exoplatform.ecm", "exo.ecm.validation-request", "jar", this.version));
+	product.addDependencies(new Project("org.exoplatform.ecm.validation-request", "exo.ecm.validation-request.component.cms", "jar", this.version));
+  product.addDependencies(new Project("org.exoplatform.ecm.validation-request", "exo.ecm.validation-request.component.publication", "jar", this.version));
 	if(this.name == "jbpm") {	                                        		
 		product.addDependencies(new Project("org.exoplatform.ecm.workflow.bp", "exo.ecm.workflow.bp.jbpm.content.backup", "jar", this.version));
 		product.addDependencies(new Project("org.exoplatform.ecm.workflow.bp", "exo.ecm.workflow.bp.jbpm.content.publishing", "jar", this.version));
