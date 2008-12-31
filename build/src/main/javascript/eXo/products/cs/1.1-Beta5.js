8e7bb2a174ef2aaedb06d5793eefd23aae1d7180
tuan_pham <tuan_pham@42d3c691-033b-0410-95b4-af486b8189e5>
2008-12-31 10:11:24 +0000

    resolve CS-2058 make js
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@26176 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/cs/1.1-Beta5.js
@@ -0,0 +1,43 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+
+  var product = new Product();
+  product.name = "eXoCS" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "cs/tags/1.1-Beta5" ;
+  product.serverPluginVersion = "2.2.1" ;
+
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.2") ;
+  var ws = Module.GetModule("ws/tags/1.2.1");
+  var core = Module.GetModule("core/tags/2.1.1") ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.2", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.9.3") ;
+  var portal = Module.GetModule("portal/tags/2.2.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
+  var cs = Module.GetModule("cs/tags/1.1Beta5", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.eXoWidget.web) ;
+            
+  product.addDependencies(cs.eXoApplication.mail) ;
+  product.addDependencies(cs.eXoApplication.calendar) ;
+  product.addDependencies(cs.eXoApplication.contact) ;
+  product.addDependencies(cs.eXoApplication.content) ;
+  
+  product.addDependencies(cs.web.webservice) ;
+  product.addDependencies(cs.web.csResources) ;
+  product.addDependencies(cs.web.csportal) ;
+  product.addDependencies(cs.web.rest) ;
+  
+  product.addServerPatch("tomcat", cs.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+
+  product.module = cs ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal ];
+    
+  return product ;
+}
