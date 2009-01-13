772057e0b6e50a58d4be971abb33b3ba05b9cd8b
chien_nguyen_van <chien_nguyen_van@42d3c691-033b-0410-95b4-af486b8189e5>
2009-01-13 10:22:59 +0000

    ECM-2983 In Add categories multiple fields, allow to have a garbage icon for the first item
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@26707 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/workflow/trunk.js
+++ build/src/main/javascript/eXo/products/workflow/trunk.js
@@ -9,15 +9,15 @@ function getProduct(version) {
   product.codeRepo = "ecm/workflow/trunk" ;
   product.useWorkflow = true;
   product.workflowVersion = "1.0-SNAPSHOT" ;
-  product.serverPluginVersion = "2.5" ;
+  product.serverPluginVersion = "2.5.1" ;
     
   var tool =  Module.GetModule("tools/trunk") ;
-  var kernel = Module.GetModule("kernel/tags/2.0.6") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.5") ;
   var core = Module.GetModule("core/tags/2.1.3") ;
-  var ws = Module.GetModule("ws/tags/1.3.1");
+  var ws = Module.GetModule("ws/tags/1.3.1", {kernel : kernel, core : core});
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.4", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.10.1", {kernel : kernel, core : core, ws : ws}) ;
-  var portal = Module.GetModule("portal/tags/2.5", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var portal = Module.GetModule("portal/tags/2.5.1", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var workflow = Module.GetModule("ecm/workflow/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
     
   product.addDependencies(portal.web.rest);
