d1062ac5398a7f19f413c12a3982dcfc2feca060
chuong_phan <chuong_phan@42d3c691-033b-0410-95b4-af486b8189e5>
2009-01-16 03:54:05 +0000

    WCM-733: [DEV] Move WCM to the new ECM structure
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@26973 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/ecm/wcm/branches/0.1/module.js
@@ -0,0 +1,48 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+  var ecm = params.ecm;
+  
+  var module = new Module();
+
+  module.version = "0.1" ;
+  module.relativeMavenRepo =  "org/exoplatform/wcm" ;
+  module.relativeSRCRepo =  "wcm/branches/0.1" ;
+  module.name =  "wcm" ;
+  
+  module.portlet = {};
+  
+  module.portlet.webpresentation = 
+    new Project("org.exoplatform.wcm", "exo.wcm.portlet.web-presentation", "exo-portlet", module.version).    
+    addDependency(new Project("org.exoplatform.wcm", "exo.wcm.webui.wcm", "jar",  module.version)).
+    addDependency(new Project("org.exoplatform.wcm", "exo.wcm.connector.fckeditor", "jar",  module.version)).
+    addDependency(new Project("org.exoplatform.wcm", "exo.wcm.component.wcm", "jar",  module.version));
+    
+ module.portlet.websearches = 
+    new Project("org.exoplatform.wcm", "exo.wcm.portlet.web-searches", "exo-portlet", module.version).
+    addDependency(new Project("org.exoplatform.wcm", "exo.wcm.webui.wcm", "jar",  module.version)).
+    addDependency(new Project("org.exoplatform.wcm", "exo.wcm.component.wcm", "jar",  module.version)).
+    addDependency(new Project("org.exoplatform.wcm", "exo.wcm.component.search", "jar",  module.version));
+      
+  module.web = {};
+  module.web.wcmResources = 
+    new Project("org.exoplatform.wcm", "exo.wcm.web.wcmResources", "war", module.version);
+    
+  module.web.wcmportal = 
+    new Project("org.exoplatform.wcm", "exo.wcm.web.portal", "exo-portal", module.version).        
+    addDependency(portal.web.eXoResources) .
+    addDependency(portal.web.eXoMacSkin) .
+    addDependency(portal.web.eXoVistaSkin) .
+    addDependency(portal.webui.portal) .
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web) ;
+
+  return module;
+}
