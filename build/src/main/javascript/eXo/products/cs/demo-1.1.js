25225119a43f38a802e1091866d6600d4bcc9bc0
patrice_lamarque <patrice_lamarque@42d3c691-033b-0410-95b4-af486b8189e5>
2009-01-19 18:27:21 +0000

    CS-1052 : upgraded CS demo to 1.1 RC1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@27203 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cs/demo-1.1.js
+++ build/src/main/javascript/eXo/products/cs/demo-1.1.js
@@ -6,17 +6,17 @@ function getProduct(version) {
   var product = new Product();
   product.name = "eXoCS" ;
   product.portalwar = "portal.war" ;
-  product.codeRepo = "cs/tags/1.1Beta4" ;
-  product.serverPluginVersion = "2.2.1" ;
+  product.codeRepo = "cs/tags/1.1RC1" ;
+  product.serverPluginVersion = "2.2.2" ;
 
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.2") ;
-  var ws = Module.GetModule("ws/tags/1.2.1");
   var core = Module.GetModule("core/tags/2.1.1") ;
+  var ws = Module.GetModule("ws/tags/1.2.1");
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.2", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.9.3") ;
-  var portal = Module.GetModule("portal/tags/2.2.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
-  var cs = Module.GetModule("cs/tags/1.1Beta4", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var portal = Module.GetModule("portal/tags/2.2.2", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
+  var cs = Module.GetModule("cs/tags/1.1RC1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
 
   // portal applications
   product.addDependencies(portal.portlet.exoadmin) ;
@@ -29,13 +29,14 @@ function getProduct(version) {
   product.addDependencies(cs.eXoApplication.contact) ;
   product.addDependencies(cs.eXoApplication.content) ;
   product.addDependencies(cs.web.webservice) ;
-  product.addDependencies(cs.web.csResources) ;
+  product.addDependencies(cs.web.csResources) ;  
+  product.addDependencies(cs.web.rest) ;  
   
   // demo portal war
   cs.web.csdemo.deployName = "portal"; // required, otherwise the Project.js takes the artifact name suffix 
   product.addDependencies(cs.web.csdemo) ;
   
-  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("tomcat", cs.server.tomcat.patch) ;
   //product.addServerPatch("jboss",  portal.server.jboss.patch) ;
   //product.addServerPatch("jonas",  portal.server.jonas.patch) ;
 
