cdc7ecde7384e30f46cc6ef3f2100551386df214
tuan_pham <tuan_pham@42d3c691-033b-0410-95b4-af486b8189e5>
2009-01-20 08:12:02 +0000

    tool for cs-2185 revert portal 2.2 version for cs
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@27237 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cs/1.1-SNAPSHOT.js
+++ build/src/main/javascript/eXo/products/cs/1.1-SNAPSHOT.js
@@ -7,7 +7,7 @@ function getProduct(version) {
   product.name = "eXoCS" ;
   product.portalwar = "portal.war" ;
   product.codeRepo = "cs/branches/1.1" ;
-  product.serverPluginVersion = "2.2.2" ;
+  product.serverPluginVersion = "2.2" ;
 
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.2") ;
@@ -15,7 +15,7 @@ function getProduct(version) {
   var ws = Module.GetModule("ws/tags/1.2.1");
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.2", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.9.3") ;
-  var portal = Module.GetModule("portal/tags/2.2.2", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
+  var portal = Module.GetModule("portal/tags/2.2", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });  
   var cs = Module.GetModule("cs/branches/1.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
 
   product.addDependencies(portal.portlet.exoadmin) ;
