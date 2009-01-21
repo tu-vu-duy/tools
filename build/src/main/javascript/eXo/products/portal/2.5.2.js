47897e3b03482d8c0ce46d254f4344e9663661c4
tam_nguyen <tam_nguyen@42d3c691-033b-0410-95b4-af486b8189e5>
2009-01-21 02:41:24 +0000

    make branches 2.5.2
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@27269 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/portal/2.5.2.js
+++ build/src/main/javascript/eXo/products/portal/2.5.2.js
@@ -15,7 +15,7 @@ function getProduct(version) {
   var ws = Module.GetModule("ws/tags/1.3.1", {kernel : kernel, core : core});
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.5", {kernel : kernel, core : core}) ;    
   var eXoJcr = Module.GetModule("jcr/tags/1.10.1", {kernel : kernel, core : core, ws : ws}) ;
-  var portal = Module.GetModule("portal/trunk", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
+  var portal = Module.GetModule("portal/branches/2.5.2", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
 
   product.addDependencies(portal.web.rest) ;
   product.addDependencies(portal.portlet.exoadmin) ;
