666f194db92a249545f64b0d97af3faec7a22bb9
thuy_le <thuy_le@42d3c691-033b-0410-95b4-af486b8189e5>
2009-02-02 07:32:18 +0000

    add dependency to migrate portal 2.5.1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@27706 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/cp040608/branches/2.0/module.js
+++ build/src/main/javascript/eXo/modules/cp040608/branches/2.0/module.js
@@ -15,8 +15,22 @@ function getModule(params) {
   module.relativeMavenRepo =  "org/exoplatform/cp040608" ;
   module.relativeSRCRepo =  "cp040608/branches/2.0" ;
   module.name =  "cp040608" ;
+  
+  module.portlet = {} ;
+  module.portlet.web = new Project("org.exoplatform.cp040608", "cp040608.portlet.web", "exo-portlet", module.version).
+  		addDependency(new Project("org.exoplatform.cp040608", "cp040608.component.rest", "jar",  module.version)).
+  		addDependency(new Project("org.exoplatform.cp040608", "cp040608.component.services", "jar",  module.version));
+  module.portlet.web.deployName = "cp040608PortletWeb" ;
+  
+  /*module.component = {} ;
+  module.component.web=
+    new Project("org.exoplatform.cp040608", "cp040608.component.web", "jar", module.version).
+    addDependency(portal.component.web) ;
+  */
+  module.web = {} ;
+  module.web.eXoResources = new Project("org.exoplatform.cp040608", "cp040608.web.cp040608Resources", "war", module.version);
+  module.web.eXoResources.deployName = "eXoResourcesCp040608" ;
        
-  module.web = {}
   module.web.portal = 
     new Project("org.exoplatform.cp040608", "exo.cp040608.web.portal", "exo-portal", module.version).
     addDependency(portal.web.eXoResources) .
