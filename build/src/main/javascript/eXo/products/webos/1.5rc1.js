5577c368adb4e2aca459a3fc56237fa3f32582fb
tam_nguyen <tam_nguyen@42d3c691-033b-0410-95b4-af486b8189e5>
2009-02-04 04:09:44 +0000

    make tag for webos 1.5rc1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@27827 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/webos/1.5rc1.js
@@ -0,0 +1,45 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "eXoWebOS" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "webos/tags/1.5rc1" ;
+  product.useWorkflow = false;
+  product.serverPluginVersion = "2.5.2rc1" ;
+    
+  var tool = Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.5") ;
+  var core = Module.GetModule("core/tags/2.1.3") ;
+  var ws = Module.GetModule("ws/tags/1.3.1");
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.5", {kernel : kernel, core : core}) ;    
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.1", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal/tags/2.5.2rc1", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
+  var webos = Module.GetModule("webos/tags/1.5rc1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  
+  product.addDependencies(portal.web.rest) ;
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.portlet.dashboard) ;
+	product.addDependencies(portal.eXoGadgetServer) ;
+	product.addDependencies(portal.eXoGadgets) ;
+  product.addDependencies(portal.webui.portal);
+  
+	product.addDependencies(portal.web.eXoResources);
+	product.addDependencies(portal.web.eXoMacSkin);
+	product.addDependencies(portal.web.eXoVistaSkin);
+	product.addDependencies(webos.web.webosResources);
+	
+  product.addDependencies(webos.web.webosportal) ;
+  
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+
+  product.module = webos ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal];
+    
+  return product ;
+}
