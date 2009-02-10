007694eba5e13006df048c9365930ec253431d5f
tuan_pham <tuan_pham@42d3c691-033b-0410-95b4-af486b8189e5>
2009-02-10 06:39:19 +0000

    resolve CS-2300 complete change cs 1.2 to use portal 2.5
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@28107 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cs/trunk.js
+++ build/src/main/javascript/eXo/products/cs/trunk.js
@@ -7,15 +7,15 @@ function getProduct(version) {
   product.name = "eXoCS" ;
   product.portalwar = "portal.war" ;
   product.codeRepo = "cs/trunk" ;
-  product.serverPluginVersion = "2.5rc1" ;
+  product.serverPluginVersion = "2.5" ;
 
   var tool =  Module.GetModule("tools/trunk") ;
-  var kernel = Module.GetModule("kernel/tags/2.0.4") ;
-  var core = Module.GetModule("core/tags/2.1.2") ;
-  var ws = Module.GetModule("ws/tags/1.3");
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.3", {kernel : kernel, core : core}) ;
-  var eXoJcr = Module.GetModule("jcr/tags/1.10") ;
-  var portal = Module.GetModule("portal/tags/2.5rc1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr }); 
+  var kernel = Module.GetModule("kernel/tags/2.0.5") ;
+  var core = Module.GetModule("core/tags/2.1.3") ;
+  var ws = Module.GetModule("ws/tags/1.3.1", {kernel : kernel, core : core});
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.4", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.1", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal/tags/2.5", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr }); 
   var cs = Module.GetModule("cs/trunk", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
 
   product.addDependencies(cs.eXoApplication.mail) ;
