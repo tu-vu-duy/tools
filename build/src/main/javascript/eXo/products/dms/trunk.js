05271257ed99a86cb5ba1d57c0e0a0a3f4ffc305
thuy_nguyen_ngoc <thuy_nguyen_ngoc@42d3c691-033b-0410-95b4-af486b8189e5>
2009-02-17 04:35:01 +0000

    DMS user portal 2.5.2
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@28446 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/dms/trunk.js
+++ build/src/main/javascript/eXo/products/dms/trunk.js
@@ -10,7 +10,7 @@ function getProduct(version) {
   product.useContentvalidation = true;
   product.contentvalidationVersion = "1.0-SNAPSHOT";
   product.workflowVersion = "1.0-SNAPSHOT";
-  product.serverPluginVersion = "2.5" ;
+  product.serverPluginVersion = "2.5.2" ;
     
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.6") ;
@@ -18,7 +18,7 @@ function getProduct(version) {
   var ws = Module.GetModule("ws/tags/1.3.2");
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.4", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.10.2", {kernel : kernel, core : core, ws : ws}) ;
-  var portal = Module.GetModule("portal/tags/2.5", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var portal = Module.GetModule("portal/tags/2.5.2", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var dms = Module.GetModule("ecm/dms/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
     
   product.addDependencies(portal.web.rest);
