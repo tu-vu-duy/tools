0fa4bc0a709a3fd760304dbfe729c7171f8ea0bf
romain_denarie <romain_denarie@42d3c691-033b-0410-95b4-af486b8189e5>
2009-02-24 09:44:25 +0000

    Add new SearchIndex for CG38 to index contents without accentued chars
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@28731 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cg38/trunk.js
+++ build/src/main/javascript/eXo/products/cg38/trunk.js
@@ -28,6 +28,7 @@ function getProduct(version) {
   product.addDependencies(cg38.portlet.web) ;
   //product.addDependencies(cg38.component.jcraction);
   product.addDependencies(cg38.component.cg38CategoriesService);
+  product.addDependencies(cg38.component.cg38Indexation);
   
   product.addDependencies(portal.portlet.exoadmin) ;
   product.addDependencies(portal.portlet.web) ;
