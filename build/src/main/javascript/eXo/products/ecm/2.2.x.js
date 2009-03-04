69cf88ea53cc00d71df63684580cbc33deffae14
minh_dang <minh_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2009-03-04 04:32:20 +0000

    ECM-3336 leverage to use portal 2.5.1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@29114 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/ecm/2.2.x.js
+++ build/src/main/javascript/eXo/products/ecm/2.2.x.js
@@ -9,7 +9,7 @@ function getProduct(version) {
   product.codeRepo = "ecm/dms/branches/2.2.x" ;
   product.useWorkflow = true;
   product.workflowVersion = "2.2.x" ;
-  product.serverPluginVersion = "2.5" ;
+  product.serverPluginVersion = "2.5.1" ;
     
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.6") ;
@@ -17,7 +17,7 @@ function getProduct(version) {
   var ws = Module.GetModule("ws/tags/1.3.2");
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.4", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.10.2", {kernel : kernel, core : core, ws : ws}) ;
-  var portal = Module.GetModule("portal/tags/2.5", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var portal = Module.GetModule("portal/tags/2.5.1", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var ecm = Module.GetModule("ecm/dms/branches/2.2.x", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
     
   product.addDependencies(portal.web.rest) ;
