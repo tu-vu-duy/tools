7fa536144db1c4cdf4dc5ed6c90b04c96721f456
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2009-03-09 05:25:17 +0000

    add new version of core (custom patch) configure cp040608 to use it
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@29290 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cp040608/2.0.js
+++ build/src/main/javascript/eXo/products/cp040608/2.0.js
@@ -13,7 +13,7 @@ function getProduct(version) {
   
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.5") ;
-  var core = Module.GetModule("core/tags/2.1.3") ;
+  var core = Module.GetModule("core/tags/2.1.3.1") ;
   var ws = Module.GetModule("ws/tags/1.3.1");
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.4", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.10.1", {kernel : kernel, core : core, ws : ws}) ;
