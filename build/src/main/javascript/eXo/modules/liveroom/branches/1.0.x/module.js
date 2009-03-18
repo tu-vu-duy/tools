592fdcf7585d28fdd77d6e169e6640bf1ef48017
uoc_nguyen <uoc_nguyen@42d3c691-033b-0410-95b4-af486b8189e5>
2009-03-18 07:20:49 +0000

    Update WS to 1.3.3 for Liveroom 1.0.x
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@29754 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/liveroom/branches/1.0.x/module.js
+++ build/src/main/javascript/eXo/modules/liveroom/branches/1.0.x/module.js
@@ -24,7 +24,7 @@ function getModule(params) {
   		addDependency(new Project("org.exoplatform.liveroom", "exo.liveroom.eXoApplication.organization.webapp", "war", module.version)).
       addDependency(new Project("org.exoplatform.liveroom", "exo.liveroom.eXoApplication.organization.client.openfire", "jar", module.version)).
   		addDependency(new Project("org.exoplatform.liveroom", "exo.liveroom.eXoApplication.organization.webapp", "war", module.version)).
-  		addDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", "1.3.1")).
+  		addDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", "1.3.3")).
   		addDependency(ws.frameworks.cometd).
   		addDependency(new Project("org.exoplatform.portal", "exo.portal.web.rest", "war", "2.2.1")).
   		addDependency(new Project("jabber.smack", "smack", "jar", "3.0.4")).
--- build/src/main/javascript/eXo/products/liveroom/1.0.x.js
+++ build/src/main/javascript/eXo/products/liveroom/1.0.x.js
@@ -11,7 +11,7 @@ function getProduct(version) {
 
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.4") ;
-  var ws = Module.GetModule("ws/tags/1.3.1");
+  var ws = Module.GetModule("ws/branches/1.3.3");
   var core = Module.GetModule("core/tags/2.1.3") ;
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.2", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.9.3.1", {ws : ws}) ;
