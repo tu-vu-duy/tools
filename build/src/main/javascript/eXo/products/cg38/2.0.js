6c2653c2a0e319739b8d19f6c4b8483d71d41b5a
romain_denarie <romain_denarie@42d3c691-033b-0410-95b4-af486b8189e5>
2009-03-19 16:39:50 +0000

    Update CS version to 1.2-RC1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@29871 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cg38/2.0.js
+++ build/src/main/javascript/eXo/products/cg38/2.0.js
@@ -20,7 +20,7 @@ function getProduct(version) {
   var portal = Module.GetModule("portal/tags/2.5", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var ecm = Module.GetModule("ecm/dms/tags/2.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
   var wcm = Module.GetModule("ecm/wcm/tags/1.0", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ecm : ecm});
-  var cs = Module.GetModule("cs/trunk", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var cs = Module.GetModule("cs/tags/1.2RC1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
   var cg38 = Module.GetModule("cg38/branches/2.0", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ecm : ecm, wcm : wcm});
   
   product.addDependencies(portal.portlet.exoadmin) ;
