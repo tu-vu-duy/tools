6814453c4bf4121103a827dbdeb244998e92b5f3
thuy_le <thuy_le@42d3c691-033b-0410-95b4-af486b8189e5>
2009-03-27 03:05:04 +0000

    SC-28: Error javascript with the page title when i open the ECM Admin page ('Administration de l'ECM' in French)
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@30207 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cp040608/2.0.js
+++ build/src/main/javascript/eXo/products/cp040608/2.0.js
@@ -9,7 +9,7 @@ function getProduct(version) {
   product.codeRepo = "cp040608/branches/2.0" ;
   product.useWorkflow = true;
   product.workflowVersion = "2.2" ;
-  product.serverPluginVersion = "2.5.1" ;
+  product.serverPluginVersion = "2.5.2" ;
   
   var tool =  Module.GetModule("tools/trunk") ;
   var kernel = Module.GetModule("kernel/tags/2.0.5") ;
@@ -17,7 +17,7 @@ function getProduct(version) {
   var ws = Module.GetModule("ws/tags/1.3.1");
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.4", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.10.1", {kernel : kernel, core : core, ws : ws}) ;
-  var portal = Module.GetModule("portal/tags/2.5.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var portal = Module.GetModule("portal/tags/2.5.2", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var ecm = Module.GetModule("ecm/dms/tags/2.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
   
   var cp040608 = Module.GetModule("cp040608/branches/2.0", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ecm : ecm});
