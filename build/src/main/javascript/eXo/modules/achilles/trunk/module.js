f64dd41d416f81490f6ef23e6cc73fb58c4ad568
sorenschmidt <sorenschmidt@42d3c691-033b-0410-95b4-af486b8189e5>
2009-03-31 09:55:21 +0000

    ALL-41 new project achilles
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@30349 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/achilles/trunk/module.js
@@ -0,0 +1,32 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+/*  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;*/
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+  
+  var module = new Module();
+
+  module.version =  "1.0" ;
+  module.relativeMavenRepo =  "org/exoplatform/achilles" ;
+  module.relativeSRCRepo =  "achilles/trunk" ;
+  module.name = "achilles" ;
+
+  module.web = {};
+ 
+  module.web.portal = 
+    new Project("org.exoplatform.achilles", "achilles.web.portal", "exo-portal", module.version).
+    addDependency(portal.web.eXoResources) .
+    addDependency(portal.web.eXoMacSkin) .
+    addDependency(portal.web.eXoVistaSkin) .
+    addDependency(portal.webui.portal) .
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web);
+
+
+  return module;
+}
---
+++ build/src/main/javascript/eXo/products/achilles/trunk.js
@@ -0,0 +1,88 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "achilles" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "achilles/trunk" ;
+  product.serverPluginVersion = "2.5.2"
+  product.useContentvalidation = true;
+  product.contentvalidationVersion = "2.3";
+  product.workflowVersion = "1.0";
+
+
+
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.6") ;
+  var core = Module.GetModule("core/tags/2.1.4") ;
+  var ws = Module.GetModule("ws/tags/1.3.2");
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.5", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.2", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal/tags/2.5.2", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var dms = Module.GetModule("ecm/dms/tags/2.3", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var cs = Module.GetModule("cs/tags/1.2RC1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var achilles = Module.GetModule("achilles/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+
+
+/* PORTAL dependencies */
+
+  product.addDependencies(portal.web.rest) ;
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.portlet.dashboard) ;
+  product.addDependencies(portal.eXoGadgetServer) ;
+  product.addDependencies(portal.eXoGadgets) ;
+  product.addDependencies(portal.webui.portal);
+  
+  product.addDependencies(portal.web.eXoResources);
+  product.addDependencies(portal.web.eXoMacSkin);
+  product.addDependencies(portal.web.eXoVistaSkin);
+
+  /* product.addDependencies(portal.web.portal) ; */
+
+  /* DMS dependencies */
+
+  /* product.addDependencies(dms.web.dmsportal); */
+  
+  product.addDependencies(dms.web.eXoDMSResources);
+  product.addDependencies(dms.portlet.dms);
+  product.addDependencies(dms.portlet.jcr_console);
+  product.addDependencies(dms.gadgets);
+
+
+  /* CS dependencies */
+  
+  product.addDependencies(cs.eXoApplication.mail) ;
+  product.addDependencies(cs.eXoApplication.calendar) ;
+  product.addDependencies(cs.eXoApplication.contact) ;
+  product.addDependencies(cs.eXoApplication.content) ;
+
+  product.addDependencies(cs.web.webservice) ;
+  product.addDependencies(cs.web.csResources) ;
+ /* product.addDependencies(cs.web.csportal) ; */
+
+  /* custom (achilles) dependencies */
+  product.addDependencies(achilles.web.portal) ; 
+
+
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jbossear",  portal.server.jbossear.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+  product.addServerPatch("ear",  portal.server.websphere.patch) ;
+
+  /* cleanup duplicated lib (Portal) */
+  product.removeDependency(new Project("commons-httpclient", "commons-httpclient", "jar", "3.0"));
+  product.removeDependency(new Project("commons-collections", "commons-collections", "jar", "3.1"));
+
+  /* from CS */
+  product.removeDependency(new Project("javax.mail", "mail", "jar", "1.4"));
+
+  product.module = achilles ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, dms, cs];
+
+
+  return product ;
+}
