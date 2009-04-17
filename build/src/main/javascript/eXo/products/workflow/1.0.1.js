7e58402a8404aa4895d15e4f8a0bcdad1e67cf29
minh_dang <minh_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2009-04-17 07:08:43 +0000

    ECM-3568 update JS pom for workflow tag 1.0.1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@31049 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/workflow/1.0.1.js
@@ -0,0 +1,43 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "eXoWorkflow" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "ecm/workflow/tags/1.0.1" ;
+  product.useWorkflow = true;
+  product.workflowVersion = "1.0.1" ;
+  product.serverPluginVersion = "2.5.3" ;
+    
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.7") ;
+  var core = Module.GetModule("core/tags/2.1.5") ;
+  var ws = Module.GetModule("ws/tags/1.3.3");
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.6", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.3", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal/tags/2.5.3", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var workflow = Module.GetModule("ecm/workflow/tags/1.0.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+    
+  product.addDependencies(portal.web.rest);
+  product.addDependencies(workflow.web.portal);
+  product.addDependencies(workflow.web.eXoWorkflowResources);
+  product.addDependencies(portal.portlet.exoadmin);
+  product.addDependencies(portal.portlet.web);
+  product.addDependencies(portal.portlet.dashboard);
+	product.addDependencies(portal.eXoGadgetServer);
+	product.addDependencies(portal.eXoGadgets);  
+  product.addDependencies(workflow.portlet.workflow);
+    
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jbossear",  portal.server.jbossear.patch) ;  
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+  product.addServerPatch("ear",  portal.server.websphere.patch) ;
+
+  product.module = workflow ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal];
+
+  return product ;
+}
