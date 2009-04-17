2e550c502a8bbfd6598361ae41bb1eb7e712f10d
pnedonosko <pnedonosko@42d3c691-033b-0410-95b4-af486b8189e5>
2009-04-17 15:45:05 +0000

    xstream deps removed
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@31095 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/kernel/trunk/module.js
+++ build/src/main/javascript/eXo/modules/kernel/trunk/module.js
@@ -13,7 +13,7 @@ function getModule(params) {
     new Project("org.exoplatform.kernel", "exo.kernel.commons", "jar", module.version).
     addDependency(new Project("commons-lang", "commons-lang", "jar", "2.3")).
     addDependency(new Project("xpp3", "xpp3", "jar", "1.1.3.4.O")).
-    addDependency(new Project("xstream", "xstream", "jar", "1.2")).
+//    addDependency(new Project("xstream", "xstream", "jar", "1.2")).
     addDependency(new Project("dom4j", "dom4j", "jar", "1.6.1"));
     
   module.container = 
