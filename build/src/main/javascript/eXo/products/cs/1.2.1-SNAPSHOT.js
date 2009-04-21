655f036b318e153962a3db366d1917344cb221a8
tuan_pham <tuan_pham@42d3c691-033b-0410-95b4-af486b8189e5>
2009-04-21 02:14:30 +0000

    cs-2648 add webos to cs 1.2.1 tools
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@31134 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/cs/1.2.1-SNAPSHOT.js
+++ build/src/main/javascript/eXo/products/cs/1.2.1-SNAPSHOT.js
@@ -16,7 +16,7 @@ function getProduct(version) {
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.6", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.10.3", {kernel : kernel, core : core, ws : ws}) ;
   var portal = Module.GetModule("portal/tags/2.5.3", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
-  //var webos = Module.GetModule("webos/tags/1.5", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var webos = Module.GetModule("webos/tags/1.5", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var cs = Module.GetModule("cs/branches/1.2.x", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
 
   product.addDependencies(cs.eXoApplication.mail) ;
@@ -29,8 +29,8 @@ function getProduct(version) {
   product.addDependencies(cs.web.csResources) ;
   product.addDependencies(cs.web.csportal) ;
   
-  /*product.addDependencies(webos.web.webosResources);	
-  product.addDependencies(webos.web.webosportal) ;*/
+  product.addDependencies(webos.web.webosResources);	
+  //product.addDependencies(webos.web.webosportal) ;
   
   product.addServerPatch("tomcat", cs.server.tomcat.patch) ;
   product.addServerPatch("jboss",  cs.server.jboss.patch) ;
