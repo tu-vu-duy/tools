78e193875ca9fb5306772f5f32c5f81833e4b201
tuan_pham <tuan_pham@42d3c691-033b-0410-95b4-af486b8189e5>
2009-04-28 02:01:21 +0000

    cs-2664 create dev branches for cs 1.3 tools
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@31345 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/cs/branches-dev/1.3lab/module.js
@@ -0,0 +1,174 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+  var ws = params.ws;
+  var module = new Module();
+
+  module.version =  "1.3-lab" ;
+  module.relativeMavenRepo =  "org/exoplatform/cs" ;
+  module.relativeSRCRepo =  "cs/branches-dev/1.3lab" ;
+  module.name = "cs" ;
+    
+  module.eXoApplication = {};
+  module.eXoApplication.mail = 
+    new Project("org.exoplatform.cs", "exo.cs.eXoApplication.mail.webapp", "war", module.version).
+    addDependency(new Project("javax.mail", "mail", "jar", "1.4.1")).
+    addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.mail.service", "jar",  module.version));
+  module.eXoApplication.mail.deployName = "mail";
+    
+  module.eXoApplication.calendar = 
+    new Project("org.exoplatform.cs", "exo.cs.eXoApplication.calendar.webapp", "war", module.version).
+      addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.calendar.service", "jar",  module.version)).
+	  //addDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", 1.3)).
+	  //addDependency(ws.frameworks.cometd).
+	  addDependency(new Project("rome", "rome", "jar", "0.8")).
+	  addDependency(new Project("jdom", "jdom", "jar", "1.0")).
+      addDependency(new Project("ical4j", "ical4j", "jar", "1.0-beta5")) ;
+  module.eXoApplication.calendar.deployName = "calendar";
+    
+  module.eXoApplication.contact = 
+    new Project("org.exoplatform.cs", "exo.cs.eXoApplication.contact.webapp", "war", module.version).
+      addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.contact.service", "jar",  module.version)).
+      addDependency(new Project("net.wimpi.pim", "jpim-0.1", "jar",  "1.0"));
+  module.eXoApplication.contact.deployName = "contact";
+  
+  module.eXoApplication.content = 
+    new Project("org.exoplatform.cs", "exo.cs.eXoApplication.content.webapp", "war", module.version).
+      addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.content.service", "jar",  module.version));
+  module.eXoApplication.content.deployName = "content";
+  
+   
+  module.eXoApplication.chat = new Project("org.exoplatform.cs", "exo.cs.eXoApplication.chat.webapp", "war", module.version).
+  	  addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.chat.service", "jar", module.version).
+  	  addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.organization.service", "jar", module.version)).
+  	  //addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.organization.webapp", "war", module.version)).
+      addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.organization.client.openfire", "jar", module.version)).
+  	  //addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.organization.webapp", "war", module.version)).
+  	  addDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", "1.3.1")).
+  	  addDependency(ws.frameworks.cometd).
+  	  addDependency(new Project("org.exoplatform.portal", "exo.portal.web.rest", "war", "2.5.3")).
+  	  addDependency(new Project("jabber.smack", "smack", "jar", "3.0.4")).
+  	  addDependency(new Project("jabber.smack", "smackx", "jar", "3.0.4")).
+  	  addDependency(new Project("org.jcrom", "jcrom", "jar", "1.2")).
+      addDependency(new Project("commons-fileupload", "commons-fileupload", "jar", "1.0")).
+	  addDependency(new Project("commons-io", "commons-io", "jar", "1.3")).
+	  addDependency(new Project("log4j", "log4j", "jar", "1.2.12")).
+	  addDependency(new Project("org.slf4j", "slf4j-api", "jar", "1.4.3")).
+	  addDependency(new Project("org.slf4j", "slf4j-log4j12", "jar", "1.4.3"))
+		  	
+  	);
+  module.eXoApplication.chat.deployName = "chat";
+  
+  module.web = {}
+  module.web.webservice = 
+    new Project("org.exoplatform.cs", "exo.cs.web.webservice", "jar",  module.version);
+  module.web.csResources = 
+    new Project("org.exoplatform.cs", "exo.cs.web.csResources", "war", module.version) ;
+  module.web.csportal = 
+    new Project("org.exoplatform.cs", "exo.cs.web.portal", "exo-portal", module.version).
+      addDependency(portal.web.eXoResources) .
+      addDependency(portal.web.eXoMacSkin) .
+      addDependency(portal.web.eXoVistaSkin) .
+      addDependency(portal.web.rest) .  
+      addDependency(ws.frameworks.servlet) .   
+	  addDependency(portal.webui.portal) .
+	  addDependency(portal.eXoGadgetServer).
+	  addDependency(portal.eXoGadgets).
+	  addDependency(portal.portlet.exoadmin) .
+	  addDependency(portal.portlet.web) .
+	  addDependency(portal.portlet.dashboard) .
+      addDependency(jcr.frameworks.command) .
+      addDependency(jcr.frameworks.web) ;
+  
+  
+  /**
+   * Configure and add server path for chat, single sign-on
+   */
+  module.server = {}
+  module.server.tomcat = {}
+  module.server.tomcat.patch = 
+    new Project("org.exoplatform.cs", "exo.cs.server.tomcat.patch", "jar", module.version);
+
+  module.server.jboss = {}
+  module.server.jboss.patch = 
+	    new Project("org.exoplatform.cs", "exo.cs.server.jboss.patch", "jar", module.version);
+  
+  
+  /**
+   * Configure and deploy Openfire
+   */
+  module.configure = function(tasks, deployServers, serverMap) {
+  	if (deployServers!==null) {
+      var server = serverMap.get("tomcat");
+      tasks.add(deployOpenfireServer(server, this));
+      //tasks.add(deployRed5Server(server, this));
+    }
+  };
+  
+      
+  return module;
+}
+
+/**
+ * Configure and deploy Openfire, Red5 servers for integrated chat on cs 
+ */
+
+function deployOpenfireServer(mainServer, module) {
+	var deployServerTask = new TaskDescriptor("Release Dependency Task", eXo.env.dependenciesDir) ;
+  var server = {};
+  server.cleanServer = "openfire-3.4.5";
+  server.name = "exo-openfire";
+  server.serverHome = eXo.env.workingDir + "/" + server.name;
+  server.deployLibDir = server.serverHome + "/lib";
+  server.openfireJar = "exo.cs.eXoApplication.organization.client.openfire-" + module.version + ".jar" ;
+  deployServerTask.description = "Deploy " + server.name + " ";
+	deployServerTask.execute = function() {
+    eXo.System.info("DELETE", "Delete " + server.serverHome);
+    eXo.core.IOUtil.remove(server.serverHome);
+		eXo.System.info("COPY", "Copy a clean server " + server.name);
+		eXo.core.IOUtil.cp(eXo.env.dependenciesDir + "/" + server.cleanServer, server.serverHome);
+    eXo.System.info("Gets the configuration file -in a buffer - of openfire (openfire.xml) from the library jar file");
+		var configBuffer = eXo.core.IOUtil.getJarEntryContent(mainServer.deployLibDir+"/"+server.openfireJar, "openfire/openfire.xml") ;
+		if (configBuffer===null) { eXo.System.info("ERROR", "Error retrieving config file from jar !"); return; }
+		// writes the buffer into the configuration file (openfire/conf/openfire.xml)
+		eXo.System.info("INFO", "Creating config file from buffer...");
+		eXo.core.IOUtil.createFile(server.serverHome+"/conf/openfire.xml", configBuffer);
+		// copies the exo openfire library to openfire server
+		eXo.System.info("INFO", "Copying exo openfire library file...");
+		eXo.core.IOUtil.cp(mainServer.deployLibDir + "/" + server.openfireJar, 
+						           server.deployLibDir + "/" + server.openfireJar);
+	}
+	return deployServerTask ;
+}
+
+/*function deployRed5Server(mainServer, module) {
+	var deployServerTask = new TaskDescriptor("Release Dependency Task", eXo.env.dependenciesDir) ;
+  var server = {};
+  server.name = "exo-red5";
+  server.cleanServer = "red5-0.7.0";
+  server.serverHome = eXo.env.workingDir + "/" + server.name;
+  server.deployWebappDir = server.serverHome + "/webapps";
+  deployServerTask.description = "Deploy " + server.name + " ";
+	deployServerTask.execute = function() {
+    eXo.System.info("DELETE", "Delete " + server.serverHome);
+    eXo.core.IOUtil.remove(server.serverHome);
+		eXo.System.info("COPY", "Copy a clean server " + server.name);
+		eXo.core.IOUtil.cp(eXo.env.dependenciesDir + "/" + server.cleanServer, server.serverHome);
+		eXo.System.info("eXo " + server.name + " applications file...");
+    var whiteboard = new Project("org.exoplatform.cs", "exo.liveroom.eXoApplication.whiteboard.service", "war", module.version);
+    whiteboard.deployName = "whiteboard";
+    whiteboard.deployTo(eXo.env.m2Repos, server);
+
+    var videoconf = new Project("org.exoplatform.cs", "exo.liveroom.eXoApplication.videoconf.service", "war", module.version);
+    videoconf.deployName = "videoconf";
+    videoconf.deployTo(eXo.env.m2Repos, server);
+	}
+	return deployServerTask ;
+}*/
