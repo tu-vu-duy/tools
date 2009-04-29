f9aedab8463ed50d16e2213a2aefa2ef1880a42d
minh_dang <minh_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2009-04-29 08:39:16 +0000

    ECM-3589 update build tools for DMS 2.4beta1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@31438 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/ecm/dms/tags/2.4beta1/module.js
+++ build/src/main/javascript/eXo/modules/ecm/dms/tags/2.4beta1/module.js
@@ -1,46 +1,65 @@
 eXo.require("eXo.projects.Module") ;
 eXo.require("eXo.projects.Product") ;
 
-function getProduct(version) {
-  var product = new Product();
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var ws = params.ws;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;  
+  var module = new Module();
   
-  product.name = "eXoDMS" ;
-  product.portalwar = "portal.war" ;
-  product.codeRepo = "ecm/dms/tags/2.4beta1" ;
-  product.useContentvalidation = true;
-  product.contentvalidationVersion = "2.4beta1";
-  product.workflowVersion = "1.0";
-  product.serverPluginVersion = "2.5.2" ;
+  module.version = "2.4beta1" ;
+  module.relativeMavenRepo =  "org/exoplatform/ecm/dms" ;
+  module.relativeSRCRepo =  "ecm/dms/tags/2.4beta1" ;
+  module.name =  "dms" ;
     
-  var tool =  Module.GetModule("tools/trunk") ;
-  var kernel = Module.GetModule("kernel/tags/2.0.6") ;
-  var core = Module.GetModule("core/tags/2.1.4") ;
-  var ws = Module.GetModule("ws/tags/1.3.2");
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.4", {kernel : kernel, core : core}) ;
-  var eXoJcr = Module.GetModule("jcr/tags/1.10.2", {kernel : kernel, core : core, ws : ws}) ;
-  var portal = Module.GetModule("portal/tags/2.5.2", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
-  var dms = Module.GetModule("ecm/dms/tags/2.4beta1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
-    
-  product.addDependencies(portal.web.rest);
-  product.addDependencies(dms.web.dmsportal);
-  product.addDependencies(dms.web.eXoDMSResources);
-  product.addDependencies(portal.portlet.exoadmin);
-  product.addDependencies(portal.portlet.web);
-  product.addDependencies(portal.portlet.dashboard);
-	product.addDependencies(portal.eXoGadgetServer);
-	product.addDependencies(portal.eXoGadgets);  
-  product.addDependencies(dms.portlet.dms);
-  product.addDependencies(dms.portlet.jcr_console);
-  product.addDependencies(dms.gadgets);
-    
-  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
-  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
-  product.addServerPatch("jbossear",  portal.server.jbossear.patch) ;  
-  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
-  product.addServerPatch("ear",  portal.server.websphere.patch) ;
-
-  product.module = dms ;
-  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal];
-
-  return product ;
+  module.portlet = {}
+  module.portlet.dms = 
+    new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.portlet.ecm", "exo-portlet", module.version).
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.cms", "jar",  module.version)) .     
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.deployment", "jar",  module.version)) .    
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.publication", "jar", module.version)).
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.connector.fckeditor", "jar", module.version)).
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.webui.dms", "jar", module.version)).
+    addDependency(new Project("rome", "rome", "jar", "0.9")) .
+    addDependency(new Project("com.totsp.feedpod", "itunes-com-podcast", "jar", "0.2")) .
+    addDependency(new Project("ical4j", "ical4j", "jar", "0.9.20")) .
+    addDependency(new Project("jdom", "jdom", "jar", "1.0")).
+    addDependency(new Project("org.apache.ws.commons", "ws-commons-util", "jar", "1.0.1")).
+    addDependency(new Project("com.sun.xml.stream", "sjsxp", "jar", "1.0")).
+    addDependency(new Project("pdfbox", "pdfbox", "jar", "0.7.2"));
+  
+  module.portlet.jcr_console = 
+    new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.portlet.jcr-console", "exo-portlet", module.version).
+    addDependency(new Project("exo-weblogic", "exo-weblogic-authproviders", "jar", "1.0")).
+	  addDependency(new Project("exo-weblogic", "exo-weblogic-loginmodule", "jar", "1.0")).  
+	  addDependency(new Project("commons-logging", "commons-logging", "jar", "1.0.4"));
+  
+  module.gadgets = 
+    new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.gadgets", "war", module.version).
+    addDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", "1.3.2"));  
+    module.gadgets.deployName = "eXoDMSGadgets";
+  
+  module.application = {}
+  module.application.rest = new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.publication","jar", module.version).
+  	addDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", "1.3.2"));
+  
+  module.web = {}
+  module.web.eXoDMSResources = 
+    new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.web.eXoDMSResources", "war", module.version) ;  
+  module.web.eXoDMSResources.deployName = "eXoDMSResources" ;
+      
+  module.web.dmsportal = 
+    new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.web.portal", "exo-portal", module.version).
+    addDependency(portal.web.eXoResources) .
+    addDependency(portal.web.eXoMacSkin) .
+    addDependency(portal.web.eXoVistaSkin) .
+    addDependency(portal.webui.portal) .
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web) ;   
+  
+  return module;
 }
