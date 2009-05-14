938800b2ee6a27b638a93093e9df13d13711aa6e
tran <tran@42d3c691-033b-0410-95b4-af486b8189e5>
2009-05-14 08:41:05 +0000

    PORTAL-2989 Update for ExoCache improvements
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@31762 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/portal/trunk.js
+++ build/src/main/javascript/eXo/products/portal/trunk.js
@@ -10,11 +10,11 @@ function getProduct(version) {
   product.serverPluginVersion = "2.6-SNAPSHOT"
 
   var tool = Module.GetModule("tools/trunk") ;
-  var kernel = Module.GetModule("kernel/tags/2.1.1") ;
+  var kernel = Module.GetModule("kernel/trunk") ;
   var core = Module.GetModule("core/trunk") ;
   var ws = Module.GetModule("ws/tags/2.0.1", {kernel : kernel, core : core});
   var eXoJcr = Module.GetModule("jcr/tags/1.11.1", {kernel : kernel, core : core, ws : ws}) ;
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.1.1", {kernel : kernel, core : core, ws : ws}) ;    
+  var eXoPortletContainer = Module.GetModule("portlet-container/trunk", {kernel : kernel, core : core, ws : ws}) ;    
   var portal = Module.GetModule("portal/trunk", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
 
   product.addDependencies(portal.web.rest) ;
