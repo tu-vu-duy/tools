b14f35442ebf378fa6d4fdb00433b3caac73012b
tung_nguyen_dang <tung_nguyen_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2009-05-14 07:19:07 +0000

    SOC-172 update to portal 2.6
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@31746 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/social/trunk.js
+++ build/src/main/javascript/eXo/products/social/trunk.js
@@ -7,16 +7,16 @@ function getProduct(version) {
   product.name = "social" ;
   product.portalwar = "portal.war" ;
   product.codeRepo = "social/trunk" ;
-  product.serverPluginVersion = "2.5.3"
+  product.serverPluginVersion = "2.6-SNAPSHOT"
   
   
   var tool = Module.GetModule("tools/trunk") ;
-  var kernel = Module.GetModule("kernel/tags/2.0.7") ;
-  var core = Module.GetModule("core/tags/2.1.5") ;
-  var ws = Module.GetModule("ws/tags/1.3.3", {kernel : kernel, core : core});
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.6", {kernel : kernel, core : core}) ;    
-  var eXoJcr = Module.GetModule("jcr/tags/1.10.3", {kernel : kernel, core : core, ws : ws}) ;
-  var portal = Module.GetModule("portal/tags/2.5.3", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
+  var kernel = Module.GetModule("kernel/tags/2.1.1") ;
+  var core = Module.GetModule("core/trunk") ;
+  var ws = Module.GetModule("ws/tags/2.0.1", {kernel : kernel, core : core});
+  var eXoJcr = Module.GetModule("jcr/tags/1.11.1", {kernel : kernel, core : core, ws : ws}) ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.1.1", {kernel : kernel, core : core, ws : ws}) ;    
+  var portal = Module.GetModule("portal/trunk", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
   var social = Module.GetModule("social/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
   
   product.addDependencies(social.web.portal) ;
