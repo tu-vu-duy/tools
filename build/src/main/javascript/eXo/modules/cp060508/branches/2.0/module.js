49a9f139f3b95b33647f2c0c5cd79da9e87c8d8a
philippe <philippe@42d3c691-033b-0410-95b4-af486b8189e5>
2009-05-22 02:23:57 +0000

    js descriptors of cp060508 v2.0
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@32000 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/cp060508/branches/2.0/module.js
@@ -0,0 +1,43 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+
+  var module = new Module();
+
+  module.version = "2.0" ;
+  module.relativeMavenRepo =  "org/exoplatform/cp060508" ;
+  module.relativeSRCRepo =  "cp060508/branches/2.0" ;
+  module.name =  "cp060508" ;  
+
+/*	module.portlet = {}
+  module.portlet.web = new Project("org.exoplatform.cp060508", "cp060508.portlet.web", "exo-portlet", module.version).
+  	addDependency(new Project("org.exoplatform.cp060508", "cp060508.component.navigation", "jar",  module.version));
+  module.portlet.web.deployName = "cp060508PortletWeb" ;
+*/	
+	module.web = {} ;
+/*	module.web.eXoResources = new Project("org.exoplatform.cp060508", "cp060508.web.cp060508Resources", "war", module.version);
+  module.web.eXoResources.deployName = "eXoResourcesCp060508" ;
+*/
+/*  module.server = {}
+  module.server.tomcat = {}
+  module.server.tomcat.patch = 
+    new Project("org.exoplatform.cp060508", "cp060508.server.tomcat.patch", "jar", module.version);
+
+  */
+
+  module.web.portal = 
+    new Project("org.exoplatform.cp060508", "cp060508.web.portal", "exo-portal", module.version).
+    addDependency(portal.web.eXoResources) .
+    addDependency(portal.webui.portal) .
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web) ;
+
+  return module;
+}
---
+++ build/src/main/javascript/eXo/products/cp060508/2.0.js
@@ -0,0 +1,67 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "cp060508" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "cp060508/branches/2.0" ;
+  product.useContentvalidation = true;
+  product.contentvalidationVersion = "2.3.1";
+  product.workflowVersion = "1.0.1" ;
+  product.serverPluginVersion = "2.5.3" ;
+  
+  var tool = Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.7") ;
+  var core = Module.GetModule("core/tags/2.1.5") ;
+  var ws = Module.GetModule("ws/tags/1.3.3");
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.6", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.3", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal/tags/2.5.3", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr}); 
+  var dms = Module.GetModule("ecm/dms/tags/2.3.1", {kernel : kernel, core : core,ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr , portal : portal});
+  var wcm = Module.GetModule("ecm/wcm/tags/1.1rc3", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, dms : dms});
+    var cs = Module.GetModule("cs/tags/1.2.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var cp060508 = Module.GetModule("cp060508/branches/2.0", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+
+
+  /* CG95 modules */    
+  product.addDependencies(cp060508.web.portal) ; 
+//  product.addDependencies(cp060508.web.eXoResources) ;
+//  product.addDependencies(cp060508.portlet.web) ;
+ 
+  /* CS modules */
+  product.addDependencies(cs.eXoApplication.mail) ;
+  product.addDependencies(cs.eXoApplication.calendar) ;
+  product.addDependencies(cs.eXoApplication.contact) ;
+  product.addDependencies(cs.eXoApplication.content) ;    
+  product.addDependencies(cs.web.webservice) ;
+  product.addDependencies(cs.web.csResources) ;
+
+  /* Portal modules */
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.portlet.dashboard) ;  
+  product.addDependencies(portal.eXoGadgetServer) ;
+  product.addDependencies(portal.eXoGadgets) ;
+  product.addDependencies(portal.web.rest);
+        
+  /* DMS modules */
+  product.addDependencies(dms.portlet.dms);
+  product.addDependencies(dms.web.eXoDMSResources) ;
+
+  /* WCM modules */
+  product.addDependencies(wcm.portlet.webpresentation);
+  product.addDependencies(wcm.portlet.websearches);
+  product.addDependencies(wcm.web.eXoWCMResources) ;
+
+  /* Server patches */
+  product.addServerPatch("tomcat",  portal.server.tomcat.patch) ; 
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+
+  product.module = cp060508 ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, dms, wcm, cs];
+    
+  return product ;
+}
