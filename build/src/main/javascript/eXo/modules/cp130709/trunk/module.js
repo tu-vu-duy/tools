7dc0af3924a558f8566769f5ce0286b5ec8ea9a8
ali_hamdi_exo <ali_hamdi_exo@42d3c691-033b-0410-95b4-af486b8189e5>
2009-07-15 08:08:36 +0000

    ALM-2: Add new js file for building the project with exobuild
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@33993 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/cp130709/trunk/module.js
@@ -0,0 +1,37 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var ws = params.ws;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+  var dms = params.dms;  
+  
+  var module = new Module();
+
+  module.version = "trunk" ;
+  module.relativeMavenRepo =  "org/exoplatform/cp130709" ;
+  module.relativeSRCRepo =  "cp130709/trunk" ;
+  module.name =  "cp130709" ;
+  
+  module.portlet = {};
+  module.portlet.strutsAlmerysPortlet = 
+    new Project("org.exoplatform.cp130709", "cp130709.portlet.strutsAlmerysPortlet", "exo-portlet", module.version);
+  
+  module.web = {};
+  module.web.resources = 
+    new Project("org.exoplatform.cp130709", "cp130709.web.resources", "war", module.version).
+    addDependency(portal.web.eXoResources) .
+    addDependency(portal.web.eXoMacSkin) .
+    addDependency(portal.web.eXoVistaSkin);
+        
+  module.web.portal = 
+    new Project("org.exoplatform.cp130709", "cp130709.web.portal", "exo-portal", module.version);
+    
+    
+  return module;
+}
