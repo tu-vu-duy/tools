5daff4fec9de719110e08cd8ec7c94addb435f6a
benjamin_paillereau <benjamin_paillereau@42d3c691-033b-0410-95b4-af486b8189e5>
2009-07-16 14:30:00 +0000

    DMS2.5=>WCM1.2
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@34068 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/wcm/trunk.js
+++ build/src/main/javascript/eXo/products/wcm/trunk.js
@@ -17,9 +17,9 @@ function getProduct(version) {
   var core = Module.GetModule("core/tags/2.1.5") ;
   var ws = Module.GetModule("ws/tags/1.3.3");
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.6", {kernel : kernel, core : core}) ;    
-  var eXoJcr = Module.GetModule("jcr/tags/1.10.3", {kernel : kernel, core : core, ws : ws}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.4", {kernel : kernel, core : core, ws : ws}) ;
   var portal = Module.GetModule("portal/tags/2.5.3", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});         
-  var dms = Module.GetModule("ecm/dms/tags/2.4rc1", {kernel : kernel, core : core,ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr , portal : portal});
+  var dms = Module.GetModule("ecm/dms/tags/2.5", {kernel : kernel, core : core,ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr , portal : portal});
   var wcm = Module.GetModule("ecm/wcm/trunk", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, dms : dms});
     
   product.addDependencies(portal.portlet.exoadmin) ;
