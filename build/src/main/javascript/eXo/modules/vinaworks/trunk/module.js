dc9afb5fc68b20723f5797b558455b9cf892d358
due_tran_tri <due_tran_tri@42d3c691-033b-0410-95b4-af486b8189e5>
2009-08-03 02:50:18 +0000

    New project for new commers
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@34816 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/vinaworks/trunk/module.js
@@ -0,0 +1,25 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+	var jcr = params.eXoJcr;
+	var portal = params.portal;
+	var module = new Module();
+	
+  module.version =  "trunk" ;
+  module.relativeMavenRepo =  "org/exoplatform/vinaworks" ;
+  module.relativeSRCRepo =  "vinaworks/trunk" ;
+  module.name =  "test" ;
+  
+  module.web = {};
+  module.web.portal = 
+    new Project("org.exoplatform.vinaworks", "exo.vinaworks.web.portal", 
+                "exo-portal", module.version).
+    addDependency(portal.web.eXoResources) .
+    addDependency(portal.web.eXoMacSkin) .
+    addDependency(portal.web.eXoVistaSkin) .
+    addDependency(portal.webui.portal) .
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web);	
+	return module;
+}
