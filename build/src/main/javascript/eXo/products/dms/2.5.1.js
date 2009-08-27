ff8a12ea4c4fc379ac94a3e37e1fecda2b9b15ba
minh_dang <minh_dang@42d3c691-033b-0410-95b4-af486b8189e5>
2009-08-27 02:37:32 +0000

    ECM-4118 update JS pom for DMS 2.5.1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@36117 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/dms/2.5.1.js
@@ -0,0 +1,46 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "eXoDMS" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "ecm/dms/tags/2.5.1" ;
+  product.useContentvalidation = true;
+  product.contentvalidationVersion = "2.5.1";
+  product.workflowVersion = "1.0.2";
+  product.serverPluginVersion = "2.5.5" ;
+    
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.7") ;
+  var core = Module.GetModule("core/tags/2.1.5") ;
+  var ws = Module.GetModule("ws/tags/1.3.3");
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.7", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.4", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal/tags/2.5.5", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var dms = Module.GetModule("ecm/dms/tags/2.5.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+    
+  product.addDependencies(portal.web.rest);
+  product.addDependencies(dms.web.dmsportal);
+  product.addDependencies(dms.web.eXoDMSResources);
+  product.addDependencies(portal.portlet.exoadmin);
+  product.addDependencies(portal.portlet.web);
+  product.addDependencies(portal.portlet.dashboard);
+	product.addDependencies(portal.eXoGadgetServer);
+	product.addDependencies(portal.eXoGadgets);  
+  product.addDependencies(dms.portlet.dms);
+  product.addDependencies(dms.portlet.jcr_console);
+  product.addDependencies(dms.gadgets);
+    
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jbossear",  portal.server.jbossear.patch) ;  
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+  product.addServerPatch("ear",  portal.server.websphere.patch) ;
+
+  product.module = dms ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal];
+
+  return product ;
+}
