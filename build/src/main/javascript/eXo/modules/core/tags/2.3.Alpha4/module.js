ffd1f6f126a57886bac9d0d6b3fc30a4c8feaa16
dbaeli <dbaeli@42d3c691-033b-0410-95b4-af486b8189e5>
2009-08-27 02:42:44 +0000

    PORTAL-3314: Prepare JCR 1.12.Alpha4 dependencies * fixed pdfbox packaged version to the one in the maven dependencies "0.7.3" * fixed org.htmlparser:htmlparser dependency declaration (to the official maven name not html-parser) * fixed jgroups:jgroups dependency declaration (to the official maven name, not javagroups:jgroups-all) * commons-fileupload 1.2.1 packaged not 1.0 (according to maven build dependencies)
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@36118 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/core/tags/2.3.Alpha4/module.js
+++ build/src/main/javascript/eXo/modules/core/tags/2.3.Alpha4/module.js
@@ -35,11 +35,11 @@ function getModule(params) {
 
   module.component.documents =
     new Project("org.exoplatform.core", "exo.core.component.document", "jar", module.version).
-    addDependency(new Project("pdfbox", "pdfbox", "jar", "0.7.2")).
+    addDependency(new Project("pdfbox", "pdfbox", "jar", "0.7.3")).
     addDependency(new Project("com.lowagie", "itext", "jar", "2.1.0")).
     addDependency(new Project("bouncycastle", "bcmail-jdk14", "jar", "136")).
     addDependency(new Project("bouncycastle", "bcprov-jdk14", "jar", "136")).
-    addDependency(new Project("html-parser", "html-parser", "jar", "1.6")).
+    addDependency(new Project("org.htmlparser", "htmlparser", "jar", "1.6")).
     addDependency(new Project("org.apache.poi", "poi", "jar", "3.0.2-FINAL")).
     addDependency(new Project("org.apache.poi", "poi-scratchpad", "jar", "3.0.2-FINAL"));
     
