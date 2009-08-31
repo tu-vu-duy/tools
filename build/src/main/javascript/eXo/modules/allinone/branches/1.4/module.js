b3b3536103d5547a9cd1270f263306d2aba93d90
paristote <paristote@42d3c691-033b-0410-95b4-af486b8189e5>
2009-08-31 11:37:36 +0000

    ALL-46 update js descriptors for All in One 1.4 and 1.4rc1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@36437 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/allinone/branches/1.4/module.js
@@ -0,0 +1,37 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+  var webos = params.webos;
+  var dms = params.dms ;
+  var workflow = params.workflow ;
+  var wcm = params.wcm ;
+  var cs = params.cs ;
+  var ks = params.ks ;
+  var ws = params.ws;
+  var module = new Module();
+
+  module.version = "1.4" ;
+  module.relativeMavenRepo =  "org/exoplatform/allinone" ;
+  module.relativeSRCRepo =  "allinone/branches/1.4" ;
+  module.name =  "allinone" ;
+       
+  module.web = {}
+  module.web.allinoneportal = 
+    new Project("org.exoplatform.allinone", "exo.allinone.web.portal", "exo-portal", module.version).   
+    addDependency(portal.web.eXoResources).
+    addDependency(portal.web.eXoMacSkin).
+    addDependency(portal.web.eXoVistaSkin).
+    addDependency(portal.webui.portal).
+    addDependency(jcr.frameworks.command).
+    addDependency(jcr.frameworks.web).
+    addDependency(portal.web.rest);
+  
+  return module;
+}
