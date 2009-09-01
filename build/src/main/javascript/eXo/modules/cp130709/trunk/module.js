70ee64702f399ea083f586e7cdf0f02f43c75a8c
kmenzli <kmenzli@42d3c691-033b-0410-95b4-af486b8189e5>
2009-09-01 08:30:25 +0000

    ALM-63 : cas jars
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@36518 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/cp130709/trunk/module.js
+++ build/src/main/javascript/eXo/modules/cp130709/trunk/module.js
@@ -61,7 +61,9 @@ function getModule(params) {
     addDependency(portal.web.eXoVistaSkin);
         
   module.web.portal = 
-    new Project("org.exoplatform.cp130709", "cp130709.web.portal", "exo-portal", module.version);
+    new Project("org.exoplatform.cp130709", "cp130709.web.portal", "exo-portal", module.version).
+	addDependency(new Project("com.almerys.sso", "sso-token", "jar", "1.0.0.0.7")).
+	addDependency(new Project("org.jasig.cas", "cas-client-core", "jar", "3.1.3"));
 	
   module.webservice = {};
   module.webservice.WSIndividu = 
