5742b69622745e4a93179138badbf8467461290d
paristote <paristote@42d3c691-033b-0410-95b4-af486b8189e5>
2009-09-01 11:06:32 +0000

    CCP-63 add module of ecm suite trunk
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@36559 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/ecm/suite/trunk/module.js
@@ -0,0 +1,35 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+  var dms = params.dms ;
+  var workflow = params.workflow ;
+  var wcm = params.wcm ;
+  var ws = params.ws;
+  var module = new Module();
+
+  module.version = "1.0-SNAPSHOT" ;
+  module.relativeMavenRepo =  "org/exoplatform/ecm/suite" ;
+  module.relativeSRCRepo =  "suite/trunk" ;
+  module.name =  "ecm-suite" ;
+       
+  module.web = {}
+  module.web.ecmsuiteportal = 
+    new Project("org.exoplatform.ecm.suite", "ecmsuite-portalweb", "exo-portal", module.version).   
+    addDependency(portal.web.eXoResources).
+    addDependency(portal.web.eXoMacSkin).
+    addDependency(portal.web.eXoVistaSkin).
+    addDependency(portal.webui.portal).
+    addDependency(jcr.frameworks.command).
+    addDependency(jcr.frameworks.web).
+    addDependency(portal.web.rest);
+  module.web.ecmsuiteportal.deployName = "portal" ;
+  
+  return module;
+}
