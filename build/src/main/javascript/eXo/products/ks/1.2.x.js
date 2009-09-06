06c0e0d0458ed38726162e7313bac6d000d169c6
hung_nguyen_quang <hung_nguyen_quang@42d3c691-033b-0410-95b4-af486b8189e5>
2009-09-06 11:08:06 +0000

    KS-1840: Tool for build KS branch 1.2.x
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@36836 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/ks/1.2.x.js
@@ -0,0 +1,54 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+
+  var product = new Product();
+  product.name = "eXoKS" ;
+  product.portalwar = "portal.war" ;
+  product.serverPluginVersion = "2.5.3"
+    
+  var tool = Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.7") ;
+  var core = Module.GetModule("core/tags/2.1.5") ;
+  var ws = Module.GetModule("ws/tags/1.3.3", {kernel : kernel, core : core});
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.6", {kernel : kernel, core : core}) ;    
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.3", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal/tags/2.5.3", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});  
+  //var webos = Module.GetModule("webos/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var ks = Module.GetModule("ks/branches/1.2.x", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  
+
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.eXoGadgetServer) ;
+	product.addDependencies(portal.eXoGadgets) ;
+  product.addDependencies(portal.web.rest) ;
+  product.addDependencies(portal.portlet.dashboard) ;
+  //product.addDependencies(webos.web.webosResources);
+  //product.addDependencies(portal.eXoWidget.web) ;
+            
+  product.addDependencies(ks.eXoApplication.forum) ;
+  product.addDependencies(ks.eXoApplication.faq) ;
+  product.addDependencies(ks.eXoApplication.common) ;
+  //product.addDependencies(ks.eXoApplication.blog) ;
+  //product.addDependencies(ks.eXoApplication.wiki) ;
+
+  product.addDependencies(ks.web.ksportal) ;
+  product.addDependencies(ks.web.webservice) ;
+  product.addDependencies(ks.web.ksResources) ;
+  //product.addDependencies(cs.eXoApplication.contact) ;
+  
+	//product.addServerPatch("tomcat", ks.server.tomcat.patch) ;
+  //product.addServerPatch("jboss",  ks.server.jboss.patch) ;
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+    
+  product.codeRepo = "ks/branches/1.2.x" ;
+
+  product.module = ks ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal];
+    
+  return product ;
+}
