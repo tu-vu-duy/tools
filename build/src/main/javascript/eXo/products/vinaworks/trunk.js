ee3d7cd380001a0ef0db7ea641113ef45435a8a0
due_tran_tri <due_tran_tri@42d3c691-033b-0410-95b4-af486b8189e5>
2009-09-18 03:34:34 +0000

    CP-152
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@37400 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/vinaworks/trunk.js
+++ build/src/main/javascript/eXo/products/vinaworks/trunk.js
@@ -3,66 +3,107 @@ eXo.require("eXo.projects.Product") ;
 
 function getProduct(version) {
   var product = new Product();
-  
-  product.name = "vinaworks" ;
+
+  product.name = "eXoVinaworksProduct" ;
   product.portalwar = "portal.war" ;
   product.codeRepo = "vinaworks/trunk" ;
-  product.serverPluginVersion = "2.5.2"
   product.useContentvalidation = true;
-  product.contentvalidationVersion = "2.3";
-  //product.workflowVersion = "1.0";
+  product.contentvalidationVersion = "2.3.1";
+  product.useWorkflow = true;
+  product.workflowVersion = "1.0.1" ;
+  product.serverPluginVersion = "2.5.3" ;
+  
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.7") ;
+  var ws = Module.GetModule("ws/tags/1.3.3");
+  var core = Module.GetModule("core/tags/2.1.5") ;
 
-   var tool =  Module.GetModule("tools/trunk") ;
-  var kernel = Module.GetModule("kernel/tags/2.0.6") ;
-  var core = Module.GetModule("core/tags/2.1.4") ;
-  var ws = Module.GetModule("ws/tags/1.3.2");
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.5", {kernel : kernel, core : core}) ;
-  var eXoJcr = Module.GetModule("jcr/tags/1.10.2", {kernel : kernel, core : core, ws : ws}) ;
-  var portal = Module.GetModule("portal/tags/2.5.2", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
-  var dms = Module.GetModule("ecm/dms/tags/2.3", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
-  var cs = Module.GetModule("cs/tags/1.2RC1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
-  var vinaworks = Module.GetModule("vinaworks/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
-
-
-  product.addDependencies(portal.web.rest) ;
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.6", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.3", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal/tags/2.5.3", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, ws : ws });  
+  
+  //var ecm = Module.GetModule("ecm/dms/tags/2.2.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var dms = Module.GetModule("ecm/dms/tags/2.3.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  
+  var workflow = Module.GetModule("ecm/workflow/tags/1.0.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var cs = Module.GetModule("cs/tags/1.2.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var ks = Module.GetModule("ks/tags/1.1.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+//  var wcm = Module.GetModule("ecm/wcm/tags/1.1", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, dms : dms});
+  var webos = Module.GetModule("webos/tags/1.5.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var vinaworks = Module.GetModule("vinaworks/trunk", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, webos: webos, cs: cs, ks: ks, dms : dms,  workflow : workflow});
+  
   product.addDependencies(portal.portlet.exoadmin) ;
   product.addDependencies(portal.portlet.web) ;
-  product.addDependencies(portal.portlet.dashboard) ; 
+  product.addDependencies(portal.portlet.dashboard) ;  
   product.addDependencies(portal.eXoGadgetServer) ;
   product.addDependencies(portal.eXoGadgets) ;
-  product.addDependencies(portal.webui.portal);  
+  
+  product.addDependencies(portal.webui.portal);
+  
   product.addDependencies(portal.web.eXoResources);
   product.addDependencies(portal.web.eXoMacSkin);
   product.addDependencies(portal.web.eXoVistaSkin);
-  
-  product.addDependencies(dms.portlet.dms);
-  product.addDependencies(dms.portlet.jcr_console);
-  product.addDependencies(dms.gadgets);
-  product.addDependencies(dms.web.eXoDMSResources);
 
+  
+
+  
+  /*product.addDependencies(ecm.portlet.ecm) ;
+  product.addDependencies(ecm.portlet.workflow) ;
+  product.addDependencies(ecm.web.eXoECMResources) ;*/
+  
+  product.addDependencies(dms.web.eXoDMSResources);
+  product.addDependencies(dms.gadgets);
+  product.addDependencies(dms.portlet.dms);
+
+  
+  product.addDependencies(workflow.web.eXoWorkflowResources);
+  product.addDependencies(workflow.portlet.workflow);
+  
+//  product.addDependencies(wcm.portlet.webpresentation);
+//  product.addDependencies(wcm.portlet.websearches); 
+//  product.addDependencies(wcm.web.eXoWCMResources) ;
+              
   product.addDependencies(cs.eXoApplication.mail) ;
   product.addDependencies(cs.eXoApplication.calendar) ;
   product.addDependencies(cs.eXoApplication.contact) ;
   product.addDependencies(cs.eXoApplication.content) ;
+
   product.addDependencies(cs.web.webservice) ;
   product.addDependencies(cs.web.csResources) ;
-  
-  //product.addDependencies(portal.web.portal) ;
+            
+  product.addDependencies(ks.eXoApplication.forum) ;
+  product.addDependencies(ks.eXoApplication.faq) ;
+  product.addDependencies(ks.eXoApplication.common) ;
+
+  product.addDependencies(ks.web.webservice) ;
+  product.addDependencies(ks.web.ksResources) ;
+
+
+  product.addDependencies(vinaworks.web.vinaworksportal) ;
+//  product.addDependencies(vinaworks.web.vinaworksResources) ;//ligne ajoutee
+//  product.addDependencies(vinaworks.web.webosResources) ;    //ligne ajoutee
+  product.addDependencies(vinaworks.web.vinaworkswebos) ;    //ligne ajoutee
+//    product.addDependencies(vinaworks.web.vinaworkswebos);
+
+  //product.addDependencies(webos.web.webosResources);
+
+
 
   product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
   product.addServerPatch("jboss",  portal.server.jboss.patch) ;
-  product.addServerPatch("jbossear",  portal.server.jbossear.patch) ;
   product.addServerPatch("jonas",  portal.server.jonas.patch) ;
-  product.addServerPatch("ear",  portal.server.websphere.patch) ;
-  product.addDependencies(vinaworks.web.portal) ;
 
 	/* cleanup duplicated lib */
+  product.removeDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", "1.3"));
+  product.removeDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", "1.3.1"));
+  product.removeDependency(new Project("ical4j", "ical4j", "jar", "0.9.20"));
+  product.removeDependency(new Project("javax.mail", "mail", "jar", "1.4"));
   product.removeDependency(new Project("commons-httpclient", "commons-httpclient", "jar", "3.0"));
   product.removeDependency(new Project("commons-collections", "commons-collections", "jar", "3.1"));
-  product.removeDependency(new Project("javax.mail", "mail", "jar", "1.4"));
+  product.removeDependency(new Project("rome", "rome", "jar", "0.8"));
   
-  product.module = portal ;
-  product.dependencyModule = [ tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, dms, cs ];
-
-  return product ;
+  product.module = vinaworks ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, dms, cs, ks, webos, workflow];
+  
+  return product;
 }
