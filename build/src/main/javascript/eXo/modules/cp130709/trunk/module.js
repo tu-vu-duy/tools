28d246595f3828ffc8b6fced9aa815c4916a804a
hnjah <hnjah@42d3c691-033b-0410-95b4-af486b8189e5>
2009-09-18 09:55:06 +0000

    ALM-162:Integrate and Validate new Almerys Jar
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@37424 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/cp130709/trunk/module.js
+++ build/src/main/javascript/eXo/modules/cp130709/trunk/module.js
@@ -31,11 +31,16 @@ function getModule(params) {
 	addDependency(new Project("org.exoplatform.cp130709", "cp130709.webservice.WSIndividu", "jar", module.version)).
 	addDependency(new Project("org.exoplatform.cp130709", "cp130709.webservice.WSSection", "jar", module.version)).
 	addDependency(new Project("com.almerys.exo", "exo-account-commons", "jar", "1.0.0.0.1")).
-	addDependency(new Project("com.almerys.lmg", "lmg-individu", "jar", "0.0.3.0.5")).
-	addDependency(new Project("com.almerys.lmg", "lmg-individu-ws-client", "jar", "0.0.3.0.5")).	
+	addDependency(new Project("com.almerys.lmg", "lmg-individu", "jar", "0.0.3.0.7")).
+	addDependency(new Project("com.almerys.lmg", "lmg-individu-ws-client", "jar", "0.0.3.0.7")).
+	addDependency(new Project("javax.xml.ws", "jaxws-api", "jar", "2.1")).
+	addDependency(new Project("javax.xml.soap", "saaj-api", "jar", "1.3")).
+	addDependency(new Project("javax.jws", "jsr181-api", "jar", "1.0-MR1")).
+	addDependency(new Project("com.sun.xml.ws", "jaxws-rt", "jar", "2.1.5")).
+	addDependency(new Project("com.sun.xml.ws", "jaxws-tools", "jar", "2.1.5")).
 	addDependency(new Project("com.almerys.portal", "portal-filters", "jar", "1.2.0.0.0")).
 	addDependency(new Project("com.almerys.emut.lmgv2", "lmgv2-portal-filters", "jar", "0.1.6.0.0")).
-	addDependency(new Project("com.almerys.emut.lmgv2", "lmgv2-adherent-common-model", "jar", "0.2.5.0.0")).
+	addDependency(new Project("com.almerys.emut.lmgv2", "lmgv2-adherent-common-model", "jar", "0.2.8.0.0")).
 	addDependency(new Project("com.almerys.emut.lmgv2", "lmgv2-contact-manager", "jar", "0.1.6.0.0")).	
 	addDependency(new Project("com.almerys.emut", "health-guarantee-description", "jar", "0.1.2.0.0")).
 	addDependency(new Project("com.almerys", "commun-agaps", "jar", "1.4.2.0.0")).	
