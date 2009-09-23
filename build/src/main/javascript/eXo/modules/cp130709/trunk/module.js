7a8f6ee479ad91d9d8b70cbfc0fb192c9ee510f1
hnjah <hnjah@42d3c691-033b-0410-95b4-af486b8189e5>
2009-09-23 08:07:59 +0000

    ALM-176:create tags 0.6
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@37572 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/cp130709/trunk/module.js
+++ build/src/main/javascript/eXo/modules/cp130709/trunk/module.js
@@ -13,7 +13,7 @@ function getModule(params) {
   
   var module = new Module();
 
-  module.version = "0.6-SNAPSHOT" ;
+  module.version = "0.7-SNAPSHOT" ;
   module.relativeMavenRepo =  "org/exoplatform/cp130709" ;
   module.relativeSRCRepo =  "cp130709/trunk" ;
   module.name =  "cp130709" ;
