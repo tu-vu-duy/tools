251dd1c094c67bcae0f44b393f6e959915fff678
paristote <paristote@42d3c691-033b-0410-95b4-af486b8189e5>
2009-09-29 12:56:10 +0000

    ALL-49 add descriptor for v1.5
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@37819 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/allinone/tags/1.5/module.js
@@ -0,0 +1,42 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+  var webos = params.webos;
+  var dms = params.dms ;
+  var workflow = params.workflow ;
+  var wcm = params.wcm ;
+  var cs = params.cs ;
+  var ks = params.ks ;
+  var ws = params.ws;
+  var module = new Module();
+
+  module.version = "1.5" ;
+  module.relativeMavenRepo =  "org/exoplatform/allinone" ;
+  module.relativeSRCRepo =  "allinone/tags/1.5" ;
+  module.name =  "allinone" ;
+       
+  module.web = {}
+  module.web.allinoneportal = 
+    new Project("org.exoplatform.allinone", "exo.allinone.web.portal", "exo-portal", module.version).   
+    addDependency(portal.web.eXoResources).
+    addDependency(portal.web.eXoMacSkin).
+    addDependency(portal.web.eXoVistaSkin).
+    addDependency(portal.webui.portal).
+    addDependency(jcr.frameworks.command).
+    addDependency(jcr.frameworks.web).
+    addDependency(portal.web.rest);
+
+  module.tomcat = {}
+  module.tomcat.patch =
+    new Project("org.exoplatform.allinone", "aio-tomcat-patch", "jar", module.version);
+
+  
+  return module;
+}
