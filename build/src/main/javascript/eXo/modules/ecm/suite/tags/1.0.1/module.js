efd9b4fd8cbd3218c1e138131e89b15d271bf55d
paristote <paristote@42d3c691-033b-0410-95b4-af486b8189e5>
2009-10-15 07:58:02 +0000

    REL-167 create js descriptor for ecm suite 1.0.1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@38654 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/ecm/suite/tags/1.0.1/module.js
@@ -0,0 +1,39 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+  var dms = params.dms ;
+  var workflow = params.workflow ;
+  var wcm = params.wcm ;
+  var ws = params.ws;
+  var module = new Module();
+
+  module.version = "1.0.1" ;
+  module.relativeMavenRepo =  "org/exoplatform/ecm/suite" ;
+  module.relativeSRCRepo =  "suite/tags/1.0.1" ;
+  module.name =  "ecm-suite" ;
+       
+  module.web = {}
+  module.web.ecmsuiteportal = 
+    new Project("org.exoplatform.ecm.suite", "ecmsuite-portalweb", "exo-portal", module.version).   
+    addDependency(portal.web.eXoResources).
+    addDependency(portal.web.eXoMacSkin).
+    addDependency(portal.web.eXoVistaSkin).
+    addDependency(portal.webui.portal).
+    addDependency(jcr.frameworks.command).
+    addDependency(jcr.frameworks.web).
+    addDependency(portal.web.rest);
+  module.web.ecmsuiteportal.deployName = "portal" ;
+
+  module.tomcat = {}
+  module.tomcat.patch =
+    new Project("org.exoplatform.ecm.suite", "ecm-suite-tomcat-patch", "jar", module.version);
+  
+  return module;
+}
