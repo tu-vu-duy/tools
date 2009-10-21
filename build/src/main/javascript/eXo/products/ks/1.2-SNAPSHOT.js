3fe454aadd33bddee768b676064b71f067b8baa7
hung_nguyen_quang <hung_nguyen_quang@42d3c691-033b-0410-95b4-af486b8189e5>
2009-10-21 04:08:50 +0000

    KS-1900: update tools for build 1.2.x and 1.2-SNAPSHOT with portal 2.5.6.1
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@38990 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/ks/1.2-SNAPSHOT.js
+++ build/src/main/javascript/eXo/products/ks/1.2-SNAPSHOT.js
@@ -6,15 +6,15 @@ function getProduct(version) {
   var product = new Product();
   product.name = "eXoKS" ;
   product.portalwar = "portal.war" ;
-  product.serverPluginVersion = "2.5.3"
+  product.serverPluginVersion = "2.5.6.1"
     
   var tool = Module.GetModule("tools/trunk") ;
-  var kernel = Module.GetModule("kernel/tags/2.0.7") ;
-  var core = Module.GetModule("core/tags/2.1.5") ;
-  var ws = Module.GetModule("ws/tags/1.3.3", {kernel : kernel, core : core});
-  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.6", {kernel : kernel, core : core}) ;    
-  var eXoJcr = Module.GetModule("jcr/tags/1.10.3", {kernel : kernel, core : core, ws : ws}) ;
-  var portal = Module.GetModule("portal/tags/2.5.3", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});  
+  var kernel = Module.GetModule("kernel/tags/2.0.8") ;
+  var core = Module.GetModule("core/tags/2.1.6") ;
+  var ws = Module.GetModule("ws/tags/1.3.4", {kernel : kernel, core : core});
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.7", {kernel : kernel, core : core}) ;    
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.5", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal/tags/2.5.6.1", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});  
   //var webos = Module.GetModule("webos/trunk", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var ks = Module.GetModule("ks/trunk", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
   
