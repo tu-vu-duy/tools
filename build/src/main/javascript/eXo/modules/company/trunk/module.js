67f02166dbc9a7a12cd85e36d9d9b90654c2fbe6
nam_phung_hai <nam_phung_hai@42d3c691-033b-0410-95b4-af486b8189e5>
2009-12-03 10:27:07 +0000

    CWI-260 Make build tool Create the trunk 2.1.6 snapshot
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@41058 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/company/trunk/module.js
+++ build/src/main/javascript/eXo/modules/company/trunk/module.js
@@ -12,7 +12,7 @@ function getModule(params) {
   var ws = params.ws;
   var module = new Module();
 
-  module.version =  "2.2-SNAPSHOT" ;
+  module.version =  "2.1.6-SNAPSHOT" ;
   module.relativeMavenRepo =  "org/exoplatform/company" ;
   module.relativeSRCRepo =  "company/trunk" ;
   module.name =  "company" ;
