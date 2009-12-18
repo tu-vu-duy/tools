4fc48c5e508144153da3ba429f24db9865e10c39
gwenhael <gwenhael@42d3c691-033b-0410-95b4-af486b8189e5>
2009-12-18 02:50:04 +0000

    CWI-307
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@41776 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/company/trunk.js
+++ build/src/main/javascript/eXo/products/company/trunk.js
@@ -16,7 +16,7 @@ function getProduct(version) {
   var core = Module.GetModule("core/tags/2.1.5") ;
   var ws = Module.GetModule("ws/tags/1.3.3", {kernel : kernel, core : core});
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.6", {kernel : kernel, core : core}) ;
-  var eXoJcr = Module.GetModule("jcr/tags/1.10.4", {kernel : kernel, core : core, ws : ws}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.5.1", {kernel : kernel, core : core, ws : ws}) ;
   var portal = Module.GetModule("portal/tags/2.5.3", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
   var dms = Module.GetModule("ecm/dms/tags/2.3.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
   var cs = Module.GetModule("cs/tags/1.2", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
