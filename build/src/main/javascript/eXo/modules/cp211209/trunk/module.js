bb6882987cc786205584a250d42f593509d33116
hnjah <hnjah@42d3c691-033b-0410-95b4-af486b8189e5>
2009-12-22 10:51:38 +0000

    TNCP-12: Create BFPME SVN sources
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@41921 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/cp211209/trunk/module.js
@@ -0,0 +1,47 @@
+eXo.require("eXo.projects.Module");
+eXo.require("eXo.projects.Product");
+
+function getModule(params) {
+
+	var kernel = params.kernel;
+	var core = params.core;
+	var ws = params.ws;
+	var eXoPortletContainer = params.eXoPortletContainer;
+	var jcr = params.eXoJcr;
+	var portal = params.portal;
+	var dms = params.dms;
+	var workflow = params.workflow ;
+	var wcm = params.wcm ;
+	var cs = params.cs ;
+	var ks = params.ks ;
+	var allinone = params.allinone;
+	var module = new Module();
+
+	module.version = "0.1-SNAPSHOT";
+	module.relativeMavenRepo = "org/exoplatform/cp211209";
+	module.relativeSRCRepo = "cp211209/trunk";
+	module.name = "cp211209";
+	
+	module.web={};
+
+	module.web.portal=
+		new Project("org.exoplatform.cp211209", "cp211209.web.portal", "exo-portal", module.version).
+			addDependency(portal.web.eXoResources).
+		    addDependency(portal.web.eXoMacSkin).
+		    addDependency(portal.web.eXoVistaSkin).
+		    addDependency(portal.webui.portal).
+		    addDependency(jcr.frameworks.command).
+		    addDependency(jcr.frameworks.web).
+		    addDependency(portal.web.rest);
+
+	module.exoconf = 
+		new Project("org.exoplatform.cp211209", "cp211209.exo-conf", "jar", module.version);
+	
+	module.server = {};
+	module.server.tomcat = {};
+	module.server.tomcat.patch = 
+		new Project("org.exoplatform.cp211209", "cp211209.server.tomcat.patch", "jar", module.version);
+	
+	return module;
+
+}
