981df08e16951907c210ff596b43e0c686f3e39c
boubaker <boubaker@42d3c691-033b-0410-95b4-af486b8189e5>
2010-01-10 22:51:03 +0000

    DGDDI-57
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@42484 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/cp040110/trunk/module.js
@@ -0,0 +1,89 @@
+eXo.require("eXo.projects.Module");
+eXo.require("eXo.projects.Product");
+
+function getModule(params) {
+
+	var kernel = params.kernel;
+	var core = params.core;
+	var ws = params.ws;
+	var eXoPortletContainer = params.eXoPortletContainer;
+	var jcr = params.eXoJcr;
+	var portal = params.portal;
+	var dms = params.dms;
+	var workflow = params.workflow ;
+	var wcm = params.wcm ;
+	var cs = params.cs ;
+	var ks = params.ks ;
+	var allinone = params.allinone;
+	var module = new Module();
+	
+	
+	var bullDevVersion = "1.0-SNAPSHOT";
+
+	module.version = "1.0-SNAPSHOT";
+	module.relativeMavenRepo = "org/exoplatform/cp040110";
+	module.relativeSRCRepo = "cp040110/trunk";
+	module.name = "cp040110";
+	
+	module.web = {};
+
+	module.web.portal =
+		new Project("org.exoplatform.cp040110", "cp040110.web.portal", "exo-portal", module.version).
+			addDependency(portal.web.eXoResources).
+		    addDependency(portal.webui.portal).
+		    addDependency(jcr.frameworks.command).
+		    addDependency(jcr.frameworks.web).
+		    addDependency(portal.web.rest).
+		    addDependency(new Project("org.postgresql", "postgresql-jdbc3", "jar", "8.2-505"));
+	
+	module.portlet = {};
+	
+	module.portlet.MesInfosPortlets =
+		new Project("org.exoplatform.cp040110", "cp040110.portlet.mesInfos", "war", module.version);
+
+	module.portlet.dms =
+		new Project("org.exoplatform.cp040110", "cp040110.portlet.ecm", "war", module.version).
+		addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.cms", "jar",  dms.version)) .     
+		addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.deployment", "jar",  dms.version)) .    
+		addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.publication", "jar", dms.version)).
+		addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.connector.fckeditor", "jar", dms.version)).
+		addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.webui.dms", "jar", dms.version)).
+		addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.webui.ext", "jar", dms.version)).
+		addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.portlet.ecm.ext.config", "jar", dms.version)).
+		addDependency(new Project("rome", "rome", "jar", "0.9")) .
+		addDependency(new Project("com.totsp.feedpod", "itunes-com-podcast", "jar", "0.2")) .
+		addDependency(new Project("ical4j", "ical4j", "jar", "0.9.20")) .
+		addDependency(new Project("jdom", "jdom", "jar", "1.0")).
+		addDependency(new Project("org.apache.ws.commons", "ws-commons-util", "jar", "1.0.1")).
+		addDependency(new Project("com.sun.xml.stream", "sjsxp", "jar", "1.0")).
+		addDependency(new Project("pdfbox", "pdfbox", "jar", "0.7.2"));
+		module.portlet.dms.deployName = "ecm";
+
+		
+	module.portlet.bullPortlets =
+		new Project("fr.bull.aladinng.dev-bull", "bullPortlets", "war", module.version).
+			addDependency(new Project("fr.bull.aladinng.dev-bull", "common", "jar", bullDevVersion)).
+			addDependency(new Project("fr.bull.aladinng.dev-bull.services.startup", "content-publication-initialization-service", "jar", bullDevVersion)).
+			addDependency(new Project("fr.bull.aladinng.dev-bull.event-listeners", "groups-listeners", "jar", bullDevVersion)).
+			addDependency(new Project("fr.bull.aladinng.dev-bull.services.management", "groups-management-service", "jar", bullDevVersion)).
+			addDependency(new Project("fr.bull.aladinng.dev-bull.event-listeners", "memberships-listeners", "jar", bullDevVersion)).
+			addDependency(new Project("fr.bull.aladinng.dev-bull.services.management", "memberships-management-service", "jar", bullDevVersion)).
+			addDependency(new Project("fr.bull.aladinng.dev-bull", "resources", "jar", bullDevVersion)).
+			addDependency(new Project("fr.bull.aladinng.dev-bull.services.REST", "REST-groups-service", "jar", bullDevVersion)).
+			addDependency(new Project("fr.bull.aladinng.dev-bull.services.REST", "REST-memberships-service", "jar", bullDevVersion)).
+			addDependency(new Project("fr.bull.aladinng.dev-bull.services.REST", "REST-users-service", "jar", bullDevVersion)).
+			addDependency(new Project("fr.bull.aladinng.dev-bull.services.startup", "symlink-publication-initialization-service", "jar", bullDevVersion)).
+			addDependency(new Project("fr.bull.aladinng.dev-bull.event-listeners", "users-listeners", "jar", bullDevVersion)).
+			addDependency(new Project("fr.bull.aladinng.dev-bull.services.management", "users-management-service", "jar", bullDevVersion));
+
+	module.exoconf = 
+		new Project("org.exoplatform.cp040110", "cp040110.config", "jar", module.version);
+	
+	module.server = {};
+	module.server.tomcat = {};
+	module.server.tomcat.patch = 
+		new Project("org.exoplatform.cp040110", "cp040110.server.tomcat.patch", "jar", module.version);
+	
+	return module;
+
+}
---
+++ build/src/main/javascript/eXo/products/cp040110/trunk.js
@@ -0,0 +1,101 @@
+eXo.require("eXo.projects.Module");
+eXo.require("eXo.projects.Product");
+
+function getProduct(version) {
+	var product = new Product();
+
+	product.name = "cp040110Product";
+	product.portalwar = "portal.war";
+	product.codeRepo = "cp040110/trunk";
+	product.useContentvalidation = true;
+	product.contentvalidationVersion = "2.5.2.1";
+	product.useWorkflow = false;
+	product.workflowVersion = "1.0.4.1";
+	product.serverPluginVersion = "2.5.6.2";
+
+	var tool = Module.GetModule("tools/trunk");
+	var kernel = Module.GetModule("kernel/tags/2.0.8");
+	var core = Module.GetModule("core/tags/2.1.6");
+	var ws = Module.GetModule("ws/tags/1.3.4");
+	var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.7", {kernel : kernel, core : core}) ;    
+	var eXoJcr = Module.GetModule("jcr/tags/1.10.5.1", {kernel : kernel, core : core, ws : ws}) ;
+	var portal = Module.GetModule("portal/tags/2.5.6.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, ws : ws });  
+	var dms = Module.GetModule("ecm/dms/tags/2.5.2.1", {kernel : kernel, core : core,ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr , portal : portal});
+	var wcm = Module.GetModule("ecm/wcm/tags/1.2.1", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, dms : dms});
+	
+	var cs = Module.GetModule("cs/tags/1.3.2.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+	var ks = Module.GetModule("ks/tags/1.2.1.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+
+	var webos = Module.GetModule("webos/tags/1.5.3.1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+    var allinone = Module.GetModule("allinone/tags/1.6", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, cs: cs, ks: ks, dms : dms, wcm: wcm,workflow : workflow});
+	
+	var cp040110 = Module.GetModule("cp040110/trunk", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, cs: cs, ks: ks, dms : dms, wcm : wcm});
+
+	product.addDependencies(portal.portlet.exoadmin);
+	product.addDependencies(portal.portlet.web);
+	product.addDependencies(portal.portlet.dashboard);
+	product.addDependencies(portal.eXoGadgetServer);
+	product.addDependencies(portal.eXoGadgets);
+	product.addDependencies(portal.webui.portal);
+	product.addDependencies(portal.web.eXoResources);
+	
+	product.addDependencies(dms.gadgets);
+
+	product.addDependencies(wcm.portlet.webpresentation);
+	product.addDependencies(wcm.portlet.websearches);
+	product.addDependencies(wcm.portlet.newsletter);
+	product.addDependencies(wcm.portlet.formgenerator);
+	product.addDependencies(wcm.web.eXoWCMResources);
+	product.addDependencies(dms.web.eXoDMSResources) ;
+	product.addDependencies(cp040110.web.portal) ;
+	product.addDependencies(cp040110.exoconf) ;
+	product.addDependencies(cp040110.portlet.MesInfosPortlets) ;
+	product.addDependencies(cp040110.portlet.bullPortlets) ;
+	product.addDependencies(cp040110.portlet.dms);
+	
+	product.addDependencies(cs.eXoApplication.mail) ;
+	product.addDependencies(cs.eXoApplication.calendar) ;
+	product.addDependencies(cs.eXoApplication.contact) ;
+	product.addDependencies(cs.eXoApplication.content) ;
+	product.addDependencies(cs.eXoApplication.chat) ;
+// Patched in AIO to display chatbar entirely in WCM layout (add css right:0; to UIChatBarPortlet)
+//  product.addDependencies(cs.eXoApplication.chatbar) ;
+	product.addDependencies(allinone.patches.chatbar) ;
+	
+	product.addDependencies(cs.web.webservice) ;
+	product.addDependencies(cs.web.csResources) ;
+	
+	/* KS */  
+	product.addDependencies(ks.eXoApplication.forum) ;
+	product.addDependencies(ks.eXoApplication.faq) ;
+	product.addDependencies(ks.eXoApplication.common) ;
+
+	product.addDependencies(ks.web.webservice) ;
+	product.addDependencies(ks.web.ksResources) ;
+
+	/* cleanup duplicated lib*/
+	product.removeDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", "1.3"));
+	product.removeDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", "1.3.1"));
+	product.removeDependency(new Project("ical4j", "ical4j", "jar", "0.9.20"));
+	product.removeDependency(new Project("javax.mail", "mail", "jar", "1.4"));
+	product.removeDependency(new Project("commons-httpclient", "commons-httpclient", "jar", "3.0"));
+	product.removeDependency(new Project("commons-collections", "commons-collections", "jar", "3.1"));
+	product.removeDependency(new Project("rome", "rome", "jar", "0.8"));
+	product.removeDependency(new Project("javax.mail", "mail", "jar", "1.4"));
+	
+	/* WEBOS */
+	product.addDependencies(webos.web.webosResources);
+  
+	//product.addServerPatch("tomcat", cs.server.tomcat.patch) ;
+	//product.addServerPatch("tomcat", allinone.patches.tomcat) ;
+	product.addServerPatch("tomcat", cp040110.server.tomcat.patch) ;
+	product.addServerPatch("jboss",  cs.server.jboss.patch) ;
+	product.addServerPatch("jbossear", portal.server.jbossear.patch);
+	product.addServerPatch("jonas", portal.server.jonas.patch);
+	product.addServerPatch("ear", portal.server.websphere.patch);
+
+	product.module = cp040110;
+	product.dependencyModule = [ tool, kernel, core, ws, eXoPortletContainer, eXoJcr, portal, dms , wcm, cs, ks, allinone];
+
+	return product;
+}
