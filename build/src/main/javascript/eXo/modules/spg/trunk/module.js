ac3d490d93c7b053c30778fc59afd8c183ea642a
due_tran_tri <due_tran_tri@42d3c691-033b-0410-95b4-af486b8189e5>
2010-02-03 07:05:13 +0000

    SPG-6
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@43262 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/modules/spg/trunk/module.js
+++ build/src/main/javascript/eXo/modules/spg/trunk/module.js
@@ -27,7 +27,29 @@ function getModule(params) {
   module.web = {};
   module.web.SPGResources = 
     new Project("org.exoplatform.spg", "spg.web.SPGResources", "war", module.version);
-        
+  
+  module.portlet = {};
+  module.portlet.dashboard = new Project("org.exoplatform.spg", "spg.portlet.dashboard", "war", module.version).
+  addDependency(new Project("org.exoplatform.portal", "exo.portal.component.dashboard", "jar", "2.5.5"));
+  
+  module.portlet.dms = 
+    new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.portlet.ecm.core.web", "war", "2.5.1.SPG_x").
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.cms", "jar",  dms.version)) .     
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.deployment", "jar",  dms.version)) .    
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.publication", "jar", dms.version)).
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.connector.fckeditor", "jar", dms.version)).
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.webui.dms", "jar", dms.version)).
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.webui.ext", "jar", dms.version)).
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.portlet.ecm.ext.config", "jar", dms.version)).
+    addDependency(new Project("rome", "rome", "jar", "0.9")) .
+    addDependency(new Project("com.totsp.feedpod", "itunes-com-podcast", "jar", "0.2")) .
+    addDependency(new Project("ical4j", "ical4j", "jar", "0.9.20")) .
+    addDependency(new Project("jdom", "jdom", "jar", "1.0")).
+    addDependency(new Project("org.apache.ws.commons", "ws-commons-util", "jar", "1.0.1")).
+    addDependency(new Project("com.sun.xml.stream", "sjsxp", "jar", "1.0")).
+    addDependency(new Project("pdfbox", "pdfbox", "jar", "0.7.2"));
+    module.portlet.dms.deployName = "ecm";  
+  
   module.web.portal = 
     new Project("org.exoplatform.spg", "spg.web.portal", "exo-portal", module.version).
     addDependency(portal.web.eXoResources) .
