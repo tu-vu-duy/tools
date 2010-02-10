e79adf43374a158ce139e8f29bfb33926d804aa7
gwenhael <gwenhael@42d3c691-033b-0410-95b4-af486b8189e5>
2010-02-10 03:40:07 +0000

    DMS 2.3.2.CWI_2
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@43565 42d3c691-033b-0410-95b4-af486b8189e5

--- build/src/main/javascript/eXo/products/company/trunk.js
+++ build/src/main/javascript/eXo/products/company/trunk.js
@@ -7,7 +7,7 @@ function getProduct(version) {
   product.name = "company" ;
   product.portalwar = "portal.war" ;
   product.codeRepo = "company/trunk" ;
-  product.contentvalidationVersion = "2.3.2.CWI_1";
+  product.contentvalidationVersion = "2.3.2.CWI_2";
   product.workflowVersion = "1.0";
   product.serverPluginVersion = "2.5.3.CWI_1" ;
   
@@ -18,7 +18,7 @@ function getProduct(version) {
   var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.6", {kernel : kernel, core : core}) ;
   var eXoJcr = Module.GetModule("jcr/tags/1.10.6.CWI_1", {kernel : kernel, core : core, ws : ws}) ;
   var portal = Module.GetModule("portal/tags/2.5.3", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
-  var dms = Module.GetModule("ecm/dms/tags/2.3.2.CWI_1", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var dms = Module.GetModule("ecm/dms/tags/2.3.2.CWI_2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
   var cs = Module.GetModule("cs/tags/1.2", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
   var webos = Module.GetModule("webos/tags/1.5", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
   var ks = Module.GetModule("ks/tags/1.1.CWI_1", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
