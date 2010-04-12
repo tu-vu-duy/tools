eefedb60611c2fb2b26c45784312e8bcf03908cc
dhifallah <dhifallah@42d3c691-033b-0410-95b4-af486b8189e5>
2010-04-12 01:43:42 +0000

    SC-300 Tag & deliver sprint 15 : add module.js
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@46207 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/cp040608/patches/ecm/dms/tags/2.3.0.SC_19/module.js
@@ -0,0 +1,66 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var ws = params.ws;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;  
+  var module = new Module();
+  
+  module.version = "2.3.0.SC_19" ;
+  module.relativeMavenRepo =  "org/exoplatform/ecm/dms" ;
+  module.relativeSRCRepo =  "patches/secours-catholique/ecm/dms/tags/2.3.0.SC_19" ;
+  module.name =  "dms" ;
+    
+  module.portlet = {}
+  module.portlet.dms = 
+    new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.portlet.ecm", "exo-portlet", module.version).
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.portlet.ecm", "war",  module.version)) .     
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.cms", "jar",  module.version)) .     
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.deployment", "jar",  module.version)) .    
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.publication", "jar", module.version)).
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.connector.fckeditor", "jar", module.version)).
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.webui.dms", "jar", module.version)).
+    addDependency(new Project("rome", "rome", "jar", "0.8")) .
+    addDependency(new Project("com.totsp.feedpod", "itunes-com-podcast", "jar", "0.2")) .
+    addDependency(new Project("ical4j", "ical4j", "jar", "0.9.20")) .
+    addDependency(new Project("jdom", "jdom", "jar", "1.0")).
+    addDependency(new Project("org.apache.ws.commons", "ws-commons-util", "jar", "1.0.1")).
+    addDependency(new Project("com.sun.xml.stream", "sjsxp", "jar", "1.0")).
+    addDependency(new Project("pdfbox", "pdfbox", "jar", "0.7.2"));
+  
+  module.portlet.jcr_console = 
+    new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.portlet.jcr-console", "exo-portlet", module.version).
+    addDependency(new Project("exo-weblogic", "exo-weblogic-authproviders", "jar", "1.0")).
+	  addDependency(new Project("exo-weblogic", "exo-weblogic-loginmodule", "jar", "1.0")).  
+	  addDependency(new Project("commons-logging", "commons-logging", "jar", "1.0.4"));
+  
+  module.gadgets = 
+    new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.gadgets", "war", module.version).
+    addDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", "1.3.5"));  
+    module.gadgets.deployName = "eXoDMSGadgets";
+  
+  module.application = {}
+  module.application.rest = new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.publication","jar", module.version).
+  	addDependency(new Project("org.exoplatform.ws", "exo.ws.frameworks.json", "jar", "1.3.5"));
+  
+  module.web = {}
+  module.web.eXoDMSResources = 
+    new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.web.eXoDMSResources", "war", module.version) ;  
+  module.web.eXoDMSResources.deployName = "eXoDMSResources" ;
+      
+  module.web.dmsportal = 
+    new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.web.portal", "exo-portal", module.version).
+    addDependency(portal.web.eXoResources) .
+    addDependency(portal.web.eXoMacSkin) .
+    addDependency(portal.web.eXoVistaSkin) .
+    addDependency(portal.webui.portal) .
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web) ;   
+  
+  return module;
+}
