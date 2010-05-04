0fe1fe2645af3657b6c91936f7b5eb01fa51bedc
gwenhael <gwenhael@42d3c691-033b-0410-95b4-af486b8189e5>
2010-05-04 04:19:17 +0000

    CWI-408
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@47157 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/products/company-forum/1.0.1.js
@@ -0,0 +1,50 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+
+  var product = new Product();
+  product.name = "CompanyForum" ;
+  product.portalwar = "portal.war" ;
+
+  var tool = Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.9") ;
+  var core = Module.GetModule("core/tags/2.1.7") ;
+  var ws = Module.GetModule("ws/tags/1.3.5", {kernel : kernel, core : core});
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.4", {kernel : kernel, core : core}) ;    
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.6.COMPANYFORUM_1", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal/tags/2.5.3", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});  
+  var ks = Module.GetModule("ks/tags/1.1.COMPANYFORUM_2", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var CompanyForum = Module.GetModule("company-forum/tags/1.0.1", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ks : ks});
+
+  product.addDependencies(portal.web.rest) ;
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.portlet.dashboard) ;
+	product.addDependencies(portal.eXoGadgetServer) ;
+	product.addDependencies(portal.eXoGadgets) ;
+  product.addDependencies(portal.webui.portal);
+  //product.addDependencies(portal.eXoWidget.web) ;
+            
+  product.addDependencies(ks.eXoApplication.forum) ;
+  product.addDependencies(ks.eXoApplication.faq) ;
+  product.addDependencies(ks.eXoApplication.common) ;
+  //product.addDependencies(ks.eXoApplication.blog) ;
+  //product.addDependencies(ks.eXoApplication.wiki) ;
+
+  product.addDependencies(ks.web.ksportal) ;
+  product.addDependencies(ks.web.webservice) ;
+  product.addDependencies(ks.web.ksResources) ;
+  //product.addDependencies(cs.eXoApplication.contact) ;
+  
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+    
+  product.codeRepo = "company-forum/tags/1.0.1" ;
+
+  product.module = CompanyForum ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, ks];
+    
+  return product ;
+}
