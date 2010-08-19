2daad1215b974dc35937b3ee615d37e408bf5399
bourasom <bourasom@42d3c691-033b-0410-95b4-af486b8189e5>
2010-08-19 16:04:37 +0000

    SST-248: create js modules for CG38-2.2
    git-svn-id: https://svn.exoplatform.org/projects/tools/trunk@50996 42d3c691-033b-0410-95b4-af486b8189e5

---
+++ build/src/main/javascript/eXo/modules/cg38/tags/2.2/module.js
@@ -0,0 +1,63 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+  var ecm = params.ecm;
+  //var cs = params.cs;
+  var wcm = params.wcm;
+  var ws = params.ws;
+
+  var module = new Module();
+
+  module.version = "2.2" ;
+  module.relativeMavenRepo =  "org/exoplatform/cg38" ;
+  module.relativeSRCRepo =  "cg38/tags/2.2" ;
+  module.name =  "cg38" ;  
+
+  module.component = {}
+  module.component.jcraction = new Project("org.exoplatform.cg38", "cg38.component.jcraction", "jar", module.version);
+//	addDependency(kernel.container).
+ //	addDependency(jcr.component.core).
+//	addDependency(jcr.component.ext).
+//	addDependency(ecm.component.cms);
+
+  module.component.cg38CategoriesService = new Project("org.exoplatform.cg38", "cg38.component.categories", "jar", module.version);
+  module.component.cg38Indexation = new Project("org.exoplatform.cg38", "cg38.component.indexation", "jar", module.version);
+  //module.component.search = new Project("org.exoplatform.cg38", "cg38.component.search", "jar", module.version);
+  
+  module.portlet = {}
+  module.portlet.web = new Project("org.exoplatform.cg38", "cg38.portlet.web", "exo-portlet", module.version).
+  	addDependency(new Project("org.exoplatform.cg38", "cg38.component.rss", "jar",  module.version)).
+  	addDependency(new Project("org.exoplatform.cg38", "cg38.component.search", "jar",  module.version)).
+	addDependency(new Project("org.exoplatform.cg38", "cg38.component.authentication", "jar",  module.version)).
+  	addDependency(new Project("org.exoplatform.cg38", "taxonomy.cg38.component", "jar",  module.version));
+  module.portlet.web.deployName = "cg38PortletWeb" ;
+  module.web = {}
+  module.web.cg38Resources = new Project("org.exoplatform.cg38", "cg38.web.cg38Resources", "war", module.version);
+  module.web.cg38Resources.deployName = "eXoResourcesCG38" ;
+        
+
+  module.server = {}
+  module.server.tomcat = {}
+  module.server.tomcat.patch = 
+    new Project("org.exoplatform.cg38", "cg38.server.tomcat.patch", "jar", module.version);
+
+
+
+  module.web.cg38portal = 
+    new Project("org.exoplatform.cg38", "cg38.web.portal", "exo-portal", module.version).
+    addDependency(portal.web.eXoResources) .
+    addDependency(portal.web.eXoMacSkin) .
+    addDependency(portal.web.eXoVistaSkin) .
+    addDependency(portal.webui.portal) .
+    addDependency(jcr.frameworks.command) .
+    addDependency(jcr.frameworks.web);
+
+  return module;
+}
---
+++ build/src/main/javascript/eXo/products/cg38/2.2.js
@@ -0,0 +1,67 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "cg38" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "cg38/tags/2.2" ;
+  product.useWorkflow = true;
+  product.workflowVersion = "2.2.0.CG38_5";
+  product.serverPluginVersion = "2.5.0.CG38_4" ;
+    
+  var tool =  Module.GetModule("tools/trunk") ;
+  var kernel = Module.GetModule("kernel/tags/2.0.6") ;
+  var core = Module.GetModule("core/tags/2.1.3") ;
+  var ws = Module.GetModule("ws/tags/1.3.1");
+  var eXoPortletContainer = Module.GetModule("portlet-container/tags/2.0.4", {kernel : kernel, core : core}) ;
+  var eXoJcr = Module.GetModule("jcr/tags/1.10.1", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal/tags/2.5.0.CG38_4", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var ecm = Module.GetModule("ecm/dms/tags/2.2.0.CG38_5", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, ws : ws, eXoJcr : eXoJcr, portal : portal});
+  var wcm = Module.GetModule("ecm/wcm/tags/1.0.0.CG38_1", {kernel : kernel, core : core, ws : ws, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ecm : ecm});
+  var cs = Module.GetModule("cs/tags/1.2.0.CG38_2", {kernel : kernel, ws : ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal});
+  var cg38 = Module.GetModule("cg38/tags/2.2", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr, portal : portal, ecm : ecm, wcm : wcm});
+  
+  product.addDependencies(portal.portlet.exoadmin) ;
+  product.addDependencies(portal.portlet.web) ;
+  product.addDependencies(portal.portlet.dashboard) ;  
+	product.addDependencies(portal.eXoGadgetServer) ;
+	product.addDependencies(portal.eXoGadgets) ;
+	product.addDependencies(portal.web.rest);
+	
+	product.addDependencies(ecm.portlet.ecm);
+  product.addDependencies(ecm.portlet.workflow);   
+  		 
+  product.addDependencies(wcm.portlet.webpresentation);
+  product.addDependencies(wcm.portlet.websearches); 
+  product.addDependencies(wcm.web.eXoWCMResources) ;
+  product.addDependencies(ecm.web.eXoECMResources) ;
+  //product.addDependencies(wcm.web.wcmportal) ; 
+    
+  product.addDependencies(cs.eXoApplication.content) ;   
+  
+  product.addDependencies(cg38.web.cg38portal) ;
+  product.addDependencies(cg38.web.cg38Resources) ;
+  product.addDependencies(cg38.portlet.web) ;
+  product.addDependencies(cg38.component.jcraction);
+  product.addDependencies(cg38.component.cg38CategoriesService);
+  product.addDependencies(cg38.component.cg38Indexation);
+  //product.addDependencies(cg38.component.search);
+  //product.addDependencies(cg38.component.rss);
+  
+  product.addDependencies(ws.frameworks.cometd);
+  
+  product.removeDependency(eXoPortletContainer.web.wsrp);
+  product.removeDependency(eXoPortletContainer.services.wsrp1);
+  product.removeDependency(eXoPortletContainer.services.wsrp2);
+  
+  product.addServerPatch("tomcat", cg38.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+
+  product.module = cg38 ;
+  product.dependencyModule = [tool, kernel, core, eXoPortletContainer, ws, eXoJcr, portal, ecm, wcm, cs];
+    
+  return product ;
+}
